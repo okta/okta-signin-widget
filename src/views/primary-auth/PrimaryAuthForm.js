feb9e29362dccefd61721e44db0921e0e17e69bc
Ujjwal Reddy <ujjwal.reddy@okta.com>
2016-02-16 17:18:49 -0800

    Spinner when the ajax requests are running
    8ac0n:test
    resolves:OKTA-78589
    
    OKTA-78589
    <<<Jenkins Check-In of Tested SHA: 9bea123638a517172b3f28d3aa886615afe9508e for ujjwal.reddy@okta.com>>> 
    Artifact: okta-core.webapp

--- src/views/primary-auth/PrimaryAuthForm.js
+++ src/views/primary-auth/PrimaryAuthForm.js
@@ -69,6 +69,10 @@ define([
           });
         }
         this.model.save();
+        this.options.appState.trigger('loading', true);
+      });
+      this.listenTo(this.model, 'error', function () {
+        this.options.appState.trigger('loading', false);
       });
       if (this.settings.get('features.preventBrowserFromSavingOktaPassword')) {
         this.add(PasswordJammer);
