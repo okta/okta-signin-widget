058079378aeda2581fb578d5b758ed6c40d8b530
Ujjwal Reddy <ujjwal.reddy@okta.com>
2016-03-23 17:17:28 -0700

    Add a  spinner when there is no security image
    bacon:test
    resolves:OKTA-82292

--- test/spec/EnrollSymantecVip_spec.js
+++ test/spec/EnrollSymantecVip_spec.js
@@ -60,9 +60,11 @@ function (Q, _, $, OktaAuth, Util, Form, Beacon, Expect, $sandbox,
 
     describe('Header & Footer', function () {
       itp('displays the correct factorBeacon', function () {
+        $.fx.off = true;
         return setup().then(function (test) {
           expect(test.beacon.isFactorBeacon()).toBe(true);
           expect(test.beacon.hasClass('mfa-symantec')).toBe(true);
+          $.fx.off = false;
         });
       });
       itp('has autocomplete off', function () {
