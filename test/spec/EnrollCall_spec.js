e592d806b05eb5d5edc47179a8a946792f93a289
Mauricio Castillo <mauricio.castillosilva@okta.com>
2016-07-22 14:52:06 -0700

    Added support for Czech
    - Added files for Czech language
    - Also, fixing small text changes due to the i18n update
    
    Resolves: OKTA-96127

--- test/spec/EnrollCall_spec.js
+++ test/spec/EnrollCall_spec.js
@@ -162,7 +162,7 @@ function (Q, _, $, OktaAuth, LoginUtil, Util, Form, Beacon, Expect, $sandbox,
           var countries = test.form.countriesList();
           expect(countries[0]).toEqual({ val: 'AF', text: 'Afghanistan' });
           expect(countries[1]).toEqual({ val: 'AL', text: 'Albania' });
-          expect(countries[237]).toEqual({ val: 'ZW', text: 'Zimbabwe' });
+          expect(countries[238]).toEqual({ val: 'ZW', text: 'Zimbabwe' });
         });
       });
       itp('does not include countries with no calling codes', function () {
