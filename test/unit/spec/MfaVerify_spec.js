09407155b74fecd32597fe1a6d4d8faa103e206f
Gowthami <gowthami.dommety@okta.com>
2017-04-04 14:49:27 -0700

    removed comment

--- test/unit/spec/MfaVerify_spec.js
+++ test/unit/spec/MfaVerify_spec.js
@@ -2015,7 +2015,6 @@ function (Okta,
         itp('shows the right beacon for Security Key (U2F)', function () {
           return setupU2F({u2f: true}).then(function (test) {
             expectHasRightBeaconImage(test, 'mfa-u2f');
-            //expect sign to be called
             return Expect.waitForSpyCall(window.u2f.sign);
           });
         });
