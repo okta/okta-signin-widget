3e9a0fa34539610fdd509f36ce29c851391d7a30
nikhilvenkatraman-okta <nikhil.venkatraman@okta.com>
2017-05-23 15:59:00 -0700

    :seedling: Add registration button as optional attribute in sign-in widget (#228)
    Add registration button as optional attribute in sign-in widget.
    
    Resolves: OKTA-125666

--- src/models/Settings.js
+++ src/models/Settings.js
@@ -132,7 +132,11 @@ function (Okta, Q, Errors, BrowserFeatures, Util, Logger, OAuth2Util, config) {
       'helpLinks.custom': 'array',
 
       //Custom Buttons
-      'customButtons': ['array', false, []]
+      'customButtons': ['array', false, []],
+
+      //Registration
+      'registration': 'object',
+      'registration.click': 'function'
     },
 
     derived: {
