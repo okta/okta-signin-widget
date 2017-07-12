07f08acd4ef4c11cf9e45304afd2e646c59310aa
rchild-okta <rchild@okta.com>
2017-07-12 09:55:52 -0700

    Moves private modules to packages, and uses package-lock.json (#260)
    * Moves private modules to packages, and package-lock
    
    - Moves checked in node_modules to packages folder
    - Swaps out npm-shrinkwrap with npm5's package-lock.json
    - Code changes to support reading from packages dir
    
    RESOLVES OKTA-132800

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
 
   require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
-  
+
   var open        = require('open'),
       Handlebars  = require('handlebars'),
       _           = require('underscore'),
@@ -72,13 +72,13 @@ module.exports = function (grunt) {
           {expand: true, cwd: 'src/', src: ['**'], dest: JS + '/'},
 
           // Courage files
-          {expand: true, cwd: 'node_modules/@okta/courage/src/', src: ['**'], dest: JS + '/shared/'},
-          {expand: true, cwd: 'node_modules/@okta/courage/src/vendor', src: ['**'], dest: JS + '/vendor/'},
+          {expand: true, cwd: 'packages/@okta/courage/src/', src: ['**'], dest: JS + '/shared/'},
+          {expand: true, cwd: 'packages/@okta/courage/src/vendor', src: ['**'], dest: JS + '/vendor/'},
 
           // i18n files
           {
             expand: true,
-            cwd: 'node_modules/@okta/i18n/dist/',
+            cwd: 'packages/@okta/i18n/dist/',
             src: [
               'json/{login,country}*.json',
               'properties/{login,country}*.properties'
@@ -97,7 +97,7 @@ module.exports = function (grunt) {
           // jquery.qtip.css -> _jquery.qtip.scss
           {
             expand: true,
-            cwd: 'node_modules/@okta/qtip2/dist/',
+            cwd: 'packages/@okta/qtip2/dist/',
             src: 'jquery.qtip.css',
             dest: 'target/sass/widgets',
             rename: function () {
@@ -441,7 +441,7 @@ module.exports = function (grunt) {
 
     retire: {
       js: ['src/**/*.js', 'test/**/*.js'],
-      node: ['node_modules'],
+      node: ['node_modules', 'packages'],
       options: {
         packageOnly: false
       }
