f03e977b32de897a7e6ba611422ad30d197c29fc
jmelberg <jordan.melberg@okta.com>
2018-07-25 10:26:08 -0700

    :white_check_mark: Adds e2e test

--- test/e2e/specs/dev_spec.js
+++ test/e2e/specs/dev_spec.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2015-2016, Okta, Inc. and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015-Present, Okta, Inc. and/or its affiliates. All rights reserved.
  * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
  *
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
@@ -10,9 +10,25 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 /* global browser, element, by, oktaSignIn */
-var util = require('../util/util');
+var PrimaryAuthPage = require('../page-objects/PrimaryAuthPage'),
+    OIDCAppPage     = require('../page-objects/OIDCAppPage'),
+    util            = require('../util/util');
 
 describe('Dev Mode flows', function() {
+
+  function renderWidget() {
+    function renderAndRedirect() {
+      oktaSignIn.renderEl(
+        { el: '#okta-login-container' },
+        function (res) {
+          if (res.status === 'SUCCESS') {
+            res.session.setCookieAndRedirect('{{{WIDGET_TEST_SERVER}}}' + '/app/UserHome');
+          }
+        }
+      );
+    }
+    browser.executeScript(renderAndRedirect);
+  }
 
   beforeEach(function() {
     browser.driver.get('about:blank');
@@ -21,6 +37,7 @@ describe('Dev Mode flows', function() {
   });
 
   it('can hide, show, remove, and start a widget', function() {
+    renderWidget();
     // Ensure the widget exists
     var el = element(by.css('#okta-sign-in'));
     expect(el.isDisplayed()).toBe(true);
@@ -47,7 +64,29 @@ describe('Dev Mode flows', function() {
     expect(el.isDisplayed()).toBe(true);
   });
 
+  it('can login and return tokens using the setupWithShowSignInToGetTokens method', function () {
+    var options = {
+      el: '#okta-login-container',
+      clientId: 'rW47c465c1wc3MKzHznu',
+      redirectUri: 'http://localhost:3000/done',
+      scope: 'openid profile'
+    };
+
+    browser.executeScript(`oktaSignIn.showSignInToGetTokens(${JSON.stringify(options)})`);
+
+    // Ensure the widget exists
+    var el = element(by.css('#okta-sign-in'));
+    expect(el.isDisplayed()).toBe(true);
+
+    var primaryAuth = new PrimaryAuthPage(),
+        oidcApp = new OIDCAppPage();
+
+    primaryAuth.loginToForm('{{{WIDGET_BASIC_USER}}}', '{{{WIDGET_BASIC_PASSWORD}}}');
+    expect(oidcApp.getIdTokenUser()).toBe('Saml Jackson');
+  });
+
   it('log a console message when tokens are not parsed from the URL after the Widget is rendered', function() {
+    renderWidget();
     // Ensure the widget exists
     var el = element(by.css('#okta-sign-in'));
     expect(el.isDisplayed()).toBe(true);
@@ -56,10 +95,16 @@ describe('Dev Mode flows', function() {
     browser.executeScript('window.location = window.location + "#id_token=abc"');
     browser.refresh(true);
 
+    renderWidget();
+
+    expectToFindLogMessage('Looks like there are still tokens in the URL!');
+  });
+
+  function expectToFindLogMessage(text) {
     browser.manage().logs().get('browser')
     .then(function(logs) {
       var log = logs.find(function(entry) {
-        var message = 'Looks like there are still tokens in the URL!';
+        var message = text;
         return entry.message.includes(message) === true;
       });
       expect(log).toBeDefined();
@@ -67,6 +112,5 @@ describe('Dev Mode flows', function() {
     .catch(function(err) {
       expect(err).toBeUndefined();
     });
-  });
-
+  }
 });
--- test/e2e/templates/basic-dev.tpl
+++ test/e2e/templates/basic-dev.tpl
@@ -3,13 +3,4 @@ var options = {
   'baseUrl': '{{{WIDGET_TEST_SERVER}}}'
 };
 var oktaSignIn = new OktaSignIn(options);
-
-oktaSignIn.renderEl(
-  { el: '#okta-login-container' },
-  function (res) {
-    if (res.status === 'SUCCESS') {
-      res.session.setCookieAndRedirect(options.baseUrl + '/app/UserHome');
-    }
-  }
-);
 {{/devLayout}}
