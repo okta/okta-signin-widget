78b4af45e39ee5a56892f2231d27dcfde2a22445
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-04-18 13:27:14 -0700

    Testing bacon

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
+export SAUCE_USERNAME=okta-qa
+export SAUCE_ACCESS_KEY="$(aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/saucelabs/saucelabs_access_key /dev/stdout)"
+export SAUCE_PLATFORM_NAME="iOS";
+aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/signinwidget/test_credentials ./test_credentials.yaml
+
+cat ./test_credentials.yaml
+
+pip install yq
+
+export WIDGET_TEST_SERVER=$(cat ./test_credentials.yaml | yq .WIDGET_TEST_SERVER | tr -d '"')
+export WIDGET_BASIC_USER=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_USER | tr -d '"')
+export WIDGET_BASIC_PASSWORD=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_PASSWORD | tr -d '"')
+export WIDGET_BASIC_USER_2=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_USER_2 | tr -d '"')
+export WIDGET_BASIC_PASSWORD_2=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_PASSWORD_2 | tr -d '"')
+export WIDGET_BASIC_USER_3=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_USER_3 | tr -d '"')
+export WIDGET_BASIC_PASSWORD_3=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_PASSWORD_3 | tr -d '"')
+export WIDGET_BASIC_USER_4=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_USER_4 | tr -d '"')
+export WIDGET_BASIC_PASSWORD_4=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_PASSWORD_4 | tr -d '"')
+export WIDGET_BASIC_USER_5=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_USER_5 | tr -d '"')
+export WIDGET_BASIC_PASSWORD_5=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_PASSWORD_5 | tr -d '"')
 
 setup_service google-chrome-stable 66.0.3359.139-1
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
+cd ${OKTA_HOME}/${REPO}
 
-export TEST_SUITE_TYPE="junit"
-export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/unit"
-echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
-echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
+setup_service grunt
 
-if ! npm test; then
-  echo "unit failed! Exiting..."
-  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+# Install required dependencies
+npm install -g @okta/ci-update-package
+npm install -g @okta/ci-pkginfo
+
+if ! npm install --no-optional --unsafe-perm; then
+  echo "npm install failed! Exiting..."
+  exit ${FAILED_SETUP}
 fi
 
-exit $PUBLISH_TYPE_AND_RESULT_DIR;
+
+if ! npm run test:e2e; then
+  echo "e2e tests on iOS failed! Exiting..."
+  exit ${FAILURE}
+fi
+
+exit ${SUCCESS}
