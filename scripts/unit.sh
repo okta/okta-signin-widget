6ecafa767aff2643d2a3e546ea382276197fb9b7
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-04-24 15:46:10 -0700

    Adding travis vars

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export SAUCE_USERNAME=okta-qa
+export SAUCE_ACCESS_KEY="$(aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/saucelabs/saucelabs_access_key /dev/stdout)"
 export SAUCE_PLATFORM_NAME="iOS";
 export TRAVIS=true # work-around to run tests on saucelabs instead of chrome
 export TRAVIS_JOB_NUMBER=${TEST_SUITE_ID}
@@ -7,8 +9,6 @@ aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/signinwidget/test
 
 pip install yq
 
-export SAUCE_USERNAME=$(cat ./test_credentials.yaml | yq .SAUCE_USERNAME | tr -d '"')
-export SAUCE_ACCESS_KEY=$(cat ./test_credentials.yaml | yq .SAUCE_ACCESS_KEY | tr -d '"')
 export WIDGET_TEST_SERVER=$(cat ./test_credentials.yaml | yq .WIDGET_TEST_SERVER | tr -d '"')
 export WIDGET_BASIC_USER=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_USER | tr -d '"')
 export WIDGET_BASIC_PASSWORD=$(cat ./test_credentials.yaml | yq .WIDGET_BASIC_PASSWORD | tr -d '"')
