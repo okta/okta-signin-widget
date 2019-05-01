d988e0154620122bd1a5b48409d54fe0856abced
manueltanzi-okta <manuel.tanzi@okta.com>
2019-05-01 10:15:29 -0700

    chore: Upgrade okta-auth-js in okta-signin-widget 3.0 (#692)

---
+++ test/unit/helpers/xhr/ERROR_VALID_RESPONSE.js
@@ -0,0 +1,5 @@
+define({
+  status: 401,
+  responseType: 'text',
+  response: { errorCode :"E0000004", errorSummary :"Authentication failed", errorLink :"E0000004", errorId :"oaeDtg9knyJR7agwMN-70SYgw", errorCauses:[]}
+});
