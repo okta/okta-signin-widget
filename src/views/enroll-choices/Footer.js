2ca36c774967263c150c25e5ae74ae32e297b7a8
Gowthami Dommety <gowthami.dommety@okta.com>
2019-05-13 12:05:55 -0700

    Adds skip link for required factors during grace period
    Resolves: OKTA-225332

---
+++ src/views/enroll-choices/Footer.js
@@ -0,0 +1,31 @@
+/*!
+ * Copyright (c) 2019, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+define(['okta'], function (Okta) {
+
+  return Okta.View.extend({
+    template: 
+      `<a href="#" class="link goto js-skip" data-se="skip-link">
+          {{i18n code="enroll.choices.setup.skip" bundle="login"}}
+       </a>`,
+    className: 'auth-footer clearfix',
+    events: {
+      'click .js-skip' : function (e) {
+        e.preventDefault();
+        this.model.doTransaction(function (transaction) {
+          return transaction.skip();
+        });
+      }
+    }
+  });
+
+});
