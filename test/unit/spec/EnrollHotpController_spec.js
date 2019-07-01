c3c0bb8d0166ffba1be24d8ba2faead1f2c80b97
Mauricio Castillo <mauricio.castillosilva@okta.com>
2019-07-01 09:35:48 -0700

    rebased latest master and then changed hotpcontroller test for back link

--- test/unit/spec/EnrollHotpController_spec.js
+++ test/unit/spec/EnrollHotpController_spec.js
@@ -20,23 +20,25 @@ function (Okta,
   Router,
   resEnrollAllFactors) {
 
+  const { _, $ } = Okta;
   const itp = Expect.itp;
   const tick = Expect.tick;
 
   Expect.describe('EnrollHotp', function () {
 
-    function setup () {
+    function setup (settings) {
+      settings || (settings = {});
       const setNextResponse = Util.mockAjax();
       const baseUrl = 'https://foo.com';
       const authClient = new OktaAuth({url: baseUrl});
       const successSpy = jasmine.createSpy('success');
       const afterErrorHandler = jasmine.createSpy('afterErrorHandler');
-      const router = new Router({
+      const router = new Router(_.extend({
         el: $sandbox,
         baseUrl: baseUrl,
         authClient: authClient,
         globalSuccessFn: successSpy
-      });
+      }, settings));
       router.on('afterError', afterErrorHandler);
       Util.registerRouter(router);
       Util.mockRouterNavigate(router);
@@ -60,6 +62,18 @@ function (Okta,
         });
     }
 
+    function setupWithCustomBackLink () {
+      return setup({
+        'features.showCustomizableBackLinkInMFA': true,
+        customizableBackLinkInMFA: {
+          label: 'Custom Back Link',
+          fn: function (e) {
+            $(e.target).addClass('test-back-link-class');
+          },
+        }
+      });
+    }
+
     Expect.describe('Header & Footer', function () {
       itp('displays the correct factorBeacon', function () {
         return setup().then(function (test) {
@@ -70,6 +84,16 @@ function (Okta,
       itp('has a "back" link in the footer', function () {
         return setup().then(function (test) {
           Expect.isVisible(test.form.backLink());
+          expect(test.form.backLink().text().trim()).toBe('Back to factor list');
+        });
+      });
+      itp('shows custom back link if features.showCustomizableBackLinkInMFA is true', function () {
+        return setupWithCustomBackLink().then(function (test) {
+          expect(test.form.backLink().length).toBe(1);
+          expect(test.form.backLink().text().trim()).toBe('Custom Back Link');
+          expect(test.form.backLink().hasClass('test-back-link-class')).toBe(false);
+          test.form.backLink().click();
+          expect(test.form.backLink().hasClass('test-back-link-class')).toBe(true);
         });
       });
     });
