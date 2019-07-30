fd90b3722da12576ad2c4d5ff0cbbcffcc3823f7
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-07-30 09:29:42 -0700

    Testing lint failures

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,9 +3,11 @@
 # if this build was triggered as cron job, run e2e tests on iOS
 if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] ; then
     export SAUCE_PLATFORM_NAME="iOS";
+    set -e
     yarn build:release
     yarn test:e2e
 else
+    set -e
     yarn build:release
     yarn lint
     yarn test
