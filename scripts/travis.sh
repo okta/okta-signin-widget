52576d6839a75003c72575e47a1d583ee2d78a55
Vijet M <vijet.mahabaleshwar@okta.com>
2019-07-30 12:32:24 -0700

    Fixes travis script to fail fast (#777)
    * Testing lint failures
    
    * Using travis matrix to run individual test suites
    
    * Removes build:release from test suites

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,11 +3,7 @@
 # if this build was triggered as cron job, run e2e tests on iOS
 if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] ; then
     export SAUCE_PLATFORM_NAME="iOS";
-    yarn build:release
     yarn test:e2e
 else
-    yarn build:release
-    yarn lint
-    yarn test
-    yarn test:e2e
+    yarn $TEST_SUITE
 fi
