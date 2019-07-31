45fad4dde275caf25dbc7a393b39c8ce719cd128
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2019-07-31 13:13:34 -0700

    temp lint fix for test

--- test/e2e/specs/dev_spec.js
+++ test/e2e/specs/dev_spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /*!
  * Copyright (c) 2015-Present, Okta, Inc. and/or its affiliates. All rights reserved.
  * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
