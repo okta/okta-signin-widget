5beccb5615f3ac10f84b481fa6508b3414585599
nikhilvenkatraman-okta <nikhil.venkatraman@okta.com>
2019-08-29 17:38:36 -0700

    Add support for success state (#855)
    - Resolves OKTA-242819

--- playground/mocks/api/v1/idx/introspect/index.js
+++ playground/mocks/api/v1/idx/introspect/index.js
@@ -3,8 +3,9 @@ const data1 = require('./data/identify.json');
 //const data1 = require('../data/factor-enroll-password.json');
 // const data1 = require('../data/factor-enroll-options.json');
 // const data1 = require('../data/factor-verification-email.json');
-// const data1 = require('../data/terminal-email-return.json');
+//const data1 = require('../data/terminal-email-return.json');
 // const data1 = require('../data/terminal-email-transfered.json');
+//const data1 = require('../data/success.json');
 
 const path = __dirname.slice(__dirname.indexOf('api') - 1);
 
