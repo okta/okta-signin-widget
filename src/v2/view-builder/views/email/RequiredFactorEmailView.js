85895d697fd5668044a41d5d4c70ecaeaf0ca0cc
nikhilvenkatraman-okta <nikhil.venkatraman@okta.com>
2019-09-10 11:22:01 -0700

    API integration for email/password factor (#864)
    * API integration for email/password factor
    Resolves: OKTA-245296, OKTA-245295

--- src/v2/view-builder/views/email/RequiredFactorEmailView.js
+++ src/v2/view-builder/views/email/RequiredFactorEmailView.js
@@ -1,32 +1,90 @@
+import { loc, View, createCallout, createButton, _ } from 'okta';
 import BaseForm from '../../internals/BaseForm';
-import BaseFooter from '../../internals/BaseFooter';
 import email from '../shared/email';
+import polling from '../shared/polling';
 import BaseFactorView from '../shared/BaseFactorView';
 
-const Body = BaseForm.extend(Object.assign(
-  {
-    save: 'Send Email Link',
-  },
-  email,
-));
+const SHOW_RESEND_TIMEOUT = 60000;
 
-const Footer = BaseFooter.extend({
-  links: function () {
-    var links = [];
-    // check if we have a select-factor form in remediation, if so add a link
-    if (this.options.appState.hasRemediationForm('select-factor')) {
-      links.push({
-        'type': 'link',
-        'label': 'Switch Factor',
-        'name': 'switchFactor',
-        'formName': 'select-factor',
-      });
+const ResendView = View.extend(
+  {
+  // by default is hide after one minute polling
+    className: 'hide resend-email-view',
+    initialize () {
+      this.add(createCallout({
+        subtitle: 'Haven\'t received an email? To try again, click "Resend Email"',
+        type: 'warning',
+      }));
+      this.add(createButton({
+        className: 'button',
+        title: 'Resend Email',
+        click () {
+          this.options.appState.trigger('invokeAction', 'factor.resend');
+        }
+      }));
+    },
+
+    postRender () {
+      this.showMeTimeout = _.delay(() => {
+        this.$el.removeClass('hide');
+      }, SHOW_RESEND_TIMEOUT);
+
+      // `clearfix` has different style in SIW than Courage
+      // and it hide anything from `:before` node hence remove
+      // `clearfix` as workaround rather change existing style
+      // just to be extreme safe.
+      this.$el.find('.infobox').removeClass('clearfix');
+    },
+
+    remove () {
+      View.prototype.remove.apply(this, arguments);
+      clearTimeout(this.showMeTimeout);
     }
-    return links;
-  }
-});
+  },
+);
+
+const Body = BaseForm.extend(Object.assign(
+
+  {
+    save: loc('mfa.challenge.verify', 'login'),
+
+    subtitle:
+    'An email has been sent to you. Please click the link in your email or enter the code from that email below.',
+
+    saveForm () {
+      BaseForm.prototype.saveForm.apply(this, arguments);
+      this.stopPolling();
+      // TODO: abort ongoing request. (https://oktainc.atlassian.net/browse/OKTA-244134)
+    },
+
+    sendEmailLink () {
+      // auto send email magic link after page has rendered.
+      _.delay(_.bind(function () {
+        this.options.appState.trigger('invokeAction', 'factor.send');
+      }, this), 300);
+    },
+
+    postRender () {
+      this.add(ResendView, {
+        selector: '.okta-form-subtitle',
+        prepend: true,
+      });
+
+      this.startPolling();
+
+      this.sendEmailLink();
+    },
+
+    remove () {
+      BaseForm.prototype.remove.apply(this, arguments);
+      this.stopPolling();
+    }
+  },
+
+  email,
+  polling,
+));
 
 export default BaseFactorView.extend({
   Body,
-  Footer
 });
