b5f8fc4e1fcbc32cd491899768403dbe7dc163b6
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2019-09-12 15:11:56 -0700

    address comments

--- src/v2/view-builder/views/shared/BaseFactorTerminalView.js
+++ src/v2/view-builder/views/shared/BaseFactorTerminalView.js
@@ -1,29 +1,7 @@
 import BaseFactorView from './BaseFactorView';
-import BaseForm from '../../internals/BaseForm';
-import BaseFooter from '../../internals/BaseFooter';
-
-const TerminalBody = BaseForm.extend({
-  title () {
-    const msg = this.options.appState.get('terminal').message || {};
-    return msg.value || 'You can close this window';
-  },
-  noButtonBar: true,
-});
-
-const TerminalFooter = BaseFooter.extend({
-  links () {
-    return [
-      {
-        'type': 'link',
-        'label': 'Back to sign in',
-        'name': 'back',
-        'href': '/'
-      }
-    ];
-  }
-});
+import TerminalView from '../TerminalView';
 
 export default BaseFactorView.extend({
-  Body: TerminalBody,
-  Footer: TerminalFooter,
+  Body: TerminalView.prototype.Body,
+  Footer: TerminalView.prototype.Footer,
 });
