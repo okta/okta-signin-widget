62e7a13c422712eab82c10ac5a2206d9e43785e2
Haisheng Wu <haisheng.wu@okta.com>
2019-11-11 19:47:17 -0800

    fix: fix the button disable style (#954)
    OKTA-260287

--- playground/mocks/api/v1/authn/index.js
+++ playground/mocks/api/v1/authn/index.js
@@ -1,4 +1,6 @@
-const data1 = require('./data/success-001.json');
+// const data1 = require('./data/success-001.json');
+// const data1 = require('./data/mfa-required-all');
+const data1 = require('./data/recovery-challenge-sms-pwd');
 
 module.exports = {
   path: '/api/v1/authn',
