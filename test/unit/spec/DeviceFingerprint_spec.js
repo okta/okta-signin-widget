bc1c273cb45c8fc0e2aae219239521408a8d471b
Haisheng Wu <haisheng.wu@okta.com>
2019-12-11 20:15:52 -0800

    test: fix remove tick in the test OKTA-266331

--- test/unit/spec/DeviceFingerprint_spec.js
+++ test/unit/spec/DeviceFingerprint_spec.js
@@ -2,8 +2,7 @@ define([
   'helpers/util/Expect',
   'sandbox',
   'util/DeviceFingerprint'
-],
-function (Expect, $sandbox, DeviceFingerprint) {
+], function (Expect, $sandbox, DeviceFingerprint) {
 
   Expect.describe('DeviceFingerprint', function () {
 
@@ -12,6 +11,13 @@ function (Expect, $sandbox, DeviceFingerprint) {
         type: 'FingerprintAvailable',
         fingerprint: 'thisIsTheFingerprint'
       } : errorMessage;
+      window.postMessage(JSON.stringify(message), '*');
+    }
+
+    function mockIFrameMessagesFingerprintServiceReady () {
+      var message = {
+        type: 'FingerprintServiceReady',
+      };
       window.postMessage(JSON.stringify(message), '*');
     }
 
@@ -42,85 +48,126 @@ function (Expect, $sandbox, DeviceFingerprint) {
     });
 
     it('returns a fingerprint if the communication with the iframe is successfull', function (done) {
-      mockIFrameMessages(true);
       bypassMessageSourceCheck();
       DeviceFingerprint.generateDeviceFingerprint(baseUrl, $sandbox)
         .then(function (fingerprint) {
+          var $iFrame = $sandbox.find('iframe');
+          expect($iFrame).not.toExist();
           expect(fingerprint).toBe('thisIsTheFingerprint');
           done();
         })
         .fail(function (reason) {
           done.fail('Fingerprint promise incorrectly failed. ' + reason);
         });
+
+      mockIFrameMessages(true);
+    });
+
+    it('notifies iframe if the message type is "finger print ready"', function (done) {
+      bypassMessageSourceCheck();
+      DeviceFingerprint.generateDeviceFingerprint(baseUrl, $sandbox);
+
+      $sandbox.find('iframe')[0].contentWindow.addEventListener(
+        'message',
+        (event) => {
+          expect(JSON.parse(event.data)).toEqual({
+            type: 'GetFingerprint'
+          });
+          expect(event.origin).toBe(baseUrl);
+
+          var $iFrame = $sandbox.find('iframe');
+          expect($iFrame).toExist();
+          done();
+        },
+        false
+      );
+
+      mockIFrameMessagesFingerprintServiceReady();
     });
 
     it('fails if there is a problem with communicating with the iframe', function (done) {
-      mockIFrameMessages(false, null);
       bypassMessageSourceCheck();
       DeviceFingerprint.generateDeviceFingerprint(baseUrl, $sandbox)
         .then(function () {
           done.fail('Fingerprint promise should have been rejected');
         })
         .fail(function (reason) {
+          var $iFrame = $sandbox.find('iframe');
+          expect($iFrame).not.toExist();
           expect(reason).not.toBeUndefined();
           done();
         });
+      mockIFrameMessages(false);
     });
 
     it('fails if there iframe sends and invalid message content', function (done) {
-      mockIFrameMessages(false, { type: 'InvalidMessageType' });
       bypassMessageSourceCheck();
       DeviceFingerprint.generateDeviceFingerprint(baseUrl, $sandbox)
         .then(function () {
           done.fail('Fingerprint promise should have been rejected');
         })
         .fail(function (reason) {
-          expect(reason).not.toBeUndefined();
+          var $iFrame = $sandbox.find('iframe');
+          expect($iFrame).not.toExist();
+          expect(reason).toBe('no data');
           done();
         });
+
+      mockIFrameMessages(false, { type: 'InvalidMessageType' });
     });
 
     it('fails if user agent is not defined', function (done) {
-      mockUserAgent(undefined);
-      mockIFrameMessages(true);
+      mockUserAgent();
       DeviceFingerprint.generateDeviceFingerprint(baseUrl, $sandbox)
         .then(function () {
           done.fail('Fingerprint promise should have been rejected');
         })
         .fail(function (reason) {
+          var $iFrame = $sandbox.find('iframe');
+          expect($iFrame).not.toExist();
           expect(reason).toBe('user agent is not defined');
           done();
         });
+      mockIFrameMessages(true);
     });
 
     it('fails if it is called from a Windows phone', function (done) {
       mockUserAgent('Windows Phone');
-      mockIFrameMessages(true);
       DeviceFingerprint.generateDeviceFingerprint(baseUrl, $sandbox)
         .then(function () {
           done.fail('Fingerprint promise should have been rejected');
         })
         .fail(function (reason) {
+          var $iFrame = $sandbox.find('iframe');
+          expect($iFrame).not.toExist();
           expect(reason).toBe('device fingerprint is not supported on Windows phones');
           done();
         });
+
+      mockIFrameMessages(true);
     });
 
     it('ignores if message is not from right iframe', function (done) {
-      mockIFrameMessages(true);
-      DeviceFingerprint.generateDeviceFingerprint(baseUrl, $sandbox)
-        .then(function () {
-          done.fail('Fingerprint promise should not have been resolved');
-        })
-        .fail(function (reason) {
-          done.fail('Fingerprint promise should not have been rejected. ' +  reason);
-        });
-      Expect.tick().then(function () {
-        // give it time to check if promise resolves or rejects.
+      spyOn(DeviceFingerprint, 'isMessageFromCorrectSource').and.callFake(() => {
+        return false;
+      });
+      DeviceFingerprint.generateDeviceFingerprint(baseUrl, $sandbox);
+      expect(DeviceFingerprint.isMessageFromCorrectSource).not.toHaveBeenCalled();
+
+      window.addEventListener('message', () => {
+        expect(DeviceFingerprint.isMessageFromCorrectSource).toHaveBeenCalled();
+
+        // either resolve or reject, the iframe will be removed.
+        // verify the exists of iframe implies promise is
+        // neither resolved or rejected
         var $iFrame = $sandbox.find('iframe');
         expect($iFrame).toExist();
+        expect($iFrame.attr('src')).toBe(baseUrl + '/auth/services/devicefingerprint');
+        expect($iFrame.is(':visible')).toBe(false);
         done();
       });
+
+      mockIFrameMessages(true);
     });
 
   });
