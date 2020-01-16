ceeb5eac9d2ca2ba17493699c257654ed79979e0
Haisheng Wu <haisheng.wu@okta.com>
2020-01-16 13:16:32 -0800

    chore: update commitlint max header length to 80
    OKTA-272441

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'references-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 80]
   },
   parserPreset: {
     parserOpts: {
