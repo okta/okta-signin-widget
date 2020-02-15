dbdadc428e958657ea0842c0bccff8939bbadb35
Jintai He <jhe@okta.com>
2020-02-14 18:20:26 -0800

    fix: add comment
    OKTA-2777722

--- src/v2/view-builder/views/SSOExtensionView.js
+++ src/v2/view-builder/views/SSOExtensionView.js
@@ -2,6 +2,11 @@ import BaseView from '../internals/BaseView';
 import BaseForm from '../internals/BaseForm';
 import Util from '../../../util/Util';
 
+// for BETA,
+// redirect is needed for Apple SSO Extension to intercept the request, because
+// - XHR request is not interceptable
+// - form post is interceptable, but some app (Outlook) closes the app after
+// We may have a different approach/UX for EA
 const Body = BaseForm.extend({
   noButtonBar: true,
 
