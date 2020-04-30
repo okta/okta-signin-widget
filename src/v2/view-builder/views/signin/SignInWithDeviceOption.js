d808314b0c8a1413d06fe6414a6573f2ea92b30a
Jintai He <jhe@okta.com>
2020-04-30 14:13:41 -0700

    feat: test reducing universal link clicks     OKTA-289825

--- src/v2/view-builder/views/signin/SignInWithDeviceOption.js
+++ src/v2/view-builder/views/signin/SignInWithDeviceOption.js
@@ -1,4 +1,5 @@
-import { View, createButton, loc } from 'okta';
+import { View, createButton, loc, $ } from 'okta';
+// import { fetchRequest } from '../../ion/httpClient';
 import hbs from 'handlebars-inline-precompile';
 
 export default View.extend({
@@ -13,7 +14,33 @@ export default View.extend({
       className: 'button',
       title: loc('oktaVerify.button', 'login'),
       click () {
-        appState.trigger('invokeAction', 'launch-authenticator');
+        // appState.trigger('invokeAction', 'launch-authenticator');
+        const rem = this.options.appState.get('rawIdxState').remediation.value
+        .filter(v => v.name === 'launch-authenticator')[0];
+        // fetchRequest(
+        //   rem.href,
+        //   rem.method,
+        //   { stateHandle: rem.value[0].value }
+        // )
+        // .then((resp) => {
+        //   const response = resp.response;
+        //   const deviceChallenge = response[
+        //     resp.response.remediation.value.filter(v => v.name === 'device-challenge-poll')[0].relatesTo
+        //   ];
+        //   Util.redirect(deviceChallenge.value.href);
+        // });
+        $.ajax({
+          url: rem.href,
+          method: rem.method,
+          contentType: 'application/json',
+          data: JSON.stringify({ stateHandle: rem.value[0].value }),
+        })
+        .then((resp) => {
+          const deviceChallenge = resp[
+            resp.remediation.value.filter(v => v.name === 'device-challenge-poll')[0].relatesTo
+          ];
+          Util.redirect(deviceChallenge.value.href);
+        });
       }
     }), '.okta-verify-container');
   }
