ab5b62a82cd60846ec09561b020bb84081c4faa2
James Bui <james.bui@okta.com>
2020-05-07 17:24:44 -0700

    ci: migrate publish script to npm-topic/npm-release
    resolves okta-305688

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Install required node version
+export REGISTRY_REPO="npm-topic"
+export REGISTRY="${ARTIFACTORY_URL}/api/npm/${REGISTRY_REPO}"
 setup_service node v12.13.0
 # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
 setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
