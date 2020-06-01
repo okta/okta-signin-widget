2521d8dcb68b1f094498e44bf6102fbb2d6c8092
santhoshbalakrishnan <santhosh.balakrishnan@okta.com>
2020-06-01 13:40:50 -0700

    feat: remove password requirements from error message for HTML desc (#1201) Resolves: OKTA-300832

--- test/unit/helpers/xhr/PASSWORD_RESET_error.js
+++ test/unit/helpers/xhr/PASSWORD_RESET_error.js
@@ -7,7 +7,7 @@ define({
     "errorLink": "E0000080",
     "errorId": "oaeZL71b-kLQyae-eG7rzghzQ",
     "errorCauses": [{
-      "errorSummary": "Password requirements were not met. Password requirements: at least 8 characters, a lowercase letter, an uppercase letter, a number, no parts of your username, does not include your first name, does not include your last name."
+      "errorSummary": "Password requirements were not met. Password requirements: at least 8 characters, a lowercase letter, an uppercase letter, a number, a symbol, no parts of your username, does not include your first name, does not include your last name."
     }]
   }
 });
