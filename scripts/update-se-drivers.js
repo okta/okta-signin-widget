4296af79af4ff35c0f3825573c19d76551372f3b
Vijet M <vijet.mahabaleshwar@okta.com>
2020-06-10 15:01:06 -0700

    test: adds support to update chromedriver dynamically based on chrome version (#1226)
    * test: adds support to update chromedriver dynamically based on chrome version
    
    resolves: okta-299731

---
+++ scripts/update-se-drivers.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const { execSync } = require('child_process');
+
+function getOS() {
+  var os = process.platform;
+  if (os == "darwin") {
+    os = "MacOS";
+  } else if (os == "win32" || os == "win64") {
+    os = "Windows";
+  } else if (os == "linux") {
+    os = "Linux";
+  }
+  return os;
+}
+
+const os = getOS();
+console.log(`Operating System - ${os}`);
+
+let chromeVersion;
+if (os === 'MacOS') {
+  chromeVersionString = execSync(`/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --version`) + '';
+  // Get the major and minor version of the chrome version using regex (1 or more digits followed by a dot followed by 1 or more digits)
+  chromeVersion = chromeVersionString.match(/(\d+(\.\d+)?)/)[0]; 
+} else {
+  chromeVersion = execSync(`google-chrome --product-version`) + '';
+}
+
+const chromeMajorVersion = chromeVersion.split(".")[0];
+console.log(`Chrome Major Version - ${chromeMajorVersion}`);
+
+const chromeDriverUrl = `https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${chromeMajorVersion}`;
+
+axios.get(chromeDriverUrl).then(response => {
+  const chromeDriverVersion = response.data;
+  console.log(`Chrome Driver Version - ${chromeDriverVersion}`);
+
+  execSync(`${__dirname}/../node_modules/protractor/bin/webdriver-manager update --versions.chrome ${chromeDriverVersion} --gecko false --versions.standalone latest`);
+}).catch(err => {
+  console.log(err);
+});
