faf5b30ac489a096d84e479802b9226ba94bd8ea
Ani Pendakur <Ani.pendakur@okta.com>
2020-06-10 17:51:40 -0700

    fix: enhanced tests
    OKTA-300141

--- test/testcafe/spec/EnrollAuthenticatorPhone_spec.js
+++ test/testcafe/spec/EnrollAuthenticatorPhone_spec.js
@@ -2,6 +2,7 @@ import { RequestMock } from 'testcafe';
 import EnrollAuthenticatorPhoneObject from '../framework/page-objects/EnrollAuthenticatorPhoneObject';
 import SuccessPageObject from '../framework/page-objects/SuccessPageObject';
 import xhrAuthenticatorEnrollPhone from '../../../playground/mocks/data/idp/idx/authenticator-enroll-phone';
+import xhrAuthenticatorEnrollPhoneVoice from '../../../playground/mocks/data/idp/idx/authenticator-enroll-phone-voice';
 import xhrSuccess from '../../../playground/mocks/data/idp/idx/success';
 
 const mock = RequestMock()
@@ -10,8 +11,13 @@ const mock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/challenge')
   .respond(xhrSuccess);
 
-fixture(`Factor Enroll Phone`)
-  .requestHooks(mock);
+const voiceOnlyOptionMock = RequestMock()
+  .onRequestTo('http://localhost:3000/idp/idx/introspect')
+  .respond(xhrAuthenticatorEnrollPhoneVoice)
+  .onRequestTo('http://localhost:3000/idp/idx/challenge')
+  .respond(xhrSuccess);
+
+fixture(`Factor Enroll Phone`);
 
 async function setup(t) {
   const enrollPhonePage = new EnrollAuthenticatorPhoneObject(t);
@@ -19,7 +25,7 @@ async function setup(t) {
   return enrollPhonePage;
 }
 
-test(`default sms mode`, async t => {
+test.requestHooks(mock)(`default sms mode`, async t => {
   const enrollPhonePage = await setup(t);
 
   // Check title
@@ -32,7 +38,7 @@ test(`default sms mode`, async t => {
   await t.expect(enrollPhonePage.extensionIsHidden()).eql(true);
 });
 
-test(`voice mode click and extension will get shown`, async t => {
+test.requestHooks(mock)(`voice mode click and extension will get shown`, async t => {
   const enrollPhonePage = await setup(t);
 
   // Switch to Voice
@@ -52,7 +58,7 @@ test(`voice mode click and extension will get shown`, async t => {
   await t.expect(enrollPhonePage.phoneNumberFieldIsSmall()).eql(true);
 });
 
-test(`phone number is required`, async t => {
+test.requestHooks(mock)(`phone number is required`, async t => {
   const enrollPhonePage = await setup(t);
   // fields are required
   await t.expect(enrollPhonePage.hasPhoneNumberError()).eql(false);
@@ -61,7 +67,7 @@ test(`phone number is required`, async t => {
   await t.expect(enrollPhonePage.hasPhoneNumberError()).eql(true);
 });
 
-test(`should succeed when values are filled in sms mode`, async t => {
+test.requestHooks(mock)(`should succeed when values are filled in sms mode`, async t => {
   const enrollPhonePage = await setup(t);
   const successPage = new SuccessPageObject(t);
 
@@ -73,10 +79,38 @@ test(`should succeed when values are filled in sms mode`, async t => {
     .eql('http://localhost:3000/app/UserHome?stateToken=mockedStateToken123');
 });
 
-test(`should succeed when values are filled in voice mode`, async t => {
+test.requestHooks(mock)(`should succeed when values are filled in voice mode`, async t => {
   const enrollPhonePage = await setup(t);
   const successPage = new SuccessPageObject(t);
   await enrollPhonePage.clickRadio();
+  await enrollPhonePage.fillPhoneNumber('4156669999');
+  await enrollPhonePage.clickSaveButton();
+
+  const pageUrl = await successPage.getPageUrl();
+  await t.expect(pageUrl)
+    .eql('http://localhost:3000/app/UserHome?stateToken=mockedStateToken123');
+});
+
+
+/* Voice only option mocks */
+test.requestHooks(voiceOnlyOptionMock)(`default is voice mode`, async t => {
+  const enrollPhonePage = await setup(t);
+
+  // Check title
+  await t.expect(enrollPhonePage.getFormTitle()).eql('Set up phone authentication');
+  await t.expect(enrollPhonePage.getFormSubtitle())
+    .eql('Set up verification with a phone number. You will receive a code sent to your phone');
+  await t.expect(enrollPhonePage.getSaveButtonLabel()).eql('Send a code via voice call');
+
+  // Extension field is not hidden
+  await t.expect(enrollPhonePage.extensionIsHidden()).eql(false);
+  // Phone field is rendered small
+  await t.expect(enrollPhonePage.phoneNumberFieldIsSmall()).eql(true);
+});
+
+test.requestHooks(voiceOnlyOptionMock)(`should succeed when values are filled when in voice only mode`, async t => {
+  const enrollPhonePage = await setup(t);
+  const successPage = new SuccessPageObject(t);
   await enrollPhonePage.fillPhoneNumber('4156669999');
   await enrollPhonePage.clickSaveButton();
 
