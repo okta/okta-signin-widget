be5a71470de17f0d627818e75fdb1b3f336253ed
nikhilvenkatraman-okta <nikhil.venkatraman@okta.com>
2020-06-12 12:57:02 -0700

    feat: adds support for optional enrollment (#1223)
    feat: adds support for optional enrollment
    
    Resolves: OKTA-296673

--- test/unit/spec/v2/view-builder/internals/FormInputFactory_spec.js
+++ test/unit/spec/v2/view-builder/internals/FormInputFactory_spec.js
@@ -1,6 +1,8 @@
 import { Collection } from 'okta';
-import FactorEnrollOptions from 'v2/view-builder/components/FactorOptions';
 import FormInputFactory from 'v2/view-builder/internals/FormInputFactory';
+import FactorEnrollOptions from 'v2/view-builder/components/FactorOptions';
+import AuthenticatorVerifyOptions from 'v2/view-builder/components/AuthenticatorVerifyOptions';
+import AuthenticatorEnrollOptions from 'v2/view-builder/components/AuthenticatorEnrollOptions';
 
 describe('v2/view-builder/internals/FormInputFactory', function () {
   it('handles factorType type', function () {
@@ -63,9 +65,9 @@ describe('v2/view-builder/internals/FormInputFactory', function () {
     });
   });
 
-  it('handles authenticatorSelect type', function () {
+  it('handles authenticatorVerify Select type', function () {
     const opt = {
-      type: 'authenticatorSelect',
+      type: 'authenticatorVerifySelect',
       options: [
         {
           'label': 'Okta Password',
@@ -86,7 +88,82 @@ describe('v2/view-builder/internals/FormInputFactory', function () {
     };
     const result = FormInputFactory.create(opt);
     expect(result).toEqual({
-      View: FactorEnrollOptions,
+      View: AuthenticatorVerifyOptions,
+      options: {
+        collection: jasmine.anything(),
+        name: 'authenticator'
+      },
+    });
+    expect(result.options.collection instanceof Collection).toBe(true);
+    expect(result.options.collection.toJSON()).toEqual([
+      {
+        'label': 'Okta Password',
+        'authenticatorType': 'password',
+        'value': {
+          id: 'autwa6eD9o02iBbtv0g3'
+        },
+        'iconClassName': 'mfa-okta-password',
+        'description': ''
+      },
+      {
+        'label': 'Okta E-mail',
+        'authenticatorType': 'email',
+        'value': {
+          id: 'autwa6eDxxx2iBbtv0g3'
+        },
+        'iconClassName': 'mfa-okta-email',
+        'description': ''
+      }
+    ]);
+    // make sure input parameter is not mutated.
+    expect(opt).toEqual({
+      type: 'authenticatorVerifySelect',
+      options: [
+        {
+          'label': 'Okta Password',
+          'authenticatorType': 'password',
+          'value': {
+            id: 'autwa6eD9o02iBbtv0g3'
+          }
+        },
+        {
+          'label': 'Okta E-mail',
+          'authenticatorType': 'email',
+          'value': {
+            id: 'autwa6eDxxx2iBbtv0g3'
+          }
+        }
+      ],
+      name: 'authenticator'
+    });
+  });
+
+
+
+  it('handles authenticatorEnrollSelect type', function () {
+    const opt = {
+      type: 'authenticatorEnrollSelect',
+      options: [
+        {
+          'label': 'Okta Password',
+          'authenticatorType': 'password',
+          'value': {
+            id: 'autwa6eD9o02iBbtv0g3'
+          }
+        },
+        {
+          'label': 'Okta E-mail',
+          'authenticatorType': 'email',
+          'value': {
+            id: 'autwa6eDxxx2iBbtv0g3'
+          }
+        }
+      ],
+      name: 'authenticator'
+    };
+    const result = FormInputFactory.create(opt);
+    expect(result).toEqual({
+      View: AuthenticatorEnrollOptions,
       options: {
         collection: jasmine.anything(),
         name: 'authenticator'
@@ -115,7 +192,7 @@ describe('v2/view-builder/internals/FormInputFactory', function () {
     ]);
     // make sure input parameter is not mutated.
     expect(opt).toEqual({
-      type: 'authenticatorSelect',
+      type: 'authenticatorEnrollSelect',
       options: [
         {
           'label': 'Okta Password',
