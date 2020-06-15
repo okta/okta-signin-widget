b8d3cdba6833fb42962a1ad3ba195bb0ea0bd05a
Haisheng Wu <haisheng.wu@okta.com>
2020-06-15 10:22:14 -0700

    chore: address comment
    - add comment to code to explain the label override.
    
    okta-123

--- src/v2/view-builder/views/security-question/ChallengeAuthenticatorSecurityQuestion.js
+++ src/v2/view-builder/views/security-question/ChallengeAuthenticatorSecurityQuestion.js
@@ -16,6 +16,12 @@ const Body = BaseForm.extend({
     const questionKey = uiSchemas.filter(s => s.name.indexOf('questionKey') >= 0);
     const answer = uiSchemas.filter(s => s.name.indexOf('answer') >= 0);
 
+    /**
+     * At verify SQ case,
+     * 'questionKey' is read-only hence it won't show up in UI anyway.
+     * 'answer' has label 'answer' but apparently UI want to display the
+     * actual question (label of `questionKey`) as better user experience.
+     */
     if (questionKey.length === 1 && answer.length === 1) {
       answer[0].label = questionKey[0].label;
     }
