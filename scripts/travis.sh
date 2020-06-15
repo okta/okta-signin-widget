e408fcc568bf4a50f258b7eba127b2f213917301
Vijet M <vijet.mahabaleshwar@okta.com>
2020-06-15 13:09:12 -0700

    test: adds support to start saucelabs tunnel via command line (#1232)
    resolves: okta-304168

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,6 +2,7 @@
 
 # if this build was triggered as cron job, run e2e tests on iOS
 if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] ; then
+    sh ./scripts/start-sauce-connect.sh
     DD=$(date +%d)
     # Run iOS tests on even days, android tests on odd
     if [ $((DD%2)) -eq 0 ];
@@ -12,5 +13,8 @@ if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] ; then
     fi
     yarn test:e2e
 else
+    if [[ "$TEST_SUITE" == *"e2e:windows"* ]] ; then
+        sh ./scripts/start-sauce-connect.sh
+    fi
     yarn $TEST_SUITE
 fi
