d3754d78e5ab2790b2c8a210e4eb5e71edd79174
anipendakur-okta <62976351+anipendakur-okta@users.noreply.github.com>
2020-06-18 11:40:40 -0700

    feat: password enroll (#1240)
    * feat: password enroll in m1
    
    Integration for password enroll and tweaking of labels
    
    OKTA-296702
    
    Co-authored-by: releng-internal-okta <eng_releng_internal@okta.com>
    Co-authored-by: Haisheng Wu <haisheng.wu@okta.com>
    Co-authored-by: Aaron Granick <aaron.granick@okta.com>
    Co-authored-by: Vijet M <vijet.mahabaleshwar@okta.com>

---
+++ src/v2/view-builder/views/password/EnrollAuthenticatorPasswordView.js
@@ -0,0 +1,58 @@
+import { loc } from 'okta';
+import BaseView from '../../internals/BaseView';
+import BaseForm from '../../internals/BaseForm';
+import BaseFactorView from '../shared/BaseFactorView';
+
+const Body = BaseForm.extend({
+  title () {
+    return loc('oie.password.enroll.title', 'login');
+  },
+  save () {
+    return loc('oie.next.button', 'login');
+  },
+
+  getUISchema () {
+    const uiSchemas = BaseForm.prototype.getUISchema.apply(this, arguments);
+    return uiSchemas.concat([
+      {
+        name: 'confirmPassword',
+        label: loc('oie.password.confirmPasswordLabel','login'),
+        type: 'password',
+        'label-top': true,
+        params: {
+          showPasswordToggle: true
+        }
+      }
+    ]);
+  }
+});
+
+export default BaseFactorView.extend({
+
+  Body,
+
+  createModelClass () {
+    const ModelClass = BaseView.prototype.createModelClass.apply(this, arguments);
+    const local = Object.assign(
+      {
+        confirmPassword: {
+          type: 'string',
+          required: true,
+        }
+      },
+      ModelClass.prototype.local,
+    );
+    return ModelClass.extend({
+      local,
+      validate () {
+        if (this.get('credentials.passcode') !== this.get('confirmPassword') &&
+          this.get('credential.value') !== this.get('confirmPassword')) {
+          const errors = {
+            'confirmPassword': loc('oie.password.match.error', 'login'),
+          };
+          return errors;
+        }
+      }
+    });
+  }
+});
