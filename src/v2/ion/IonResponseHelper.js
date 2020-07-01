7ff608475ab480f92ded40cdaf7e0ff0b0246efc
Haisheng Wu <haisheng.wu@okta.com>
2020-07-01 08:23:57 -0700

    fix: fix error message conversion during SQ enrollment
    OKTA-309274

--- src/v2/ion/IonResponseHelper.js
+++ src/v2/ion/IonResponseHelper.js
@@ -1,47 +1,70 @@
 import { _ } from 'okta';
 
-/*
-returns errors
-errors = [
-  {property : fieldName1, errorSummary: [errorMessage1]},
-  {property : fieldName2, errorSummary: [errorMessage1]}
-  {property : fieldName3, errorSummary: [errorMessage1, errorMessage2]}
-]
-*/
-const getRemediationErrors = (res) => {
-  let errors = [];
-  let remediationErrors = res.remediation && 
-    res.remediation.value[0] && 
-    _.omit(res.remediation.value[0].value, (value) => {
-      return (value.visible && !value.visible);
-    });
-
-  if (remediationErrors) {
-    _.each(remediationErrors, (remediationForm) => {
-      if (remediationForm.form && remediationForm.form.value.length) {
-        const formName = remediationForm.name;
-        _.each(remediationForm.form.value, (field) => {
-          if (field.messages && field.messages.value.length) {
-            let cause = {};
-            cause.property = `${formName}.${field.name}`;
-            cause.errorSummary = [];
-            _.each(field.messages.value, (err) => {
-              cause.errorSummary.push(err.message);
-            });
-            errors.push(cause);
-          }
-        });
-      }
-    });
-  }
-
-  return errors;
+const convertErrorMessageToErrorSummary = (maybeFormName, remediationValues = []) => {
+  return _.chain(remediationValues)
+    .filter(field => {
+      return field.messages
+        && Array.isArray(field.messages.value)
+        && field.messages.value.length;
+    })
+    .map(field => {
+      return {
+        property: maybeFormName ? `${maybeFormName}.${field.name}` : field.name,
+        errorSummary: _.map(field.messages.value, (err) => err.message),
+      };
+    })
+    .value();
 };
 
-/*
-  return global error summary combined into one string
-  allErrors = 'error string1. error string2'
-*/
+/**
+ * returns errors
+ * @example
+ * errors = [
+ *   {property : fieldName1, errorSummary: [errorMessage1]},
+ *   {property : fieldName2, errorSummary: [errorMessage1]}
+ *   {property : fieldName3, errorSummary: [errorMessage1, errorMessage2]}
+ * ]
+ */
+const getRemediationErrors = (res) => {
+  let errors = [];
+
+  if (!res.remediation || !Array.isArray(res.remediation.value) || res.remediation.value.length === 0) {
+    return errors;
+  }
+  let remediationFormFields = res.remediation.value[0].value ;
+
+  if (!Array.isArray(remediationFormFields)) {
+    return errors;
+  }
+
+  // error at field
+  errors.push(convertErrorMessageToErrorSummary(null, remediationFormFields));
+
+  _.each(remediationFormFields, (remediationForm) => {
+    const formName = remediationForm.name;
+
+    // error at form.value
+    if (remediationForm.form && Array.isArray(remediationForm.form.value)) {
+      errors.push(convertErrorMessageToErrorSummary(formName, remediationForm.form.value));
+    }
+
+    // error at option.value.form.value
+    if (Array.isArray(remediationForm.options)) {
+      _.each(remediationForm.options, (option) => {
+        if (option.value && option.value.form && Array.isArray(option.value.form.value)) {
+          errors.push(convertErrorMessageToErrorSummary(formName, option.value.form.value));
+        }
+      });
+    }
+  });
+
+  return _.flatten(errors);
+};
+
+/**
+ * return global error summary combined into one string
+ * allErrors = 'error string1. error string2'
+ */
 const getGlobalErrors  = (res) => {
   let allErrors = [];
 
