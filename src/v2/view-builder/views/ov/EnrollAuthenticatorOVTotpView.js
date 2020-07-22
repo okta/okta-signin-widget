0b58aafd1b8ad949a7dae1dc1f5659535369240e
magizh <magizh@okta.com>
2020-07-22 11:21:33 -0700

    removed qr code in enroll

--- src/v2/view-builder/views/ov/EnrollAuthenticatorOVTotpView.js
+++ src/v2/view-builder/views/ov/EnrollAuthenticatorOVTotpView.js
@@ -9,7 +9,6 @@ const Body = BaseForm.extend({
     const uiSchemas = BaseForm.prototype.getUISchema.apply(this, arguments);
     uiSchemas.push({
       View: `
-      <img src="${this.options.appState.get('currentAuthenticator').contextualData.qrcode.href}"></img>
       Totp shared secret - ${this.options.appState.get('currentAuthenticator').contextualData.sharedSecret}
       `,
     });
