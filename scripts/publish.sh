7bc5decd6a1c7d0b71455fd4aafb33a9bb2d7604
jamesbui-okta <james.bui@okta.com>
2020-08-06 18:59:41 +0000

    ci: migrate publish script to npm-topic/npm-release
    OKTA-304272
    
    OKTA-304272
    <<<Jenkins Check-In of Tested SHA: a04eed9a97d5985892ba271cf03038972b231931 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    
    OKTA-320515
    <<<Jenkins Check-In of Tested SHA: ae200258c2999be3f37f98c7680dd62cce0e5f2a for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    
    OKTA-323553
    <<<Jenkins Check-In of Tested SHA: e06f9723f3233bc142a48e2c689eac7acfd06ee6 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,37 +3,23 @@
 source $OKTA_HOME/$REPO/scripts/setup.sh
 
 # Install required dependencies
-yarn global add @okta/ci-update-package
+yarn global add @okta/ci-append-sha
 yarn global add @okta/ci-pkginfo
 
 export PATH="${PATH}:$(yarn global bin)"
 export TEST_SUITE_TYPE="build"
 
-if [ -n "$action_branch" ];
-then
-  echo "Publishing from bacon task using branch $action_branch"
-  TARGET_BRANCH=$action_branch
-else
-  echo "Publishing from bacon testSuite using branch $BRANCH"
-  TARGET_BRANCH=$BRANCH
-fi
-
-if ! ci-update-package --branch ${TARGET_BRANCH}; then
-  echo "ci-update-package failed! Exiting..."
+if ! ci-append-sha; then
+  echo "ci-append-sha failed! Exiting..."
   exit $FAILED_SETUP
 fi
 
 ### Not able to use 'yarn publish' which failed at
 ### publish alpha version.
 ### Didn't figure out root cause but keep using npm.
-if ! npm publish --unsafe-perm; then
+npm config set @okta:registry ${REGISTRY}
+if ! npm publish --registry ${REGISTRY}; then
   echo "npm publish failed! Exiting..."
-  exit $PUBLISH_ARTIFACTORY_FAILURE
-fi
-
-DATALOAD=$(ci-pkginfo -t dataload)
-if ! artifactory_curl -X PUT -u ${ARTIFACTORY_CREDS} ${DATALOAD} -v -f; then
-  echo "artifactory_curl failed! Exiting..."
   exit $PUBLISH_ARTIFACTORY_FAILURE
 fi
 
