521c3da7a13ba35c9d63f2a4e6d5dfcda8803159
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2020-08-09 22:07:45 -0700

    test: adds jasmine reporters to e2e tests
    resolves: okta-318535

--- scripts/e2e-windows.sh
+++ scripts/e2e-windows.sh
@@ -7,6 +7,11 @@ export TRAVIS=true # to make sure right version of sauce tunnel is installed in 
 
 source $OKTA_HOME/$REPO/scripts/setup.sh
 
+export TEST_SUITE_TYPE="junit"
+export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/junit"
+echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
+echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
+
 # This file contains all the env vars we need for e2e tests
 aws s3 --quiet --region us-east-1 cp s3://ci-secret-stash/prod/signinwidget/export-test-credentials.sh $OKTA_HOME/$REPO/scripts/export-test-credentials.sh
 source $OKTA_HOME/$REPO/scripts/export-test-credentials.sh
@@ -15,7 +20,7 @@ sh ./scripts/start-sauce-connect.sh
 
 if ! yarn test:e2e:windows; then
   echo "e2e windows saucelabs tests failed! Exiting..."
-  exit ${FAILURE}
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
-exit ${SUCCESS};
+exit ${PUBLISH_TYPE_AND_RESULT_DIR};
