d795ffadaff70ed9cc61a2fedd6ed8ca6708bb81
Gowthami Dommety <gowthami.dommety@okta.com>
2020-08-14 21:02:11 -0700

    feat: implement resend push for okta verify on idx
    Resolves: OKTA-322821

---
+++ src/v2/view-builder/views/ov/ChallengeOktaVerifyResendPushView.js
@@ -0,0 +1,45 @@
+import { loc, createCallout } from 'okta';
+import BaseForm from '../../internals/BaseForm';
+import BaseAuthenticatorView from '../../components/BaseAuthenticatorView';
+import AuthenticatorVerifyFooter from '../../components/AuthenticatorVerifyFooter';
+
+const Body = BaseForm.extend(Object.assign(
+  {
+    className: 'okta-verify-resend-push',
+
+    title () {
+      return loc('oie.okta_verify.push.title', 'login');
+    },
+
+    save () {
+      return loc('oie.okta_verify.push.send', 'login');
+    },
+
+    showMessages () {
+      // override showMessages to display error in cases like reject push
+      // or timeout. Borrowed this logic from TerminalView.
+      const messagesObjs = this.options.appState.get('messages');
+      if (messagesObjs && Array.isArray(messagesObjs.value)) {
+        this.add('<div class="ion-messages-container"></div>', '.o-form-error-container');
+
+        messagesObjs.value.forEach(messagesObj => {
+          const msg = messagesObj.message;
+          if (messagesObj.class && messagesObj.class === 'ERROR') {
+            this.add(createCallout({
+              content: msg,
+              type: 'error',
+            }), '.o-form-error-container');
+          } else {
+            this.add(`<p>${msg}</p>`, '.ion-messages-container');
+          }
+        });
+
+      }
+    },
+  },
+));
+
+export default BaseAuthenticatorView.extend({
+  Body,
+  Footer: AuthenticatorVerifyFooter,
+});
