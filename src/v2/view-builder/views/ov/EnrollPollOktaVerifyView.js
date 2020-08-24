7ab59d6d763d0be7bf06f5de3100c94e049243ae
magizh <magizh@okta.com>
2020-08-24 16:32:12 -0700

    feat: ov idx enroll enhancements
    * Added resend to email and sms on timeout.
    * Added more options link to email and sms views.
    * Filtered selectedChannel option in select channel form.
    
    * Added select-authenticator-enroll forms to mocks.
    
    RESOLVES: OKTA-320368

--- src/v2/view-builder/views/ov/EnrollPollOktaVerifyView.js
+++ src/v2/view-builder/views/ov/EnrollPollOktaVerifyView.js
@@ -1,13 +1,13 @@
-import { View, loc } from 'okta';
+import { loc } from 'okta';
 import BaseForm from '../../internals/BaseForm';
 import { isAndroid, isIOS } from '../../../../util/BrowserFeatures';
 import BaseAuthenticatorView from '../../components/BaseAuthenticatorView';
 import AuthenticatorEnrollFooter from '../../components/AuthenticatorEnrollFooter';
-import Link from '../../components/Link';
-import { getSwitchOVEnrollChannelLink } from '../../utils/LinksUtil';
 import polling from '../shared/polling';
-import hbs from 'handlebars-inline-precompile';
 import { FORMS as RemediationForms } from '../../../ion/RemediationConstants';
+import ResendView from './ResendView';
+import SwitchEnrollChannelLinkView from './SwitchEnrollChannelLinkView';
+import EnrollChannelPollDescriptionView from './EnrollChannelPollDescriptionView';
 
 const Body = BaseForm.extend(Object.assign(
   {
@@ -41,66 +41,20 @@ const Body = BaseForm.extend(Object.assign(
       const schema = [];
       const contextualData = this.options.appState.get('currentAuthenticator').contextualData;
       const selectedChannel = contextualData.selectedChannel;
-      if (selectedChannel === 'qrcode') {
+      schema.push({
+        View: EnrollChannelPollDescriptionView,
+      });
+      schema.push({
+        View: SwitchEnrollChannelLinkView,
+        options: {
+          selectedChannel,
+        },
+        selector: selectedChannel === 'qrcode' ? '.qrcode-container' : '.switch-channel-content',
+      });
+      if (['email', 'sms'].includes(selectedChannel)) {
         schema.push({
-          View: View.extend({
-            template: hbs`
-              <ol class="qrcode-info ov-info">
-                <li>{{i18n code="oie.enroll.okta_verify.qrcode.step1" bundle="login"}}</li>
-                <li>{{i18n code="oie.enroll.okta_verify.qrcode.step2" bundle="login"}}</li>
-                <li>{{i18n code="oie.enroll.okta_verify.qrcode.step3" bundle="login"}}</li>
-              </ol>
-              <div class="qrcode-container">
-                <img class="qrcode" src={{href}} alt="qr code"></img>
-              </div>
-            `,
-            getTemplateData () {
-              return {
-                href: contextualData.qrcode.href,
-              };
-            },
-          }),
-        });
-      } else if (selectedChannel === 'email') {
-        schema.push({
-          View: View.extend({
-            template: hbs`
-              <ul class="email-info ov-info">
-                <li>{{i18n code="oie.enroll.okta_verify.email.step1" bundle="login" arguments="email"}}</li>
-                <li>{{i18n code="oie.enroll.okta_verify.email.step2" bundle="login"}}</li>
-                <li>{{i18n code="oie.enroll.okta_verify.email.step3" bundle="login"}}</li>
-              </ul>
-            `,
-            getTemplateData () {
-              return {
-                email: contextualData.email,
-              };
-            },
-          }),
-        });
-      }  else if (selectedChannel === 'sms') {
-        schema.push({
-          View: View.extend({
-            template: hbs`
-              <ul class="sms-info ov-info">
-                <li>{{i18n code="oie.enroll.okta_verify.sms.step1" bundle="login" arguments="phoneNumber"}}</li>
-                <li>{{i18n code="oie.enroll.okta_verify.sms.step2" bundle="login"}}</li>
-                <li>{{i18n code="oie.enroll.okta_verify.sms.step3" bundle="login"}}</li>
-              </ul>
-            `,
-            getTemplateData () {
-              return {
-                phoneNumber: contextualData.phoneNumber,
-              };
-            },
-          }),
-        });
-      }
-      if (this.options.appState.get('currentAuthenticator').contextualData.selectedChannel === 'qrcode') {
-        schema.push({
-          View: Link,
-          options: getSwitchOVEnrollChannelLink(this.options.appState),
-          selector: '.qrcode-container',
+          View: ResendView,
+          selector: '.o-form-error-container',
         });
       }
       return schema;
---
+++ src/v2/view-builder/views/ov/ResendView.js
@@ -0,0 +1,45 @@
+import { View, createCallout, _ } from 'okta';
+import hbs from 'handlebars-inline-precompile';
+import { SHOW_RESEND_TIMEOUT } from '../../utils/Constants';
+
+export default View.extend(
+  {
+    //only show after certain threshold of polling
+    className: 'hide resend-ov-link-view',
+    events: {
+      'click a.resend-link' : 'handelResendLink'
+    },
+
+    initialize () {
+      const selectedChannel = this.options.appState.get('currentAuthenticator').contextualData.selectedChannel;
+      this.add(createCallout({
+        content: selectedChannel === 'email' ?
+          hbs `{{{i18n code="oie.enroll.okta_verify.email.notReceived" bundle="login"}}}`:
+          hbs `{{{i18n code="oie.enroll.okta_verify.sms.notReceived" bundle="login"}}}`,
+        type: 'warning',
+      }));
+    },
+
+    handelResendLink () {
+      this.options.appState.trigger('invokeAction', 'currentAuthenticator-resend');
+      //hide warning, but reinitiate to show warning again after some threshold of polling
+      this.$el.addClass('hide');
+      this.showCalloutWithDelay();
+    },
+
+    postRender () {
+      this.showCalloutWithDelay();
+    },
+
+    showCalloutWithDelay () {
+      this.showMeTimeout = _.delay(() => {
+        this.$el.removeClass('hide');
+      }, SHOW_RESEND_TIMEOUT);
+    },
+
+    remove () {
+      View.prototype.remove.apply(this, arguments);
+      clearTimeout(this.showMeTimeout);
+    }
+  },
+);
