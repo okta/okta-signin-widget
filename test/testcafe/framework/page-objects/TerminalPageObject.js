09ee034ab9a426733f69e4908d321258ef5f3688
Thomas Maslen <thomas.maslen@okta.com>
2020-11-22 18:09:56 -0800

    test: partial cherry-pick from anipendakur #fa68926
    OKTA-312021

--- test/testcafe/framework/page-objects/TerminalPageObject.js
+++ test/testcafe/framework/page-objects/TerminalPageObject.js
@@ -1,7 +1,12 @@
+import { Selector } from 'testcafe';
 import BasePageObject from './BasePageObject';
 import CalloutObject from './components/CalloutObject';
 
 export default class TerminalPageObject extends BasePageObject {
+
+  constructor () {
+    this.beacon = new Selector('.beacon-container');
+  }
 
   getHeader() {
     return this.form.getTitle();
@@ -19,4 +24,7 @@ export default class TerminalPageObject extends BasePageObject {
     return this.form.waitForErrorBox();
   }
 
+  getBeaconClass() {
+    return this.beacon.find('[data-se="factor-beacon"]').getAttribute('class');
+  }
 }
