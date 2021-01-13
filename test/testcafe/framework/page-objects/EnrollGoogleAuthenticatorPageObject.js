6a88919458580e6905ebe5177d2c1053416532d1
tian.gan <tian.gan@okta.com>
2021-01-12 16:05:12 -0800

    feat: test

---
+++ test/testcafe/framework/page-objects/EnrollGoogleAuthenticatorPageObject.js
@@ -0,0 +1,48 @@
+import BasePageObject from './BasePageObject';
+import ResendEmailObject from './components/ResendEmailObject';
+
+const CODE_FIELD_NAME = 'credentials.otp';
+
+export default class EnrollGoogleAuthenticatorPageObject extends BasePageObject {
+
+  constructor (t) {
+    super(t);
+    this.resendEmail = new ResendEmailObject(t, this.form.el);
+  }
+
+  getBarcodeSubtitle () {
+    return this.getTextContent('.barcode-setup-title');
+  }
+
+  getManuallySetupSubtitle () {
+    return this.getTextContent('.manually-setup-title');
+  }
+
+  getSetUpDescription () {
+    return this.getTextContent('.google-authenticator-setup-info');
+  }
+
+  hasQRcode () {
+    return this.form.elementExist('.qrcode');
+  }
+
+  getSharedSecret () {
+    return this.form.getElement('.shared-secret input').getAttribute('placeholder');
+  }
+
+  async goToManuallySetup() {
+    await this.form.clickElement('.cannot-scan-link');
+  }
+
+  async goToNextPage() {
+    await this.form.clickElement('.google-authenticator-next');
+  }
+
+  enterCode(value) {
+    return this.form.setTextBoxValue(CODE_FIELD_NAME, value);
+  }
+
+  getCodeFieldError() {
+    return this.form.getTextBoxErrorMessage(CODE_FIELD_NAME);
+  }
+}
