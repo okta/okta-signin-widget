0d12e448badeeb4af0f5f4781bb3d6d78bd24d83
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2021-02-10 00:01:25 +0000

    Add support for number challenge polling view Resolves: OKTA-366354 Update button text for push and num challenge error state Resolves: OKTA-366355
    OKTA-366354
    <<<Jenkins Check-In of Tested SHA: 7099ab7d53b687ca28eb59b6b8aef2434c7debe3 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    
    OKTA-366355
    <<<Jenkins Check-In of Tested SHA: 39bc070d6893d5e3fe0e50b44abb86e6f7b474ff for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget

---
+++ src/v2/view-builder/views/ov/NumberChallengePushView.js
@@ -0,0 +1,65 @@
+import { loc } from 'okta';
+import BaseForm from '../../internals/BaseForm';
+import polling from '../shared/polling';
+import ResendNumberChallengeView from './ResendNumberChallengeView';
+import NumberChallengePhoneView from './NumberChallengePhoneView';
+
+const Body = BaseForm.extend(Object.assign(
+  {
+    noButtonBar: true,
+
+    className: 'okta-verify-number-challenge',
+
+    events: {
+      'click a.resend-number-challenge': 'handleResendNumberChallenge'
+    },
+
+    handleResendNumberChallenge () {
+      this.options.appState.trigger('invokeAction', 'currentAuthenticator-resend');
+      // hide the warning
+      this.options.appState.trigger('hideNumberChallengeWarning');
+    },
+
+    title () {
+      return loc('oie.okta_verify.push.sent', 'login');
+    },
+
+    initialize () {
+      BaseForm.prototype.initialize.apply(this, arguments);
+      this.add(NumberChallengePhoneView);
+    },
+
+    triggerAfterError () {
+      BaseForm.prototype.triggerAfterError.apply(this, arguments);
+      this.stopPolling();
+      this.$el.find('.o-form-fieldset-container').empty();
+    },
+
+    postRender () {
+      this.startPoll();
+    },
+
+    startPoll () {
+      this.startPolling();
+      this.addWarning();
+    },
+
+    stopPoll () {
+      this.stopPolling();
+    },
+
+    addWarning () {
+      this.add(ResendNumberChallengeView, '.o-form-error-container');
+      this.options.appState.trigger('showNumberChallengeWarning');
+    },
+
+    remove () {
+      BaseForm.prototype.remove.apply(this, arguments);
+      this.stopPolling();
+    },
+  },
+
+  polling,
+));
+
+export default Body;
