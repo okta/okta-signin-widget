665df0394759a823d6006bff38fc0e42d4ba6170
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2021-02-18 19:24:13 +0000

    Add support for number challenge polling view Resolves: OKTA-366354 Update button text for push and num challenge error state Resolves: OKTA-366355
    OKTA-366354
    <<<Jenkins Check-In of Tested SHA: 7099ab7d53b687ca28eb59b6b8aef2434c7debe3 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    
    OKTA-366355
    <<<Jenkins Check-In of Tested SHA: 39bc070d6893d5e3fe0e50b44abb86e6f7b474ff for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    
    OKTA-370904
    <<<Jenkins Check-In of Tested SHA: 0d12e448badeeb4af0f5f4781bb3d6d78bd24d83 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget

---
+++ src/v2/view-builder/views/ov/NumberChallengePhoneView.js
@@ -0,0 +1,32 @@
+import { View } from 'okta';
+import hbs from 'handlebars-inline-precompile';
+
+const NumberChallengePhoneView = View.extend({
+  className: 'number-challenge-section',
+  template: hbs`
+     <p>
+     {{i18n
+      code="oie.numberchallenge.instruction"
+      bundle="login"
+      arguments="correctAnswer"
+      $1="<span class='strong'>$1</span>"}}
+     </p>
+    <div class="phone">
+      <div class="phone--body">
+        <div class="phone--screen">
+          <span class="phone--number" data-se="challenge-number">{{correctAnswer}}</span>
+        </div>
+        <div class="phone--home-button"></div>
+      </div>
+    </div>
+  `,
+  getTemplateData () {
+    const correctAnswer = this.options.appState.get('currentAuthenticator')?.contextualData?.correctAnswer;
+    return {
+      correctAnswer
+    };
+  },
+});
+
+
+export default NumberChallengePhoneView;
