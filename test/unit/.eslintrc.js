5263e1fc50354fc49261bf1676406caefe6c10ac
Aaron Granick <aaron.granick@okta.com>
2021-02-25 13:55:01 -0800

    revert lint to use .js file

---
+++ test/unit/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: 'plugin:jasmine/recommended',
+  plugins: [
+    'jasmine'
+  ],
+  env: {
+    node: true,
+    jasmine: true,
+    es6: true,
+  },
+  rules: {
+    'max-len': 0,
+    'complexity': 0,
+    'max-params': 0,
+    'max-statements': 0,
+    'jasmine/new-line-before-expect': 0,
+    'jasmine/new-line-between-declarations': 0,
+    'jasmine/no-spec-dupes': [1, 'branch'],
+    'jasmine/no-suite-dupes': [1, 'branch'],
+
+    // Consider enabling these
+    'jasmine/no-unsafe-spy': 0,
+    'jasmine/prefer-toHaveBeenCalledWith': 0
+  }
+};
