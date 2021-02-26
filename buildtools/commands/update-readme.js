f9ac5ec719a886af3b9157c5307437d87e825788
Aaron Granick <aaron.granick@okta.com>
2021-02-26 22:34:55 +0000

    adds new build tool: update-readme
    OKTA-304593
    <<<Jenkins Check-In of Tested SHA: aed8a98683df8f381005d9898f13e2add63f7b85 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget

---
+++ buildtools/commands/update-readme.js
@@ -0,0 +1,26 @@
+const { resolve, join } = require('path');
+const { execSync } = require('child_process');
+const replace = require('replace-in-file');
+
+const ROOT_DIR = resolve(__dirname, '../../');
+
+function getPublishedWidgetVersion () {
+  const stdout = execSync('yarn info @okta/okta-signin-widget --json');
+  const meta = JSON.parse(stdout);
+  const version = meta.data['dist-tags'].latest;
+  console.log('Last published okta-signin-widget version: ', version);
+  return version;
+}
+
+exports.command = 'update-readme';
+exports.describe = 'Updates CDN URLs in the README file';
+exports.handler = () => {
+  const version = getPublishedWidgetVersion();
+  const options = {
+    files: resolve(join(ROOT_DIR, 'README.md')),
+    from: /https:\/\/global\.oktacdn\.com\/okta-signin-widget\/\d+\.\d+\.\d+/g,
+    to: `https://global.oktacdn.com/okta-signin-widget/${version}`
+  };
+  const results = replace.sync(options);
+  console.log(results); // Results will say hasChanged: true|false. https://www.npmjs.com/package/replace-in-file
+};
