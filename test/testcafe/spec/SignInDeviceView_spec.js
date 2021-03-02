829da70122d3ab413eba254cb1bb384ba90bb46e
Aaron Granick <aaron.granick@okta.com>
2021-03-02 01:43:42 +0000

    fix: only show beacon when customized (v2 only)
    OKTA-372808
    <<<Jenkins Check-In of Tested SHA: 5b2105e8257b2cd79bc59573d5f1b4138da2ba7e for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    
    OKTA-375492
    <<<Jenkins Check-In of Tested SHA: 9af01318392a07e76dbd6149546cfdcc8002ca34 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget

--- test/testcafe/spec/SignInDeviceView_spec.js
+++ test/testcafe/spec/SignInDeviceView_spec.js
@@ -23,7 +23,6 @@ async function setup(t) {
 test('shows the correct content', async t => {
   const signInDevicePage = await setup(t);
   await t.expect(signInDevicePage.getHeader()).eql('Sign In');
-  await t.expect(signInDevicePage.getBeaconClass()).contains('undefined-user');
   await t.expect(signInDevicePage.getOVButtonIcon()).eql('icon okta-verify-authenticator');
   await t.expect(signInDevicePage.getContentText()).eql('To access this resource, your organization requires you to sign in using your device.');
   await t.expect(signInDevicePage.getOVButtonLabel()).eql('Sign in using Okta Verify on this device');
