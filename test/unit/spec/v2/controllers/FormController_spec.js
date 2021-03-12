64ee25c66696a224d99c55c57a37402c835ea2be
Sheri Zada <sheri.zada@okta.com>
2021-03-12 02:04:55 +0000

    implement config setting transformUsername for v2 OIE
    Resolves OKTA-314098
    
    OKTA-314098
    <<<Jenkins Check-In of Tested SHA: 8a2c085be9703dfe313d7e9a1b396bc850d5aab0 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget

---
+++ test/unit/spec/v2/controllers/FormController_spec.js
@@ -0,0 +1,55 @@
+import {Model} from 'okta';
+
+import Settings from 'models/Settings';
+
+import AppState from 'v2/models/AppState';
+
+import FormController from 'v2/controllers/FormController';
+
+describe('v2/controllers/FormController', function () {
+  let testContext;
+  beforeEach(function () {
+    testContext = {};
+    testContext.init = (settings) => {
+      const appState = new AppState({idx: {neededToProceed: []}});
+      appState.hasRemediationObject = jest.fn();
+      appState.get('idx').neededToProceed.find = jest.fn().mockImplementation(() => true);
+      appState.get('idx').proceed = jest.fn().mockImplementation(() => ({then: () => Promise.resolve()}));
+      testContext.controller = new FormController({appState, settings});
+      testContext.model = new Model({formName: 'identify'});
+    };
+  });
+
+  afterEach(function () {
+    testContext.controller.options.appState.get('idx').proceed.mockReset();
+  });
+
+  it('passes identifier to idx.proceed', function () {
+    const settings = new Settings({
+      baseUrl: 'http://localhost:3000',
+    });
+    testContext.init(settings);
+
+    testContext.model.set('identifier', 'foo');
+    testContext.controller.handleSaveForm(testContext.model);
+
+    expect(testContext.controller.options.appState.get('idx').proceed.mock.calls[0][1].identifier).toEqual('foo');
+  });
+
+  it('passes transformed identifier using settings.transformUsername to idx.proceed', function () {
+    const settings = new Settings({
+      baseUrl: 'http://localhost:3000',
+      transformUsername: function (username, operation) {
+        if (operation === 'PRIMARY_AUTH') {
+          return `${username}@okta.com`;
+        }
+      }
+    });
+    testContext.init(settings);
+
+    testContext.model.set('identifier', 'foo');
+    testContext.controller.handleSaveForm(testContext.model);
+
+    expect(testContext.controller.options.appState.get('idx').proceed.mock.calls[0][1].identifier).toEqual('foo@okta.com');
+  });
+});
