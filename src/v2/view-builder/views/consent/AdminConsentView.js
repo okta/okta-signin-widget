29fbea0ce009d05a8e174a15cb4fc45165c36388
Conor Hanrahan <conor.hanrahan@okta.com>
2021-03-22 13:03:15 -0700

    Use showCancelBeforeSave in AdminConsentView

--- src/v2/view-builder/views/consent/AdminConsentView.js
+++ src/v2/view-builder/views/consent/AdminConsentView.js
@@ -5,13 +5,6 @@ import ConsentViewForm from './ConsentViewForm';
 export default BaseView.extend({
   Header: AdminConsentViewHeader,
   Body: ConsentViewForm,
-  postRender() {
-    // Move buttons in DOM to match visual hierarchy to fix tab order.
-    // TODO https://oktainc.atlassian.net/browse/OKTA-350521
-    const buttonContainer = this.$el.find('.o-form-button-bar');
-    buttonContainer.find('.button-primary').appendTo(buttonContainer);
-
-  },
   createModelClass(currentViewState) {
     const ModelClass = BaseView.prototype.createModelClass.apply(this, arguments);
     const {scopes} = currentViewState.uiSchema[0];
