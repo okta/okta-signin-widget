197a74df8326dbd3a9ba135b79ceefbaf1478cd5
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2021-03-31 18:42:57 +0000

    Adds error message to OV verify & enroll flows for enabling biometrics Resolves: OKTA-376948
    OKTA-376948
    <<<Jenkins Check-In of Tested SHA: d6b8cec01ded6fc18f554e10bb8c0f3ed3c63f51 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    
    OKTA-383976
    <<<Jenkins Check-In of Tested SHA: 0e21a95b3f1102d0be68003ee81c0d43a39a5db0 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget

--- test/testcafe/spec/EnrollAuthenticatorOktaVerify_spec.js
+++ test/testcafe/spec/EnrollAuthenticatorOktaVerify_spec.js
@@ -19,6 +19,10 @@ import xhrAuthenticatorEnrollOktaVerifyViaEmailVersionUpgradeNonIos from '../../
 import xhrAuthenticatorEnrollOktaVerifyViaSMSVersionUpgradeNonIos from '../../../playground/mocks/data/idp/idx/authenticator-enroll-ov-sms-version-upgrade-non-ios';
 import xhrAuthenticatorEnrollOktaVerifyViaQRVersionUpgradeNonIos from '../../../playground/mocks/data/idp/idx/authenticator-enroll-ov-qr-version-upgrade-non-ios';
 
+import xhrAuthenticatorEnrollEnableBiometricsQr from '../../../playground/mocks/data/idp/idx/authenticator-enroll-ov-qr-enable-biometrics';
+import xhrAuthenticatorEnrollEnableBiometricsEmail from '../../../playground/mocks/data/idp/idx/authenticator-enroll-ov-email-enable-biometrics';
+import xhrAuthenticatorEnrollEnableBiometricsSMS from '../../../playground/mocks/data/idp/idx/authenticator-enroll-ov-sms-enable-biometrics';
+
 import xhrSuccess from '../../../playground/mocks/data/idp/idx/success';
 
 const logger = RequestLogger(/introspect|poll|send|enroll/, {
@@ -135,6 +139,32 @@ const enrollViaQRcodeVersionUpgradeMocksNonIos = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/challenge/poll')
   .respond(xhrSuccess);
 
+const enrollViaQRcodeEnableBiometricsMocks = RequestMock()
+  .onRequestTo('http://localhost:3000/idp/idx/introspect')
+  .respond(xhrAuthenticatorEnrollEnableBiometricsQr)
+  .onRequestTo('http://localhost:3000/idp/idx/challenge/poll')
+  .respond(xhrSuccess);
+
+const enrollViaEmailEnableBiometricsMocks = RequestMock()
+  .onRequestTo('http://localhost:3000/idp/idx/introspect')
+  .respond(xhrAuthenticatorEnrollOktaVerifyQr)
+  .onRequestTo('http://localhost:3000/idp/idx/credential/enroll')
+  .respond(xhrAuthenticatorEnrollOktaVerifyViaEmail)
+  .onRequestTo('http://localhost:3000/idp/idx/challenge/send')
+  .respond(xhrAuthenticatorEnrollEnableBiometricsEmail)
+  .onRequestTo('http://localhost:3000/idp/idx/challenge/poll')
+  .respond(xhrSuccess);
+
+const enrollViaSMSEnableBiometricsMocks = RequestMock()
+  .onRequestTo('http://localhost:3000/idp/idx/introspect')
+  .respond(xhrAuthenticatorEnrollOktaVerifyQr)
+  .onRequestTo('http://localhost:3000/idp/idx/credential/enroll')
+  .respond(xhrAuthenticatorEnrollOktaVerifyViaSMS)
+  .onRequestTo('http://localhost:3000/idp/idx/challenge/send')
+  .respond(xhrAuthenticatorEnrollEnableBiometricsSMS)
+  .onRequestTo('http://localhost:3000/idp/idx/challenge/poll')
+  .respond(xhrSuccess);
+
 const smsInstruction = 'We sent an SMS to +18008885555 with an Okta Verify setup link. To continue, open the link on your mobile device.\nOr try a different way to set up Okta Verify.';
 const emailInstruction = 'We sent an email to joy@okta.com with an Okta Verify setup link. To continue, open the link on your mobile device.\nOr try a different way to set up Okta Verify.';
 const qrCodeInstruction = 'On your mobile device, download the Okta Verify app from the App Store (iPhone and iPad) or Google Play (Android devices).\nOpen the app and follow the instructions to add your account\nWhen prompted, tap Scan a QR code, then scan the QR code below:';
@@ -142,6 +172,9 @@ const qrCodeInstruction = 'On your mobile device, download the Okta Verify app f
 const fipsUpgradeMessage = 'The device used to set up Okta Verify does not meet your organization’s security requirements because it is not FIPS compliant. Contact your administrator for help.';
 const fipsUpgradeMessageNonIos = 'The Okta Verify version on the device used does not meet your organization’s security requirements. To add your account, update Okta Verify to the latest version, then try again.';
 const fipsUpgradeTitle = 'Update Okta Verify';
+
+const enableBiometricsMessage = 'Your organization requires biometrics. To proceed, ensure your device supports biometrics, then add your account and enable biometrics when prompted.';
+const enableBiometricsMessageTitle = 'Enable biometrics to add an account in Okta Verify';
 
 fixture('Enroll Okta Verify Authenticator');
 
@@ -434,3 +467,85 @@ test.requestHooks(logger, enrollViaSmsVersionUpgradeMocksGoBack)('should not sho
   await t.expect(errorBox.exists).notOk;
 });
 
+test.requestHooks(logger, enrollViaQRcodeEnableBiometricsMocks)('should see ov enable biometrics message during enroll via QR code', async t => {
+  const enrollOktaVerifyPage = await setup(t);
+  await t.expect(enrollOktaVerifyPage.getFormTitle()).eql('Set up Okta Verify');
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaQRInstruction()).eql(true);
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaEmailInstruction()).eql(false);
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaSmsInstruction()).eql(false);
+  await t.expect(enrollOktaVerifyPage.hasQRcode()).eql(true);
+  await t.expect(enrollOktaVerifyPage.getQRInstruction()).eql(qrCodeInstruction);
+  const errorBox = enrollOktaVerifyPage.getErrorBox();
+  await t.expect(errorBox.innerText).contains(enableBiometricsMessage);
+  const errorTitle = enrollOktaVerifyPage.getErrorTitle();
+  await t.expect(errorTitle.innerText).contains(enableBiometricsMessageTitle);
+  await t.wait(4000);
+  await t.expect(logger.count(
+    record => record.response.statusCode === 200 &&
+    record.request.url.match(/poll/)
+  )).eql(1);
+  const successPage = new SuccessPageObject(t);
+  const pageUrl = await successPage.getPageUrl();
+  await t.expect(pageUrl)
+    .eql('http://localhost:3000/app/UserHome?stateToken=mockedStateToken123');
+});
+
+test.requestHooks(enrollViaEmailEnableBiometricsMocks)('should see ov enable biometrics message during enroll via email', async t => {
+  const enrollOktaVerifyPage = await setup(t);
+  await enrollOktaVerifyPage.clickSwitchChannel();
+  const switchChannelPageObject = new SwitchOVEnrollChannelPageObject(t);
+  await t.expect(switchChannelPageObject.getFormTitle()).eql('More options');
+  await switchChannelPageObject.selectChannelOption(1);
+  await switchChannelPageObject.clickNextButton();
+  const enrollViaEmailPageObject = new EnrollOVViaEmailPageObject(t);
+  await t.expect(enrollViaEmailPageObject.getFormTitle()).eql('Set up Okta Verify via email link');
+  const enrollViaSMSPageObject = new EnrollOVViaSMSPageObject(t);
+  await t.expect(enrollViaSMSPageObject.hasCountryField()).notOk();
+  await t.expect(enrollViaEmailPageObject.hasSwitchChannelText).ok();
+  await enrollViaEmailPageObject.fillEmailField('test@gmail.com');
+  await enrollViaEmailPageObject.clickNextButton();
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaQRInstruction()).eql(false);
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaEmailInstruction()).eql(true);
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaSmsInstruction()).eql(false);
+  await t.expect(enrollOktaVerifyPage.getEmailInstruction()).eql(emailInstruction);
+  const errorBox = enrollOktaVerifyPage.getErrorBox();
+  await t.expect(errorBox.innerText).contains(enableBiometricsMessage);
+  const errorTitle = enrollOktaVerifyPage.getErrorTitle();
+  await t.expect(errorTitle.innerText).contains(enableBiometricsMessageTitle);
+  const successPage = new SuccessPageObject(t);
+  const pageUrl = await successPage.getPageUrl();
+  await t.expect(pageUrl)
+    .eql('http://localhost:3000/app/UserHome?stateToken=mockedStateToken123');
+});
+
+test.requestHooks(logger, enrollViaSMSEnableBiometricsMocks)('should see ov enable biometrics message during enroll via sms', async t => {
+  const enrollOktaVerifyPage = await setup(t);
+  await enrollOktaVerifyPage.clickSwitchChannel();
+  const switchChannelPageObject = new SwitchOVEnrollChannelPageObject(t);
+  await t.expect(switchChannelPageObject.getFormTitle()).eql('More options');
+  await switchChannelPageObject.selectChannelOption(1);
+  await switchChannelPageObject.clickNextButton();
+  const enrollViaSMSPageObject = new EnrollOVViaSMSPageObject(t);
+  await t.expect(enrollViaSMSPageObject.getFormTitle()).eql('Set up Okta Verify via SMS');
+  await t.expect(enrollViaSMSPageObject.hasSwitchChannelText).ok();
+  await t.expect(enrollViaSMSPageObject.hasCountryField()).ok();
+  await t.expect(enrollViaSMSPageObject.getCountryLabel()).eql('+1');
+  await enrollViaSMSPageObject.fillPhoneField('8887227871');
+  await enrollViaSMSPageObject.clickNextButton();
+  await t.expect(logger.count(() => true)).eql(3);
+  const { request: { body: answerRequestBodyString }} = logger.requests[2];
+  const answerRequestBody = JSON.parse(answerRequestBodyString);
+  await t.expect(answerRequestBody.phoneNumber).eql('+18887227871');
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaQRInstruction()).eql(false);
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaEmailInstruction()).eql(false);
+  await t.expect(enrollOktaVerifyPage.hasEnrollViaSmsInstruction()).eql(true);
+  await t.expect(enrollOktaVerifyPage.getSmsInstruction()).eql(smsInstruction);
+  const errorBox = enrollOktaVerifyPage.getErrorBox();
+  await t.expect(errorBox.innerText).contains(enableBiometricsMessage);
+  const errorTitle = enrollOktaVerifyPage.getErrorTitle();
+  await t.expect(errorTitle.innerText).contains(enableBiometricsMessageTitle);
+  const successPage = new SuccessPageObject(t);
+  const pageUrl = await successPage.getPageUrl();
+  await t.expect(pageUrl)
+    .eql('http://localhost:3000/app/UserHome?stateToken=mockedStateToken123');
+});
