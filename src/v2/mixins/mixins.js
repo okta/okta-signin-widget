c7a9a6be96570e6079fa0edf607064ad07f5fb5e
Anatolii Boiarshchykov-okta <anatolii.boiarshchykov@okta.com>
2021-04-08 19:11:38 +0000

    update AppState.shouldReRenderView() to ignore 'stateHandle' data OKTA-384206 - update shouldReRenderView() to ignore 'stateHandle' to match previous & current data - add unit test to check 'stateHandle' field is ignored
    OKTA-384206
    <<<Jenkins Check-In of Tested SHA: 114b32187e50fa155bee7833778f76c32d64b552 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 3
    
    OKTA-385741
    <<<Jenkins Check-In of Tested SHA: 7b21d5c75d54ede81464a187fd1696d3dc1a8fb2 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 102

--- src/v2/mixins/mixins.js
+++ src/v2/mixins/mixins.js
@@ -1,17 +1,24 @@
 import { _ } from 'okta';
 
 _.mixin({
-  nestedOmit: function(obj, iteratee, context) {
-    var result = _.omit(obj, iteratee, context);
-          
+  nestedOmit: function(obj, names) {
+    var result = _.omit(obj, names);
+
+    if (names.includes(result.name)) {
+      result = _.omit(result, 'value');
+    }
+
     _.each(result, function(val, key) {
-      if (typeof(val) === 'object') {
-        result[key] = _.nestedOmit(val, iteratee, context);
+      if (Array.isArray(val)) {
+        result[key] = val.map((v) => {
+          return _.nestedOmit(v, names);
+        });
+      } else if (typeof val === 'object') {
+        result[key] = _.nestedOmit(val, names);
       }
     });
-          
     return result;
-  }
+  },
 });
 
 export { _ };
