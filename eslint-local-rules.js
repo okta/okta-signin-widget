2ba5dd0cb2a47e6c61b595a9e6c89957b7ccd685
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2021-04-10 11:49:56 -0700

    feat: Add lint rule for missing keys in logi_ok_PL.properties Resolves: OKTA-386795

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -1,5 +1,7 @@
 var noBareTemplates = require('./packages/@okta/eslint-plugin-okta-ui/lib/rules/no-bare-templates');
+var noMissingKeys = require('./packages/@okta/eslint-plugin-okta-ui/lib/rules/no-missing-keys');
 
 module.exports = {
-  'no-bare-templates': noBareTemplates
+  'no-bare-templates': noBareTemplates,
+  'no-missing-keys': noMissingKeys,
 };
