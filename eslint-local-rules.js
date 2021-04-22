0391a0273ba1d4ff12dde11885ddb241a8f8c076
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2021-04-22 19:14:12 +0000

    feat: Add lint rule to find missing i18n keys in mock files Resolves: OKTA-380268
    OKTA-380268
    <<<Jenkins Check-In of Tested SHA: 12c57959cca2ed45a012985990ceee2db82b8ee4 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 4

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -1,7 +1,9 @@
 var noBareTemplates = require('./packages/@okta/eslint-plugin-okta-ui/lib/rules/no-bare-templates');
 var noMissingKeys = require('./packages/@okta/eslint-plugin-okta-ui/lib/rules/no-missing-keys');
+var noMissingAPIKeys = require('./packages/@okta/eslint-plugin-okta-ui/lib/rules/no-missing-api-keys');
 
 module.exports = {
   'no-bare-templates': noBareTemplates,
   'no-missing-keys': noMissingKeys,
+  'no-missing-api-keys': noMissingAPIKeys
 };
