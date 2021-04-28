f312aec91a3b3202ec53701f7e41f4c406057859
Gowthami Dommety <gowthami.dommety@okta.com>
2021-04-28 15:56:39 -0700

    response config changes

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -14,7 +14,7 @@ const idx = {
   ],
 
   '/idp/idx/introspect': [
-    'identify',
+    // 'identify',
     // 'error-with-failure-redirect',
     // 'error-feature-not-enabled',
     // 'error-403-security-access-denied',
@@ -60,7 +60,7 @@ const idx = {
     // 'identify-with-device-probing-loopback-3',
     // 'identify-with-device-probing-loopback-challenge-not-received',
     // 'identify-with-no-sso-extension',
-    // 'identify-with-third-party-idps',
+    'identify-with-third-party-idps',
     // 'identify-with-only-third-party-idps',
     // 'identify-with-only-one-third-party-idp',
     // 'identify-with-password',
