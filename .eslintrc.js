f611d60b6007e580f89e08e00b32bcb36501329d
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2021-04-30 04:32:09 +0000

    feat: Enable shared courage-app lint rules for SIW  Resolves: OKTA-379673, OKTA-379674
    OKTA-379673
    <<<Jenkins Check-In of Tested SHA: 08f532ee9c2f7e68fbb036f11f103f6b0562edae for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 18
    
    (cherry picked from commit 4854d0b269b932f92e570623791c6969223937fe)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,99 @@
 module.exports = {
+  'root': true,
   'extends': [
-    'eslint:recommended',
-    'plugin:json/recommended'
+    'plugin:@okta/okta/courage-app' // apply courage-app rules to all files including properties
   ],
-  'env': {
-    'browser': true,
-    'jasmine': true,
-    'node': true,
-    'amd': true
-  },
   'parser': 'babel-eslint',
   'parserOptions': {
     'sourceType': 'module',
     'ecmaVersion': 2017
   },
-  'plugins': [
-    'eslint-plugin-local-rules',
-    'eslint-plugin-no-only-tests'
-  ],
-  'globals': {
-    'spyOnEvent': false,
-    'JSON': true,
-    'DEBUG': true,
-    'Promise': true
-  },
   'overrides': [
+    {
+      // temporarily ignoring files that violate the @okta/okta/no-exclusive-language rule
+      'files': [
+        'packages/**/login.properties',
+        'packages/**/country_fr.properties',
+      ],
+      'rules': {
+        '@okta/okta/no-exclusive-language': 0,
+      },
+    },
+    {
+      'files': ['*.js'],
+      'extends': [
+        'eslint:recommended',
+      ],
+      'env': {
+        'browser': true,
+        'jasmine': true,
+        'node': true,
+        'amd': true
+      },
+      'plugins': [
+        'eslint-plugin-local-rules',
+        'eslint-plugin-no-only-tests'
+      ],
+      'globals': {
+        'spyOnEvent': false,
+        'JSON': true,
+        'DEBUG': true,
+        'Promise': true
+      },
+      'rules': {
+        'camelcase': 2,
+        'complexity': [2, 10],
+        'curly': 2,
+        'eqeqeq': 2,
+        'guard-for-in': 2,
+        'indent': [2, 2, { 'VariableDeclarator': 2 }],
+        'max-depth': [2, 2],
+        'max-len': [2, 120],
+        'max-params': [2, 10],
+        'max-statements': [2, 20],
+        'new-cap': [2, { 'properties': false, 'capIsNewExceptions': ['Q'] }],
+        'no-caller': 2,
+        'no-eval': 2,
+        'no-implied-eval': 2,
+        'no-new': 2,
+        'no-unused-expressions': [2, { 'allowShortCircuit': true, 'allowTernary': true }],
+        'no-use-before-define': [2, 'nofunc'],
+        'no-only-tests/no-only-tests': 'error',
+        'quotes': [2, 'single'],
+        'semi': 2,
+        'space-before-function-paren': [2, {
+          'anonymous': 'never',
+          'named': 'never',
+          'asyncArrow': 'always'
+        }],
+        'strict': 0,
+      },
+    },
     {
       'files': ['src/**/*.js'],
       'rules': {
-        'local-rules/no-bare-templates': 2,
-      }
+        'local-rules/no-bare-templates': 2, // we dont want no-bare-templates rule to be applied to test files
+      },
+      'overrides': [
+        {
+          // temporarily ignoring files that violate @okta/okta/no-unlocalized-text-in-templates rule
+          'files': [
+            'src/**/CustomButtons.js',
+            'src/**/SignInWithIdps.js',
+          ],
+          'rules': {
+            '@okta/okta/no-unlocalized-text-in-templates': 0,
+          },
+        },
+      ]
     },
     {
-      'files': ['packages/**/*.json'],
+      'files': ['playground/**/*.json', 'packages/**/*.json'],
+      'extends': [
+        'plugin:json/recommended',
+      ],
       'rules': {
         'local-rules/no-missing-keys': 2,
-      }
-    },
-    {
-      'files': ['playground/**/*.json'],
-      'rules': {
         'local-rules/no-missing-api-keys': 2,
       },
       'overrides': [
@@ -83,33 +137,4 @@ module.exports = {
       ]
     }
   ],
-  'rules': {
-    'camelcase': 2,
-    'complexity': [2, 10],
-    'curly': 2,
-    'eqeqeq': 2,
-    'guard-for-in': 2,
-    'indent': [2, 2, { 'VariableDeclarator': 2 }],
-    'max-depth': [2, 2],
-    'max-len': [2, 120],
-    'max-params': [2, 10],
-    'max-statements': [2, 20],
-    'new-cap': [2, { 'properties': false, 'capIsNewExceptions': ['Q'] }],
-    'no-caller': 2,
-    'no-eval': 2,
-    'no-implied-eval': 2,
-    'no-new': 2,
-    'no-unused-expressions': [2, { 'allowShortCircuit': true, 'allowTernary': true }],
-    'no-use-before-define': [2, 'nofunc'],
-    'no-only-tests/no-only-tests': 'error',
-    'quotes': [2, 'single'],
-    'semi': 2,
-    'space-before-function-paren': [2, {
-      'anonymous': 'never',
-      'named': 'never',
-      'asyncArrow': 'always'
-    }],
-    'strict': 0,
-  },
-  'root': true
 };
--- scripts/buildtools/index.js
+++ scripts/buildtools/index.js
@@ -3,6 +3,7 @@ const commands = './commands';
 
 /* eslint-disable no-unused-expressions */
 require('yargs')
+/* eslint-disable-next-line @okta/okta/no-unlocalized-text-in-templates */
   .usage('Usage: $0 <command> [options]')
   .demandCommand(1)
   .commandDir(commands)
