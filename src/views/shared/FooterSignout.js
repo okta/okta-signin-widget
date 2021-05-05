9d55ac908e78aa905011fec6f0e22d0c244af07c
Sheri Zada <sheri.zada@okta.com>
2021-05-05 22:24:38 +0000

    feat[v1]: ignore settings.signOutLink if SMS Password Recovery flow
    OKTA-374204
    <<<Jenkins Check-In of Tested SHA: ea433314ff672c6128f7d264531dda53d97730ae for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 3
    PR Link:

--- src/views/shared/FooterSignout.js
+++ src/views/shared/FooterSignout.js
@@ -28,19 +28,20 @@ export default View.extend({
   },
   handleSignout: function(e) {
     e.preventDefault();
-    this.options.appState.trigger('signOut');
-    const self = this;
+
+    const appState = this.options.appState;
+    appState.trigger('signOut');
 
     this.model
       .doTransaction(function(transaction) {
         return transaction.cancel();
       })
-      .then(function() {
-        if (self.settings.get('signOutLink')) {
-          Util.redirect(self.settings.get('signOutLink'));
+      .then(() => {
+        if (this.settings.get('signOutLink') && !appState.get('isSMSPasswordRecovery')) {
+          Util.redirect(this.settings.get('signOutLink'));
         } else {
-          self.state.set('navigateDir', Enums.DIRECTION_BACK);
-          self.options.appState.trigger('navigate', '');
+          this.state.set('navigateDir', Enums.DIRECTION_BACK);
+          appState.trigger('navigate', '');
         }
       });
   },
