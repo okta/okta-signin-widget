ad9331d8d3fb8ede00794735d052605f028d9f3e
Sheri Zada <sheri.zada@okta.com>
2021-05-11 10:44:03 -0700

    fix "error-unlock-account"

--- .eslintrc.js
+++ .eslintrc.js
@@ -110,7 +110,6 @@ module.exports = {
             'playground/**/error-okta-verify-totp.json',
             'playground/**/error-pre-versioning-ff-session-expired.json',
             'playground/**/error-session-expired.json',
-            'playground/**/error-unlock-account.json',
             'playground/**/error-user-is-not-assigned.json',
             'playground/**/identify-unknown-user.json',
             'playground/**/terminal-registration.json',
