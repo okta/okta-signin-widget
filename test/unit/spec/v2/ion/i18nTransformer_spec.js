60e1ad6c44525378bc319c063200fb1eefab8d74
Ani Pendakur <Ani.pendakur@okta.com>
2021-05-12 13:12:37 -0700

    adding a missing i18n mapping and tweaking a corresponding test for it
    OKTA-388452

--- test/unit/spec/v2/ion/i18nTransformer_spec.js
+++ test/unit/spec/v2/ion/i18nTransformer_spec.js
@@ -45,7 +45,7 @@ describe('v2/ion/i18nTransformer', function() {
       'security.disliked_food': 'dislike food answer',
       'security.name_of_first_plush_toy': 'first plush toy answer',
       'security.favorite_vacation_location': 'vacation location answer',
-      'idx.email.verification.required': 'To finish signing in, check your email.',
+      'registration.complete.confirm.text': 'To finish signing in, check your email.',
       'idx.foo': 'hello the {0} authenticator',
       'password.forgot.email.or.username.placeholder': 'email or username',
       'oie.browser.error.NotAllowedError': 'translated browser thrown error',
