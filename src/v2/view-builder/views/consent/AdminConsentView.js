332495a67f7024680e71ef0460347740d4a3ff52
Sheri Zada <sheri.zada@okta.com>
2021-05-17 17:31:34 -0700

    add i18n keys for all scopes and logo alt text

--- src/v2/view-builder/views/consent/AdminConsentView.js
+++ src/v2/view-builder/views/consent/AdminConsentView.js
@@ -1,4 +1,5 @@
 import { BaseView } from '../../internals';
+import { loc } from 'okta';
 import AdminConsentViewHeader from './AdminConsentViewHeader';
 import ConsentViewForm from './ConsentViewForm';
 
@@ -8,10 +9,16 @@ export default BaseView.extend({
   createModelClass(currentViewState) {
     const ModelClass = BaseView.prototype.createModelClass.apply(this, arguments);
     const {scopes} = currentViewState.uiSchema[0];
+    const i18nKeyPrefix = 'consent.scopes';
+    const localizedScopes = scopes.map(({name}) => ({
+      name,
+      displayName: loc(`${i18nKeyPrefix}.${name}.label`, 'login'),
+      description: loc(`${i18nKeyPrefix}.${name}.desc`, 'login'),
+    }));
 
     return ModelClass.extend({
       props: {
-        scopes: {type: 'array', value: scopes},
+        scopes: {type: 'array', value: localizedScopes},
       },
       local: {
         consent: {type: 'boolean', value: false},
