48b846ccf130523af1e6d0e4b5fa043b8ac9f285
Ani Pendakur <Ani.pendakur@okta.com>
2021-06-07 14:00:05 -0700

    fix: pr recommendation OKTA-392962

--- src/v2/controllers/FormController.js
+++ src/v2/controllers/FormController.js
@@ -245,15 +245,13 @@ export default Controller.extend({
       error = 'FormController - unknown error found';
       this.options.settings.callGlobalError(error);
     }
-
+    let errorObj;
     if (IonResponseHelper.isIonErrorResponse(error)) {
-      const convertedErrors = IonResponseHelper.convertFormErrors(error);
-      model.trigger('error', model, convertedErrors, true);
+      errorObj = IonResponseHelper.convertFormErrors(error);
     } else if (error.errorSummary) {
-      model.trigger('error', model, { responseJSON: error }, true);
-    } else {
-      model.trigger('error', model, { responseJSON: { errorSummary: String(error) } }, true);
+      errorObj = { responseJSON: error };
     }
+    model.trigger('error', model, errorObj || { responseJSON: { errorSummary: String(error) } }, true);
   },
 
   handleIdxSuccess: function(idxResp) {
