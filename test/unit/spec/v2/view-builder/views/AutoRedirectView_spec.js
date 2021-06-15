4d9768a43f6a6939d6f28e351f4651c924a6464f
Kamaldeep Cheema <kamaldeep.cheema@okta.com>
2021-06-15 17:04:53 -0400

    Hide redirect spinner if option not set

---
+++ test/unit/spec/v2/view-builder/views/AutoRedirectView_spec.js
@@ -0,0 +1,54 @@
+import { Model } from 'okta';
+import AutoRedirectView from 'v2/view-builder/views/AutoRedirectView';
+import AppState from 'v2/models/AppState';
+import Settings from 'models/Settings';
+import SuccessWithAppUser
+  from '../../../../../../playground/mocks/data/idp/idx/success-with-app-user.json';
+
+describe('v2/view-builder/views/AutoRedirectView', function() {
+  let testContext;
+  let settings = new Settings({ 
+    baseUrl: 'http://localhost:3000'
+  });  
+  beforeEach(function() { 
+    testContext = {};
+    testContext.init = (user = SuccessWithAppUser.user.value, app = SuccessWithAppUser.app.value) => {
+      const appState = new AppState();
+      appState.set('user', user);
+      appState.set('app', app);
+
+      testContext.view = new AutoRedirectView({
+        appState,
+        settings,
+        currentViewState: {},
+        model: new Model(),
+      });
+      testContext.view.render();
+    };
+  });
+  afterEach(function() {
+    jest.resetAllMocks();
+  });
+
+  it('view renders corectly - default case', function() {
+    testContext.init();
+    expect(testContext.view.el).toMatchSnapshot();
+  });
+
+  it('view renders correctly according to showRedirectAnimation', function() {
+    settings = new Settings({ 
+      baseUrl: 'http://localhost:3000',
+      'features.showRedirectAnimation': false
+    });    
+    testContext.init();
+    expect(testContext.view.el).toMatchSnapshot('should NOT render spinner');
+    
+
+    settings = new Settings({ 
+      baseUrl: 'http://localhost:3000',
+      'features.showRedirectAnimation': true
+    });    
+    testContext.init();
+    expect(testContext.view.el).toMatchSnapshot('should have spinner');
+  });
+});
