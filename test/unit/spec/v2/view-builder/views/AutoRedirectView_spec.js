0c5a5c24753a81b761f1abfb80ab6c785fe6dd5f
Kamaldeep Cheema <kamaldeep.cheema@okta.com>
2021-06-16 14:44:13 -0400

    updating option name

--- test/unit/spec/v2/view-builder/views/AutoRedirectView_spec.js
+++ test/unit/spec/v2/view-builder/views/AutoRedirectView_spec.js
@@ -35,10 +35,10 @@ describe('v2/view-builder/views/AutoRedirectView', function() {
     expect(testContext.view.el).toMatchSnapshot();
   });
 
-  it('view renders correctly according to showRedirectAnimation', function() {
+  it('view renders correctly according to showRedirectView', function() {
     settings = new Settings({ 
       baseUrl: 'http://localhost:3000',
-      'features.showRedirectAnimation': false
+      'features.showRedirectView': false
     });    
     testContext.init();
     expect(testContext.view.el).toMatchSnapshot('should NOT render spinner');
@@ -46,7 +46,7 @@ describe('v2/view-builder/views/AutoRedirectView', function() {
 
     settings = new Settings({ 
       baseUrl: 'http://localhost:3000',
-      'features.showRedirectAnimation': true
+      'features.showRedirectView': true
     });    
     testContext.init();
     expect(testContext.view.el).toMatchSnapshot('should have spinner');
