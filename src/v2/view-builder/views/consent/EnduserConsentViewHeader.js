a2a6283f747e2369575008e6cd33d3ebd2e5640a
Thomas Maslen <thomas.maslen@okta.com>
2021-07-07 23:00:15 +0000

    fix: deduplicate "oie.consent.enduser.title" i18n key
    Resolves: OKTA-408851
    
    OKTA-408851
    <<<Jenkins Check-In of Tested SHA: 43d9e6d8b79c1aa3cac701cd8506a85e46a87000 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 6
    PR Link:

--- src/v2/view-builder/views/consent/EnduserConsentViewHeader.js
+++ src/v2/view-builder/views/consent/EnduserConsentViewHeader.js
@@ -2,6 +2,6 @@ import { loc } from 'okta';
 import AdminConsentViewHeader from './AdminConsentViewHeader';
 
 export default AdminConsentViewHeader.extend({
-  titleText: () => loc('oie.consent.enduser.title', 'login'),
+  titleText: () => loc('oie.consent.scopes.enduser.title', 'login'),
   hasIssuer: false,
 });
