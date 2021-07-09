7f4544970c00ffeb50c057c3dc06ab2b8677c507
Kamaldeep Cheema <kamaldeep.cheema@okta.com>
2021-07-09 17:01:00 -0400

    small updates

--- src/v2/view-builder/views/AutoRedirectView.js
+++ src/v2/view-builder/views/AutoRedirectView.js
@@ -1,6 +1,12 @@
 import { _, loc } from 'okta';
 import { BaseForm, BaseView } from '../internals';
 
+// Possible options for the SIW interistitial redirect view
+const REDIRECT_VIEW = {
+  DEFAULT: 'DEFAULT',
+  NONE: 'NONE'
+};
+
 const Body = BaseForm.extend({
   title() {
     let titleString = loc('oie.success.text.signingIn', 'login');
@@ -8,7 +14,10 @@ const Body = BaseForm.extend({
     const app = this.options.appState.get('app');
     const user = this.options.appState.get('user');
 
-    if (!this.redirectView || this.redirectView === 'NONE') {
+    // If the option is not set, we treat that as being the case where we don't render the spinner.
+    // This is to account for the customer hosted scenario, because by default okta-core will pass in
+    // the correct value as set by the option in the Admin UI (which by default is "DEFAULT").
+    if (!this.redirectView || this.redirectView === REDIRECT_VIEW.NONE) {
       titleString = loc('oie.success.text.signingIn.with.ellipsis', 'login');
       return titleString;
     }
@@ -40,7 +49,7 @@ const Body = BaseForm.extend({
 
   render() {
     BaseForm.prototype.render.apply(this, arguments);
-    if (this.redirectView === 'DEFAULT') {
+    if (this.redirectView === REDIRECT_VIEW.DEFAULT) {
       this.add('<div class="okta-waiting-spinner"></div>');
     }
   }
