8fef811f359bebbeb1afcaf12e351c5398cae383
Kamaldeep Cheema <kamaldeep.cheema@okta.com>
2021-07-13 14:59:15 +0000

    Resolve OKTA-399274 - Make interstitial view configurable
    OKTA-399274
    <<<Jenkins Check-In of Tested SHA: cd51f357456576f4c048e82f9b758c5ab5b6e41b for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 6
    PR Link:
    
    OKTA-412936
    <<<Jenkins Check-In of Tested SHA: 32062640004d197597169a78028df77af748b38f for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 52
    PR Link: "https://github.com/okta/okta-signin-widget/pull/2107"

---
+++ test/unit/spec/v2/view-builder/views/AutoRedirectView_spec.js
@@ -0,0 +1,56 @@
+import { Model } from 'okta';
+import AutoRedirectView from 'v2/view-builder/views/AutoRedirectView';
+import AppState from 'v2/models/AppState';
+import Settings from 'models/Settings';
+import SuccessWithAppUser
+  from '../../../../../../playground/mocks/data/idp/idx/success-with-app-user.json';
+import { INTERSTITIAL_REDIRECT_VIEW } from 'v2/ion/RemediationConstants';
+
+
+describe('v2/view-builder/views/AutoRedirectView', function() {
+  let testContext;
+  let settings = new Settings({ 
+    baseUrl: 'http://localhost:3000'
+  });  
+  beforeEach(function() { 
+    testContext = {};
+    testContext.init = (user = SuccessWithAppUser.user.value, app = SuccessWithAppUser.app.value) => {
+      const appState = new AppState();
+      appState.set('user', user);
+      appState.set('app', app);
+
+      testContext.view = new AutoRedirectView({
+        appState,
+        settings,
+        currentViewState: {},
+        model: new Model(),
+      });
+      testContext.view.render();
+    };
+  });
+  afterEach(function() {
+    jest.resetAllMocks();
+  });
+
+  it('view renders corectly - default case', function() {
+    testContext.init();
+    expect(testContext.view.el).toMatchSnapshot();
+  });
+
+  it('view renders correctly according to interstitialBeforeLoginRedirect', function() {
+    settings = new Settings({ 
+      baseUrl: 'http://localhost:3000',
+      'interstitialBeforeLoginRedirect': INTERSTITIAL_REDIRECT_VIEW.NONE
+    });    
+    testContext.init();
+    expect(testContext.view.el).toMatchSnapshot('should NOT render spinner');
+    
+
+    settings = new Settings({ 
+      baseUrl: 'http://localhost:3000',
+      'interstitialBeforeLoginRedirect': INTERSTITIAL_REDIRECT_VIEW.DEFAULT
+    });    
+    testContext.init();
+    expect(testContext.view.el).toMatchSnapshot('should have spinner');
+  });
+});
