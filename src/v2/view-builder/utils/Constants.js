ec97fc1b1d3f3ef2da4dade9b41f1c224e071404
vakaevich-okta <vakaevich@okta.com>
2021-07-27 16:43:37 +0300

    Increasing poll timeout to 5min. Resolves OKTA-406443

--- src/v2/view-builder/utils/Constants.js
+++ src/v2/view-builder/utils/Constants.js
@@ -1,5 +1,6 @@
 export const SHOW_RESEND_TIMEOUT = 30000;
 export const WARNING_TIMEOUT = 30000;
+export const CHALLENGE_TIMEOUT = 300000;
 export const MS_PER_SEC = 1000;
 export const UNIVERSAL_LINK_POST_DELAY = 500;
 export const CANCEL_POLLING_ACTION = 'authenticatorChallenge-cancel';
