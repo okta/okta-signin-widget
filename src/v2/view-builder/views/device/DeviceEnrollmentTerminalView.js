b5d19abc09115e411dc28d67ce93ee7d778533e4
Jintai He <jhe@okta.com>
2021-09-20 23:37:45 -0700

    feature: Add UIs for when the app link approach fails. OKTA-426211

--- src/v2/view-builder/views/device/DeviceEnrollmentTerminalView.js
+++ src/v2/view-builder/views/device/DeviceEnrollmentTerminalView.js
@@ -1,50 +1,125 @@
 import { loc } from 'okta';
 import { BaseForm, BaseView } from '../../internals';
-import OdaOktaVerifyTerminalView from '../../components/OdaOktaVerifyTerminalView';
+import { IosAndAndroidLoopbackOdaTerminalView,
+  AndroidAppLinkWithAccountOdaTerminalView,
+  AndroidAppLinkWithoutAccountOdaTerminalView,
+  getDeviceEnrollmentContext} from '../../components/OdaOktaVerifyTerminalView';
 import MdmOktaVerifyTerminalView from '../../components/MdmOktaVerifyTerminalView';
 import Enums from 'util/Enums';
 import OktaVerifyAuthenticatorHeader from '../../components/OktaVerifyAuthenticatorHeader';
+import Link from '../../components/Link';
 
 const BaseDeviceEnrollTerminalForm = BaseForm.extend({
   noButtonBar: true,
   className: 'device-enrollment-terminal',
 });
 
-const OdaTerminalForm = BaseDeviceEnrollTerminalForm.extend({
-  title() {
-    return loc('enroll.title.oda', 'login');
-  },
+const AndroidAppLinkPreselectForm = BaseForm.extend({
+  attributes: { 'data-se': 'android-app-link-setup-options-terminal' },
+  title: loc('enroll.title.oda.with.account', 'login'),
   initialize() {
     BaseForm.prototype.initialize.apply(this, arguments);
-    this.add(OdaOktaVerifyTerminalView);
+    const deviceEnrollmentContext = getDeviceEnrollmentContext(this.options.appState.get('deviceEnrollment'));
+    this.model.set('hasOVAccount', 'no');
+    this.addInput({
+      label: () => loc('enroll.subtitle.fastpass', 'login', [ deviceEnrollmentContext.orgName ]),
+      'label-top': true,
+      options: {
+        'no': loc('enroll.option.noaccount.fastpass', 'login'),
+        'yes': loc('enroll.option.account.fastpass', 'login'),
+      },
+      name: 'hasOVAccount',
+      type: 'radio',
+    });
+  },
+
+  saveForm() {
+    //remove any existing warnings or messages before saving form
+    this.$el.find('.o-form-error-container').empty();
+    this.options.appState.trigger('updateDeviceEnrollmentView', this.model.get('hasOVAccount') === 'yes');
+  }
+});
+
+const AndroidAppLinkWithAccountOdaTerminalForm = BaseDeviceEnrollTerminalForm.extend({
+  attributes: { 'data-se': 'android-oda-app-link-with-ov-account-terminal' },
+  title: loc('enroll.title.oda.with.account', 'login'),
+  initialize() {
+    BaseDeviceEnrollTerminalForm.prototype.initialize.apply(this, arguments);
+    this.add(AndroidAppLinkWithAccountOdaTerminalView);
+  },
+});
+
+const AndroidAppLinkWithoutAccountOdaTerminalForm = BaseDeviceEnrollTerminalForm.extend({
+  attributes: { 'data-se': 'android-oda-app-link-without-ov-account-terminal' },
+  title: loc('enroll.title.oda.without.account', 'login'),
+  initialize() {
+    BaseDeviceEnrollTerminalForm.prototype.initialize.apply(this, arguments);
+    this.add(AndroidAppLinkWithoutAccountOdaTerminalView);
+  },
+});
+
+const IosAndAndroidLoopbackOdaTerminalForm = BaseDeviceEnrollTerminalForm.extend({
+  attributes: { 'data-se': 'loopback-terminal' },
+  title: loc('enroll.title.oda', 'login'),
+  initialize() {
+    BaseDeviceEnrollTerminalForm.prototype.initialize.apply(this, arguments);
+    this.add(IosAndAndroidLoopbackOdaTerminalView);
   },
 });
 
 const MdmTerminalForm = BaseDeviceEnrollTerminalForm.extend({
-  title() {
-    return loc('enroll.title.mdm', 'login');
-  },
+  attributes: { 'data-se': 'mdm-terminal' },
+  title: loc('enroll.title.mdm', 'login'),
   initialize() {
-    BaseForm.prototype.initialize.apply(this, arguments);
+    BaseDeviceEnrollTerminalForm.prototype.initialize.apply(this, arguments);
     this.add(MdmOktaVerifyTerminalView);
   },
+});
+
+const AndroidAppLinkTerminalViewFooter = Link.extend({
+  postRender() {
+    this.$el.click((event) => {
+      event.preventDefault();
+      this.options.appState.trigger('switchBackToPreselect');
+    });
+  }
 });
 
 export default BaseView.extend({
   initialize() {
     BaseView.prototype.initialize.apply(this, arguments);
+    this.listenTo(this.options.appState, 'updateDeviceEnrollmentView', this.handleUpdateDeviceEnrollmentView);
+    this.listenTo(this.options.appState, 'switchBackToPreselect', this.handleSwitchBackToPreselect);
 
-    const deviceEnrollment = this.options.appState.get('deviceEnrollment');
-    this.enrollmentType = (deviceEnrollment.name || '').toLowerCase(); // oda/mdm
+    const deviceEnrollmentContext = getDeviceEnrollmentContext(this.options.appState.get('deviceEnrollment'));
+    this.enrollmentType = (deviceEnrollmentContext.enrollmentType|| '').toLowerCase(); // oda/mdm
 
     switch (this.enrollmentType) {
     case Enums.ODA:
       this.Header = OktaVerifyAuthenticatorHeader;
-      this.Body = OdaTerminalForm;
+      this.Body = deviceEnrollmentContext.isAndroidAppLink ?
+        AndroidAppLinkPreselectForm : IosAndAndroidLoopbackOdaTerminalForm;
       break;
     case Enums.MDM:
       this.Body = MdmTerminalForm;
       break;
     }
-  }
+  },
+
+  handleUpdateDeviceEnrollmentView(withAccount) {
+    this.Body = withAccount ? AndroidAppLinkWithAccountOdaTerminalForm : AndroidAppLinkWithoutAccountOdaTerminalForm;
+    this.backLink = this.add(AndroidAppLinkTerminalViewFooter, {
+      options: {
+        name: 'back-to-preselect',
+        label: loc('oform.back', 'login'),
+      }
+    }).last();
+    this.renderForm();
+  },
+
+  handleSwitchBackToPreselect() {
+    this.Body = AndroidAppLinkPreselectForm;
+    this.backLink && this.backLink.remove();
+    this.render();
+  },
 });
--- test/testcafe/spec/DeviceEnrollmentTerminalView_spec.js
+++ test/testcafe/spec/DeviceEnrollmentTerminalView_spec.js
@@ -2,7 +2,7 @@ import {RequestLogger, RequestMock} from 'testcafe';
 import { renderWidget as rerenderWidget } from '../framework/shared';
 import DeviceEnrollmentTerminalPageObject from '../framework/page-objects/DeviceEnrollmentTerminalPageObject';
 import IOSOdaEnrollment from '../../../playground/mocks/data/idp/idx/oda-enrollment-ios';
-import AndroidOdaEnrollment from '../../../playground/mocks/data/idp/idx/oda-enrollment-android';
+import AndroidOdaEnrollmentLoopback from '../../../playground/mocks/data/idp/idx/oda-enrollment-android';
 import MdmEnrollment from '../../../playground/mocks/data/idp/idx/mdm-enrollment';
 
 const logger = RequestLogger(/introspect/);
@@ -10,9 +10,9 @@ const logger = RequestLogger(/introspect/);
 const iosOdaMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/introspect')
   .respond(IOSOdaEnrollment);
-const androidOdaMock = RequestMock()
+const androidOdaLoopbackMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/introspect')
-  .respond(AndroidOdaEnrollment);
+  .respond(AndroidOdaEnrollmentLoopback);
 const mdmMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/introspect')
   .respond(MdmEnrollment);
@@ -46,7 +46,7 @@ test
   });
 
 test
-  .requestHooks(logger, androidOdaMock)('shows the correct content in Android ODA terminal view', async t => {
+  .requestHooks(logger, androidOdaLoopbackMock)('shows the correct content in Android ODA Loopback terminal view', async t => {
     const deviceEnrollmentTerminalPage = await setup(t);
     await t.expect(deviceEnrollmentTerminalPage.getHeader()).eql('Download Okta Verify');
     await t.expect(deviceEnrollmentTerminalPage.getBeaconClass()).contains('mfa-okta-verify');
@@ -74,13 +74,13 @@ test
     await t.expect(deviceEnrollmentTerminalPage.getCopiedValue()).eql('https://sampleEnrollmentlink.com');
   });
 
-// Below two tests are covering a special use case in ODA Universal Link flow
+// Below two tests are covering special use cases in ODA Universal Link/App Link flow
 // When the user clicks on the button, if Okta Verify is not installed it will be redirected to /authenticators/ov-not-installed
 // And the endpoint will return a new SIW with proxyIdxResponse in config
 // These tests are testing SIW can directly render terminal page when receiving such proxyIdxResponse instead of making introspect call
 // The mocks and device enrollment values are intentionally set differently from above tests to make sure the we properly consume SIW config
 test
-  .requestHooks(logger, mdmMock)('shows the correct content in iOS ODA terminal view when Okta Verify is not installed in Universal Link flow', async t => {
+  .requestHooks()('shows the correct content in iOS ODA terminal view when Okta Verify is not installed in Universal Link flow', async t => {
     const deviceEnrollmentTerminalPage = await setup(t);
     await rerenderWidget({
       'proxyIdxResponse': {
@@ -114,7 +114,7 @@ test
   });
 
 test
-  .requestHooks(logger, iosOdaMock)('shows the correct content in iOS MDM terminal view when Okta Verify is not installed in Universal Link flow', async t => {
+  .requestHooks()('shows the correct content in iOS MDM terminal view when Okta Verify is not set up in Universal Link flow', async t => {
     const deviceEnrollmentTerminalPage = await setup(t);
     await rerenderWidget({
       'proxyIdxResponse': {
@@ -126,6 +126,147 @@ test
             'enrollmentLink': 'https://anotherSampleEnrollmentlink.com',
             'vendor': 'MobileIron',
             'signInUrl': ''
+          }
+        }
+      }
+    });
+    await t.expect(deviceEnrollmentTerminalPage.getHeader()).eql('Additional setup required');
+    const content = deviceEnrollmentTerminalPage.getContentText();
+    await t.expect(content).contains('To access this app, your device needs to meet your organization');
+    await t.expect(content).contains('s security requirements. Follow the instructions below to continue.');
+    await t.expect(content).contains('Tap the Copy Link button below.');
+    await t.expect(content).contains('On this device, open your browser, then paste the copied link into the address bar.');
+    await t.expect(content).contains('Follow the instructions in your browser to set up MobileIron, then try accessing this app again');
+    await t.expect(deviceEnrollmentTerminalPage.getCopyButtonLabel()).eql('Copy link to clipboard');
+    await t.expect(deviceEnrollmentTerminalPage.getCopiedValue()).eql('https://anotherSampleEnrollmentlink.com');
+  });
+
+test
+  .requestHooks()('shows the correct content in Android ODA terminal view', async t => {
+    const deviceEnrollmentTerminalPage = await setup(t);
+    await rerenderWidget({
+      'proxyIdxResponse': {
+        'deviceEnrollment': {
+          'type': 'object',
+          'value': {
+            'name': 'oda',
+            'platform': 'ANDROID',
+            'enrollmentLink': '',
+            'vendor': '',
+            'signInUrl': 'https://rain.okta1.com',
+            'challengeMethod': 'APP_LINK',
+            'orgName': 'testOrg'
+          }
+        }
+      }
+    });
+    await t.expect(deviceEnrollmentTerminalPage.getHeader()).eql('Additional setup required to use Okta FastPass');
+    await t.expect(deviceEnrollmentTerminalPage.getBeaconClass()).contains('mfa-okta-verify');
+    const content = deviceEnrollmentTerminalPage.getContentText();
+    await t.expect(content).contains('On this device, do you already have an Okta Verify account for testOrg?');
+    await t.expect(content).contains('No, I don’t have an account');
+    await t.expect(content).contains('Yes, I already have an account');
+    await t.expect(deviceEnrollmentTerminalPage.getBackLink().exists).eql(false);
+
+    // switch to the next page when OV account is not setup
+    await deviceEnrollmentTerminalPage.clickWithoutOVAccount();
+    await deviceEnrollmentTerminalPage.clickNextButton();
+    await t.expect(deviceEnrollmentTerminalPage.getBackLink().exists).eql(true);
+
+    // go back then switch to the next page when OV account is setup
+    await deviceEnrollmentTerminalPage.clickBackLink();
+    await t.expect(deviceEnrollmentTerminalPage.getBackLink().exists).eql(false);
+    await deviceEnrollmentTerminalPage.clickWithOVAccount();
+    await deviceEnrollmentTerminalPage.clickNextButton();
+    await t.expect(deviceEnrollmentTerminalPage.getBackLink().exists).eql(true);
+
+    await deviceEnrollmentTerminalPage.clickBackLink();
+    await t.expect(deviceEnrollmentTerminalPage.getBackLink().exists).eql(false);
+    await t.expect(deviceEnrollmentTerminalPage.getHeader()).eql('Additional setup required to use Okta FastPass');
+  });
+
+test
+  .requestHooks()('shows the correct content in Android ODA terminal view when Okta Verify is not installed in App Link flow', async t => {
+    const deviceEnrollmentTerminalPage = await setup(t);
+    await rerenderWidget({
+      'proxyIdxResponse': {
+        'deviceEnrollment': {
+          'type': 'object',
+          'value': {
+            'name': 'oda',
+            'platform': 'ANDROID',
+            'enrollmentLink': '',
+            'vendor': '',
+            'signInUrl': 'https://rain.okta1.com',
+            'challengeMethod': 'APP_LINK',
+            'orgName': 'testOrg'
+          }
+        }
+      }
+    });
+    // switch to the next page when OV account is not setup
+    await deviceEnrollmentTerminalPage.clickWithoutOVAccount();
+    await deviceEnrollmentTerminalPage.clickNextButton();
+    await t.expect(deviceEnrollmentTerminalPage.getHeader()).eql('Set up an Okta Verify account');
+    await t.expect(deviceEnrollmentTerminalPage.getSubHeader()).eql('To sign in with Okta FastPass, you’ll need to set up Okta Verify on this device.');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(1)).contains('If you don’t have Okta Verify installed,');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(1)).contains('download the app.');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(2)).eql('Open Okta Verify and follow the steps to add your account.');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(3)).contains('When prompted, choose Sign In, then enter the sign-in URL:');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(3)).contains('https://rain.okta1.com');
+    await t.expect(deviceEnrollmentTerminalPage.getCopyOrgLinkButtonLabel()).eql('Copy sign-in URL to clipboard');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(4)).eql('Finish setting up your account in Okta Verify, then try signing in again.');
+    await t.expect(deviceEnrollmentTerminalPage.getBackLink().exists).eql(true);
+    await t.expect(deviceEnrollmentTerminalPage.getBackLinkText()).eql('Back');
+  });
+
+test
+  .requestHooks()('shows the correct content in Android ODA terminal view when Okta Verify is installed in App Link flow', async t => {
+    const deviceEnrollmentTerminalPage = await setup(t);
+    await rerenderWidget({
+      'proxyIdxResponse': {
+        'deviceEnrollment': {
+          'type': 'object',
+          'value': {
+            'name': 'oda',
+            'platform': 'ANDROID',
+            'enrollmentLink': '',
+            'vendor': '',
+            'signInUrl': 'https://rain.okta1.com',
+            'challengeMethod': 'APP_LINK',
+            'orgName': 'testOrg'
+          }
+        }
+      }
+    });
+    // go back then switch to the next page when OV account is setup
+    await deviceEnrollmentTerminalPage.clickWithOVAccount();
+    await deviceEnrollmentTerminalPage.clickNextButton();
+    await t.expect(deviceEnrollmentTerminalPage.getHeader()).eql('Additional setup required to use Okta FastPass');
+    await t.expect(deviceEnrollmentTerminalPage.getSubHeader()).eql('Okta FastPass is a security method that can sign you in without needing your username.');
+    await t.expect(deviceEnrollmentTerminalPage.getTextContent('.subtitle ')).eql('Already have Okta FastPass enabled for your account?');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(1)).eql('On this device, open the Okta Verify app.');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(2)).eql('On the list of accounts, tap your account for https://rain.okta1.com.');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(3)).eql('Under the “Okta FastPass” section, tap Setup, then follow the instructions.');
+    await t.expect(deviceEnrollmentTerminalPage.getContentByIndex(4)).eql('After your setup is complete, return here to try signing in again.');
+    await t.expect(deviceEnrollmentTerminalPage.getBackLink().exists).eql(true);
+    await t.expect(deviceEnrollmentTerminalPage.getBackLinkText()).eql('Back');
+  });
+
+test
+  .requestHooks()('shows the correct content in ANDROID MDM terminal view when Okta Verify is not installed in App Link flow', async t => {
+    const deviceEnrollmentTerminalPage = await setup(t);
+    await rerenderWidget({
+      'proxyIdxResponse': {
+        'deviceEnrollment': {
+          'type': 'object',
+          'value': {
+            'name': 'mdm',
+            'platform': 'ANDROID',
+            'enrollmentLink': 'https://anotherSampleEnrollmentlink.com',
+            'vendor': 'MobileIron',
+            'signInUrl': '',
+            'challengeMethod': 'APP_LINK',
           }
         }
       }
