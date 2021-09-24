e927fc468d599f02b0640aea89502ef492e6a090
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-09-24 13:14:02 +0300

    chore: fail build when yarn.lock contains restricted URL

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,6 +7,12 @@ export TEST_RESULT_FILE_DIR="${REPO}/build2"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
+RESTRICTED_URL_SECTION='npm-okta-all\|npm-okta-master'
+if grep -q "${RESTRICTED_URL_SECTION}" "${REPO}/yarn.lock"; then
+  echo "lint failed! yarn.lock contains restricted URL. Exiting..."
+  exit 1
+fi
+
 if ! yarn lint:report; then
   echo "lint failed! Exiting..."
   # PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL will invoke bacon to parse results and display number of tests
