ac2ecade99b9f8b10bbc53483f11a16aff1db79c
Aaron Granick <aaron.granick@okta.com>
2021-09-30 15:51:51 +0000

    update README for release 5.12.0 Update .widgetrc.sample.js
    OKTA-431777
    <<<Jenkins Check-In of Tested SHA: f3aa012d1c337ffdf3f553239ff300493b5aca7f for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 2
    PR Link: "https://github.com/okta/okta-signin-widget/pull/2229"

--- .widgetrc.sample.js
+++ .widgetrc.sample.js
@@ -1,17 +1,4 @@
-function createDummyHook(name, waitTimeMs) {
-  // Hook functions receive no parameters. They may return a promise but the value is not used.
-  return function() {
-    console.log('hook started: ' + name);
-    return new Promise(resolve => {
-      setTimeout(() => {
-        console.log('hook finished: ' + name);
-        resolve();
-      }, waitTimeMs);
-    });
-  };
-}
-
-module.exports = {
+const config = {
   baseUrl: 'http://localhost:3000',
   logo: '/img/logo_widgico.png',
   logoText: 'Windico',
@@ -42,3 +29,18 @@ module.exports = {
     }
   }
 };
+
+function createDummyHook(name, waitTimeMs) {
+  // Hook functions receive no parameters. They may return a promise but the value is not used.
+  return function() {
+    console.log('hook started: ' + name);
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('hook finished: ' + name);
+        resolve();
+      }, waitTimeMs);
+    });
+  };
+}
+
+module.exports = config;
