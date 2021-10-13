91ff41637a9abab9f205a41d8ebe49e3f0c522f8
Shaw Yu <shaw.yu@okta.com>
2021-10-13 00:17:54 +0000

    Wraps logo in h1 to provide semantic heading info to screenreaders
    OKTA-430954
    <<<Jenkins Check-In of Tested SHA: fe1741a5267bd9ea5415126cce8304342c424368 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 4
    PR Link: "https://github.com/okta/okta-signin-widget/pull/2238"

--- playground/mocks/spec-okta-api/app/userHome.js
+++ playground/mocks/spec-okta-api/app/userHome.js
@@ -7,7 +7,7 @@ const apis = [
     method: 'GET',
     template(params, query) {
       return `
-        <h1>Mock User Dashboard</h1>
+        <h1 id="mock-user-dashboard-title">Mock User Dashboard</h1>
         <h2>Query parameters</h2>
         <pre id="preview-query">${JSON.stringify(query, null, 2)}</pre>
         <a href="/">Back to Login</a>`;
