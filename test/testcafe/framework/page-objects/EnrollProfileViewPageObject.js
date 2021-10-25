757c48a36f6621fdefbd1fde935e404340e88eee
Fang Shi <fang.shi@okta.com>
2021-10-25 13:27:26 -0700

    fs-435137-fix-feedback-from-review

--- test/testcafe/framework/page-objects/EnrollProfileViewPageObject.js
+++ test/testcafe/framework/page-objects/EnrollProfileViewPageObject.js
@@ -4,8 +4,5 @@ export default class EnrollProfileViewPageObject extends BasePageObject {
   constructor(t) {
     super(t);
   }
-
-  clickSubmitButton() {
-    return this.form.clickSaveButton();
-  }
+  
 }
