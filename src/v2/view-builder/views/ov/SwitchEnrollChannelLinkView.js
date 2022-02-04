83425d633e0388e6817b9fa4e4cebcc5c192a82d
Lokesh Hemrajani <92340204+lokeshhemrajani-okta@users.noreply.github.com>
2022-02-04 09:54:32 -0500

    Revert "OKTA-461740 added desc and aria to support i11y"
    This reverts commit 55e2ae26b90f7222d2dd63d0928c7ca6a438eee1.

--- src/v2/view-builder/views/ov/SwitchEnrollChannelLinkView.js
+++ src/v2/view-builder/views/ov/SwitchEnrollChannelLinkView.js
@@ -6,7 +6,7 @@ export default View.extend({
   className: 'switch-channel-text',
   template: hbs `
     {{#if isQRcodeChannel}}
-      <a href="#" class="switch-channel-link" aria-label="{{i18n code="enroll.totp.aria.cannotScan" bundle="login" }}">
+      <a href="#" class="switch-channel-link">
         {{i18n code="enroll.totp.cannotScan" bundle="login"}}</a>
     {{else}}
       {{{i18n code="oie.enroll.okta_verify.switch.channel.link.text" bundle="login"}}}
