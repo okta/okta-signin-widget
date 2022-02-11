81325d06e8851815649feae5c8bab836721af078
Shuo Wu <wushuo2010@gmail.com>
2022-02-11 16:52:17 -0500

    test beta @okta/okta-auth-js@6.2.0-g16cdcbf

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,7 @@
+# For siw-coordinator ONLY !!!
+# Copy this script to scripts/setup.sh and replace `AUTHJS_BETA_VERSION` with correct beta version to test downstream in bacon
+# DO NOT MERGE CHANGES IN scripts/setup.sh !!!
+
 #!/bin/bash
 
 # Install required node version
@@ -8,6 +12,16 @@ setup_service node v12.13.0
 setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
 
 cd ${OKTA_HOME}/${REPO}
+
+# INSTALL BETA VERSION - script outside this block should be identical with setup.sh
+echo "Installing BETA VERSION"
+
+npm config set strict-ssl false
+
+yarn add -DW --no-lockfile https://artifacts.aue1d.saasure.com/artifactory/npm-topic/@okta/okta-auth-js/-/@okta/okta-auth-js@6.2.0-g16cdcbf.tgz
+
+echo "BETA VERSION installed"
+# END INSTALL BETA VERSION
 
 if ! yarn install ; then
   echo "yarn install failed! Exiting..."
