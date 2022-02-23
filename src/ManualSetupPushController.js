e573bcb6cb4ad5d37435c117a51a7ec6e90d9280
Praveen Durai <praveen.durai@okta.com>
2022-02-23 17:47:10 -0500

    OKTA-471085 updated text

--- src/ManualSetupPushController.js
+++ src/ManualSetupPushController.js
@@ -185,10 +185,10 @@ export default FormController.extend({
               <p class="sr-only">{{i18n code="enroll.totp.sharedSecretInstructions.aria.intro" bundle="login"}}</p>
               <ol>
                 <li>{{step1Instruction}}</li>
-                <li>{{i18n code="enroll.totp.sharedSecretInstructions.step2" bundle="login"
-                $1="<strong>$1</strong>"}}</li>
+                <li>{{i18n code="enroll.totp.sharedSecretInstructions.step2" bundle="login"}}</li>
                 <li>{{i18n code="enroll.totp.sharedSecretInstructions.step3" bundle="login" 
-                $1="<strong>$1</strong>"}}</li>
+                $1="<strong>$1</strong>" $2="<strong>$2</strong>"}}</li>
+                <li>{{i18n code="enroll.totp.sharedSecretInstructions.step4" bundle="login"}}</li>
               </ol>
               <p class="shared-key margin-top-10" tabindex=0 
                 aria-label="{{i18n code="enroll.totp.sharedSecretInstructions.aria.secretKey" bundle="login"}} 
