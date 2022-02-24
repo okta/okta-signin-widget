d4cc3e7d9daa91f0f59552e9ba76ac290069792a
Praveen Durai <praveen.durai@okta.com>
2022-02-23 19:28:31 -0500

    OKTA-471085 updated property key

--- src/ManualSetupPushController.js
+++ src/ManualSetupPushController.js
@@ -140,7 +140,7 @@ export default FormController.extend({
 
       return loc('enroll.totp.title', 'login', [factorName]);
     },
-    subtitle: _.partial(loc, 'enroll.totp.cannotScanBarcode', 'login'),
+    subtitle: _.partial(loc, 'enroll.totp.moreOption', 'login'),
     noButtonBar: true,
     attributes: { 'data-se': 'step-manual-setup' },
 
