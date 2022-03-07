2a55b2868b475d6e2cd1673a8e78eb57a6e29e8d
Justin Abrokwah <justin.abrokwah@okta.com>
2022-03-07 14:43:39 -0500

    Refactoring base attribute i18n mappings

--- src/v2/ion/i18nTransformer.js
+++ src/v2/ion/i18nTransformer.js
@@ -46,6 +46,7 @@ import Bundles from 'util/Bundles';
 import Logger from 'util/Logger';
 import { getAuthenticatorDisplayName } from '../view-builder/utils/AuthenticatorUtil';
 import { FORMS, AUTHENTICATOR_KEY } from './RemediationConstants';
+import { I18N_BASE_ATTRIBUTE_ENROLL_PROFILE_MAPPINGS } from './utils/i18nBaseAttributeMappings';
 
 const WEBAUTHN_API_GENERIC_ERROR_KEY = 'authfactor.webauthn.error';
 
@@ -142,39 +143,6 @@ const I18N_OVERRIDE_MAPPINGS = {
   'reenroll-authenticator.okta_password.credentials.passcode': 'oie.password.newPasswordLabel',
   'reenroll-authenticator-warning.okta_password.credentials.passcode': 'oie.password.newPasswordLabel',
   'incorrectPassword': 'oie.password.incorrect.message',
-
-  // Base Attributes
-  'enroll-profile.userProfile.lastName': 'oie.user.profile.lastname',
-  'enroll-profile.userProfile.firstName': 'oie.user.profile.firstname',
-  'enroll-profile.userProfile.email': 'oie.user.profile.primary.email',
-  'enroll-profile.userProfile.secondEmail': 'oie.user.profile.secondary.email',
-  'enroll-profile.userProfile.middleName': 'oie.user.profile.middleName',
-  'enroll-profile.userProfile.honorificPrefix': 'oie.user.profile.honorificPrefix',
-  'enroll-profile.userProfile.honorificSuffix': 'oie.user.profile.honorificSuffix',
-  'enroll-profile.userProfile.title': 'oie.user.profile.title',
-  'enroll-profile.userProfile.displayName': 'oie.user.profile.displayName',
-  'enroll-profile.userProfile.nickName': 'oie.user.profile.nickName',
-  'enroll-profile.userProfile.profileUrl': 'oie.user.profile.profileUrl',
-  'enroll-profile.userProfile.mobilePhone': 'oie.user.profile.mobilePhone',
-  'enroll-profile.userProfile.primaryPhone': 'oie.user.profile.primaryPhone',
-  'enroll-profile.userProfile.streetAddress': 'oie.user.profile.streetAddress',
-  'enroll-profile.userProfile.city': 'oie.user.profile.city',
-  'enroll-profile.userProfile.state': 'oie.user.profile.state',
-  'enroll-profile.userProfile.zipCode': 'oie.user.profile.zipCode',
-  'enroll-profile.userProfile.postalCode': 'oie.user.profile.postalCode',
-  'enroll-profile.userProfile.countryCode': 'oie.user.profile.countryCode',
-  'enroll-profile.userProfile.postalAddress': 'oie.user.profile.postalAddress',
-  'enroll-profile.userProfile.preferredLanguage': 'oie.user.profile.preferredLanguage',
-  'enroll-profile.userProfile.locale': 'oie.user.profile.locale',
-  'enroll-profile.userProfile.timezone': 'oie.user.profile.timezone',
-  'enroll-profile.userProfile.userType': 'oie.user.profile.userType',
-  'enroll-profile.userProfile.employeeNumber': 'oie.user.profile.employeeNumber',
-  'enroll-profile.userProfile.costCenter': 'oie.user.profile.costCenter',
-  'enroll-profile.userProfile.organization': 'oie.user.profile.organization',
-  'enroll-profile.userProfile.division': 'oie.user.profile.division',
-  'enroll-profile.userProfile.department': 'oie.user.profile.department',
-  'enroll-profile.userProfile.managerId': 'oie.user.profile.managerId',
-  'enroll-profile.userProfile.manager': 'oie.user.profile.manager',
   
   'profile-update.userProfile.secondEmail': 'oie.user.profile.secondary.email',
 
@@ -299,6 +267,9 @@ const getI18nKey = (i18nPath) => {
 
   if (I18N_OVERRIDE_MAPPINGS[i18nPath]) {
     i18nKey = I18N_OVERRIDE_MAPPINGS[i18nPath];
+  }
+  if (I18N_BASE_ATTRIBUTE_ENROLL_PROFILE_MAPPINGS[i18nPath]) {
+    i18nKey = I18N_BASE_ATTRIBUTE_ENROLL_PROFILE_MAPPINGS[i18nPath];
   }
 
   if (i18nKey && !Bundles.login[i18nKey]) {
