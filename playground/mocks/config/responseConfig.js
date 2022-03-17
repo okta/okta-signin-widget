3d29f686025d1b13b3603bcc38971bd04386b3a6
Praveen Durai <praveen.durai@okta.com>
2022-03-17 11:29:32 -0400

    OKTA-454593 dummy commit for test

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -1032,7 +1032,7 @@ const selectOktaVerifyMethod = {
     'authenticator-verification-okta-verify-push-autoChallenge-on',
   ],
   '/idp/idx/authenticators/poll': [
-    'authenticator-verification-okta-verify-push-autoChallenge-on',
+    'authenticator-verification-okta-verify-push-autoChallenge-o',
   ],
 };
 
