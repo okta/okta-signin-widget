4c0aeee57adbb51d4ce9d5aa39f4be6a439b4ebd
Aaron Granick <aaron.granick@okta.com>
2022-03-29 12:31:46 -0700

    fix: english leak custom app auto push

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -18,7 +18,8 @@ const idx = {
   ],
 
   '/idp/idx/introspect': [
-    'identify',
+    'authenticator-verification-custom-app-push-autochallenge'
+    // 'identify',
     // 'error-identify-multiple-errors',
     // 'authenticator-enroll-ov-qr-enable-biometrics',
     // 'authenticator-verification-okta-verify-push',
