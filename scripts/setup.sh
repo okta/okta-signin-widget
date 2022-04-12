57d2d68721a0e198f5890ad4c6b3e910f710e9fa
Aaron Granick <aaron.granick@okta.com>
2022-04-12 11:11:28 -0700

    setup downstream build script for authJS

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Can be used to run a canary build against a beta AuthJS version that has been published to artifactory.
+# This is available from the "downstream artifact" menu on any okta-auth-js build in Bacon.
+# DO NOT MERGE ANY CHANGES TO THIS LINE!!
+export AUTHJS_VERSION=""
+
 # Install required node version
 export REGISTRY_REPO="npm-topic"
 export REGISTRY="${ARTIFACTORY_URL}/api/npm/${REGISTRY_REPO}"
@@ -8,6 +13,18 @@ setup_service node v12.13.0
 setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
 
 cd ${OKTA_HOME}/${REPO}
+
+if [ ! -z "$AUTHJS_VERSION" ]; then
+  echo "Installing AUTHJS_VERSION: ${AUTHJS_VERSION}"
+  npm config set strict-ssl false
+
+  if ! yarn add -DW --no-lockfile https://artifacts.aue1d.saasure.com/artifactory/npm-topic/@okta/okta-auth-js/-/@okta/okta-auth-js-${AUTHJS_VERSION}.tgz ; then
+    echo "AUTHJS_VERSION could not be installed: ${AUTHJS_VERSION}"
+    exit ${FAILED_SETUP}
+  fi
+  
+  echo "AUTHJS_VERSION installed: ${AUTHJS_VERSION}"
+fi
 
 if ! yarn install ; then
   echo "yarn install failed! Exiting..."
