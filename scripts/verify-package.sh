f9aebaf47b26978e5588484ebe9a6132b1358ad7
Aaron Granick <aaron.granick@okta.com>
2022-04-14 16:13:49 -0700

    separate publish and verify package, reorder bacon tasks

--- scripts/verify-package.sh
+++ scripts/verify-package.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -xe
 
+source $OKTA_HOME/$REPO/scripts/setup.sh
+
+export PATH="${PATH}:$(yarn global bin)"
+export TEST_SUITE_TYPE="build"
+
+# Build
+if ! yarn build:release; then
+  echo "build failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
+
 mkdir -p test-reports
 
 pushd dist
