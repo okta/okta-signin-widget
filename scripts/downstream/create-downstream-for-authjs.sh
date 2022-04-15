38820415caf2232696e2ecdec39b7842e3a11345
Aaron Granick <aaron.granick@okta.com>
2022-04-15 15:26:33 -0700

    use force to update package.json

--- scripts/downstream/create-downstream-for-authjs.sh
+++ scripts/downstream/create-downstream-for-authjs.sh
@@ -14,13 +14,17 @@ pushd ${OKTA_HOME}/okta-signin-widget/scripts > /dev/null
 AUTHJS_VERSION="$(echo ${upstream_artifact_version} | cut -d'@' -f3)"
 
 # Update setup script
-# echo "Update okta-auth-js version in scripts/setup.sh to ${AUTHJS_VERSION}"
-# sed -i "s/\(AUTHJS_VERSION\=\).*/\1\"${AUTHJS_VERSION}\"/g" setup.sh
-# popd > /dev/null
+echo "Update okta-auth-js version in scripts/setup.sh to ${AUTHJS_VERSION}"
+sed -i "s/\(AUTHJS_VERSION\=\).*/\1\"${AUTHJS_VERSION}\"/g" setup.sh
 
-# Also install it so that package.json is updated
-npm config set strict-ssl false
-if ! yarn add -DW https://artifacts.aue1d.saasure.com/artifactory/npm-topic/@okta/okta-auth-js/-/@okta/okta-auth-js-${AUTHJS_VERSION}.tgz ; then
-  echo "AUTHJS_VERSION could not be installed: ${AUTHJS_VERSION}"
-  exit ${FAILED_SETUP}
-fi
+popd > /dev/null
+
+# Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
+# setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
+
+# # Also install it so that package.json is updated
+# npm config set strict-ssl false
+# if ! yarn add -DW https://artifacts.aue1d.saasure.com/artifactory/npm-topic/@okta/okta-auth-js/-/@okta/okta-auth-js-${AUTHJS_VERSION}.tgz ; then
+#   echo "AUTHJS_VERSION could not be installed: ${AUTHJS_VERSION}"
+#   exit ${FAILED_SETUP}
+# fi
