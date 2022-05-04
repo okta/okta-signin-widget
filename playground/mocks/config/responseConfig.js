3430ce5ad4c1316f8443cb5b48c6148711b6499e
Aaron Granick <aaron.granick@okta.com>
2022-05-04 16:17:59 -0700

    wip

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -219,8 +219,8 @@ const emailActivation = {
 const consent = {
   '/idp/idx/introspect': [
     // 'consent-admin',
-    'consent-enduser',
-    'consent-enduser2',
+    // 'consent-enduser',
+    'consent-enduser3',
   ],
   '/idp/idx/consent': [
     // note that the success 'href' is in reality a redirect (i.e. /login/token/redirect?stateToken={{stateToken}})
