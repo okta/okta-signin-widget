bf7b1ea3a12ec598055390d6a3a1e0476c39894a
Aaron Granick <aaron.granick@okta.com>
2022-05-24 19:23:12 +0000

    feat: upgrade to jQuery 3.6
    OKTA-422804
    <<<Jenkins Check-In of Tested SHA: e966d241023a971301e7a8363e52c23fc77babc3 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 31
    PR Link:

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -63,7 +63,6 @@ module.exports = function(outputFilename, mode = 'development') {
         'okta-i18n-bundles': 'util/Bundles',
 
         // Vendor files from courage that
-        'jquery': `${COURAGE_DIST}/src/courage/vendor/lib/jquery-1.12.4.js`,
         'handlebars/runtime': `${COURAGE_DIST}/lib/handlebars/dist/cjs/handlebars.runtime`,
         'handlebars$': `${COURAGE_DIST}/lib/handlebars/dist/cjs/handlebars.runtime`,
 
