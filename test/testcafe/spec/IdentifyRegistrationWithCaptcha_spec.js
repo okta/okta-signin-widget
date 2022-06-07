498254f60be71f3799ea889b3f44e2204da3dd9c
Aaron Granick <aaron.granick@okta.com>
2022-06-07 18:50:11 +0000

    disables hcaptcha tests
    OKTA-504996
    <<<Jenkins Check-In of Tested SHA: 7a1f7b828aeeccfb930118a1abd13dc6ef8b11b5 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 3
    PR Link: https://github.com/okta/okta-signin-widget/pull/2555

--- test/testcafe/spec/IdentifyRegistrationWithCaptcha_spec.js
+++ test/testcafe/spec/IdentifyRegistrationWithCaptcha_spec.js
@@ -56,7 +56,8 @@ test.requestHooks(reCaptchaRequestLogger, mockWithReCaptcha)('should be able to 
   await t.expect(req.url).contains('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI');
 });
 
-test.requestHooks(mockWithHCaptcha)('should be able to create account with hCaptcha enabled', async t => {
+// TODO: enable this test OKTA-504996
+test.requestHooks(mockWithHCaptcha).skip('should be able to create account with hCaptcha enabled', async t => {
   // mock is configured to show registration page immediately
   const registrationPage = new RegistrationPageObject(t);
   await registrationPage.navigateToPage();
