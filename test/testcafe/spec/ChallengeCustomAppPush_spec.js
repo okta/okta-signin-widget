43a4a00d8d8090a1229f59ba2be478aeb75712d6
Kenton Smith <kenton.smith@okta.com>
2022-06-18 07:28:21 +0000

    Hide autopush checkbox for custom push
    OKTA-506495
    <<<Jenkins Check-In of Tested SHA: 8d3fa995f9027894857a23ec46d0f395273f13a9 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 3
    PR Link:

--- test/testcafe/spec/ChallengeCustomAppPush_spec.js
+++ test/testcafe/spec/ChallengeCustomAppPush_spec.js
@@ -35,6 +35,10 @@ const pushWaitMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/challenge/poll')
   .respond(pushPoll);
 
+const pushNoAutoChallengeMock = RequestMock()
+  .onRequestTo('http://localhost:3000/idp/idx/introspect')
+  .respond(pushPoll);
+
 const pushAutoChallengeMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/introspect')
   .respond(pushPollAutoChallenge);
@@ -107,6 +111,9 @@ test
     await t.expect(checkboxLabel.hasClass('checked')).ok();
     await t.expect(checkboxLabel.textContent).eql('Send push automatically');
 
+    // make sure checkbox is visible
+    await t.expect(await challengeCustomAppPushPageObject.autoChallengeInputIsVisible()).ok();
+
     // unselect checkbox on click
     await challengeCustomAppPushPageObject.clickAutoChallengeCheckbox();
     await t.expect(checkboxLabel.hasClass('checked')).notOk();
@@ -117,6 +124,33 @@ test
     await t.expect(await challengeCustomAppPushPageObject.signoutLinkExists()).ok();
     await t.expect(challengeCustomAppPushPageObject.getSignoutLinkText()).eql('Back to sign in');
     
+  });
+
+test
+  .requestHooks(pushNoAutoChallengeMock)(`challenge custom app push screen does not have checkbox
+    when autoChallenge object is missing from IDX remediation response`, async t => {
+    
+    const challengeCustomAppPushPageObject = await setup(t);
+    
+    // all UI elements should be present except the checkbox
+    const pageTitle = challengeCustomAppPushPageObject.getPageTitle();
+    const pushBtn = challengeCustomAppPushPageObject.getPushButton();
+    const a11ySpan = challengeCustomAppPushPageObject.getA11ySpan();
+    const logoClass = challengeCustomAppPushPageObject.getBeaconClass();
+    await t.expect(pushBtn.textContent).contains('Push notification sent');
+    await t.expect(a11ySpan.textContent).contains('Push notification sent');
+    await t.expect(pushBtn.hasClass('link-button-disabled')).ok();
+    await t.expect(logoClass).contains('custom-app-logo');
+    await t.expect(pageTitle).contains('Verify with Custom Push');
+
+    // Verify links
+    await t.expect(await challengeCustomAppPushPageObject.switchAuthenticatorLinkExists()).ok();
+    await t.expect(challengeCustomAppPushPageObject.getSwitchAuthenticatorLinkText()).eql('Verify with something else');
+    await t.expect(await challengeCustomAppPushPageObject.signoutLinkExists()).ok();
+    await t.expect(challengeCustomAppPushPageObject.getSignoutLinkText()).eql('Back to sign in');
+
+    // assert checkbox not visible
+    await t.expect(await challengeCustomAppPushPageObject.autoChallengeInputIsVisible()).notOk();
   });
 
 test
