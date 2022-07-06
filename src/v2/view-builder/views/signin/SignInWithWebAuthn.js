3f491ee7e8ea0ba625f43195b0f7edf7be95d3d3
Xifeng Fang <87032825+xifengfang-okta@users.noreply.github.com>
2022-07-06 17:18:53 -0400

    remove WebAuthnWithoutInterstitial code

--- src/v2/view-builder/views/signin/SignInWithWebAuthn.js
+++ src/v2/view-builder/views/signin/SignInWithWebAuthn.js
@@ -22,74 +22,11 @@ export default View.extend({
       title: 'Use Touch ID',
       click: async () => {
         this.handleWebAuthnWithInterstitial();
-        //await this.handleWebAuthnWithoutInterstitial();
       }
     }), '.okta-verify-container');
   },
   handleWebAuthnWithInterstitial: function(){
     this.options.appState.trigger('invokeAction', FORMS.LAUNCH_WEBAUTHN_AUTHENTICATOR);
-  },
-  //this is no use for now.
-  handleWebAuthnWithoutInterstitial: async function(){
-    //get response from idx/webauthn/launch api call
-    const actionPath = "launch-webauthn-authenticator";
-    const { appState, settings } = this.options;
-    const idx = appState.get('idx');
-    const { stateHandle } = idx.context;
-    let invokeOptions = {exchangeCodeForTokens: false, shouldProceedWithEmailAuthenticator: false, stateHandle, step: actionPath };
-    const authClient = this.options.settings.getAuthClient();
-    let resp;
-    try {
-      resp = await authClient.idx.proceed(invokeOptions);
-      if (resp.requestDidSucceed === false) {
-        this.displayError(resp);
-        return;
-      }
-    } catch (e) {
-      this.displayError(e);
-      return;
-    }
-
-    //webauthn
-    const challengeData = resp.context.currentAuthenticator.value.contextualData.challengeData;
-    this.webauthnAbortController = new AbortController();
-    const options = _.extend({}, challengeData, {
-      allowCredentials: [],
-      challenge: CryptoUtil.strToBin(challengeData.challenge),
-    });
-    navigator.credentials.get({
-      publicKey: options,
-      signal: this.webauthnAbortController.signal
-    }).then(async (assertion) => {
-      //process idx response and set appState
-      const lastResponse = this.options.appState.get('idx');
-      if (lastResponse) {
-        sessionStorageHelper.setStateHandle(resp?.context?.stateHandle);
-      }
-      const ionResponse = transformIdxResponse(this.options.settings, resp, lastResponse);
-      this.options.appState.clearAppStateCache();
-      this.options.appState.set(ionResponse);
-
-      //process webauthn response
-      const userHandle = CryptoUtil.binToStr(assertion.response.userHandle);
-      console.log("userHandle: " + userHandle);
-      this.model.set({
-        formName: 'challenge-authenticator',
-        credentials : {
-          clientData: CryptoUtil.binToStr(assertion.response.clientDataJSON),
-          authenticatorData: CryptoUtil.binToStr(assertion.response.authenticatorData),
-          signatureData: CryptoUtil.binToStr(assertion.response.signature),
-          userHandle: userHandle,
-        }
-      });
-      this.options.appState.trigger('saveForm', this.model);
-    }, (error) => {
-      if (this.webauthnAbortController) {
-        this.displayError(error);
-      }
-    }).finally(() => {
-      this.webauthnAbortController = null;
-    });
   },
   displayError: function(error){
     console.log(error);
