98cb356f612e472fdd1e7d4bb238679c192c768d
Jared Perreault <jared.perreault@okta.com>
2022-07-06 11:58:39 -0400

    linter fix

--- test/app/babel.config.js
+++ test/app/babel.config.js
@@ -1,7 +1,7 @@
 const presets = [
   '@babel/preset-typescript',
   '@babel/preset-env'
-]
+];
 const plugins = ['@babel/plugin-transform-modules-commonjs'];
 
 // Do not include async generator in development bundle (debug on modern browser)
