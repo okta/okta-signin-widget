8284886ec70875a9427fd21e8a234d3eab7c2080
lesterchoi <lester.choi@okta.com>
2022-07-20 15:54:19 -0400

    add notes to bacon configs, scripts

--- scripts/v3-parity.sh
+++ scripts/v3-parity.sh
@@ -9,6 +9,7 @@ export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/junit"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
+# TODO target specs enabled in v3
 # ENABLED_TESTS=$(cat <<-END
 # test/testcafe/spec/IdentifyWithPassword_spec.js
 # test/testcafe/spec/Identify_spec.js
