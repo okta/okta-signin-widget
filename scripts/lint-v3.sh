1b0504484ddd75b1bed3b9f5193f714f6c87e01f
Shuo Wu <wushuo2010@gmail.com>
2022-07-20 16:33:05 -0400

    lint

--- scripts/lint-v3.sh
+++ scripts/lint-v3.sh
@@ -2,7 +2,7 @@
 
 source $OKTA_HOME/$REPO/scripts/setup.sh
 
-export TEST_SUITE_TYPE="junit"
+export TEST_SUITE_TYPE="checkstyle"
 export TEST_RESULT_FILE_DIR="${REPO}/src/v3/build2/reports/lint"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
@@ -12,4 +12,4 @@ if ! yarn workspace v3 lint:report; then
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
-exit $PUBLISH_TYPE_AND_RESULT_DIR;
+exit $PUBLISH_TYPE_AND_RESULT_DIR_BUT_SUCCEED_IF_NO_RESULTS;
