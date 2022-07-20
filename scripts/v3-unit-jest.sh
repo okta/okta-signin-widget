d6e2efe9eef19f61adbef88acbf8f0f125795b7a
lesterchoi <lester.choi@okta.com>
2022-07-20 16:29:19 -0400

    add bacon scripts to run v3 jest and testcafe

---
+++ scripts/v3-unit-jest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source $OKTA_HOME/$REPO/scripts/setup.sh
+
+export TEST_SUITE_TYPE="junit"
+export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/unit"
+echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
+echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
+
+if ! yarn workspace v3 test; then
+  echo "v3 jest tests failed! Exiting..."
+  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+fi
+
+exit $PUBLISH_TYPE_AND_RESULT_DIR;
