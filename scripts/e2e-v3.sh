258099e6bca7e43c994115af2a0b7097c7be4678
Shuo Wu <wushuo2010@gmail.com>
2022-07-20 14:41:12 -0400

    e2e

--- scripts/e2e-v3.sh
+++ scripts/e2e-v3.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-export CI=true
-setup_service google-chrome-stable 91.0.4472.77-1
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
-
-export TEST_SUITE_TYPE="junit"
-export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/e2e-v3"
+export TEST_SUITE_TYPE="jsunit"
+export TEST_RESULT_FILE_DIR=${REPO}/build2/reports/e2e
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
-if ! yarn workspace v3 test:e2e; then
-  echo "testcafe tests failed! Exiting..."
-  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
+
+cd ${OKTA_HOME}/${REPO}
+
+setup_service google-chrome-stable 91.0.4472.77-1
+
+function run_e2e() {
+  echo "Starting e2e test suite"
+
+  yarn workspace v3 test:e2e
+}
+
+export CI=true
+
+if ! run_e2e; then
+  echo "e2e test failure!"
+  report_results FAILURE publish_type_and_result_dir_but_always_fail
+  exit 1
 fi
 
-exit ${PUBLISH_TYPE_AND_RESULT_DIR};
+echo "e2e test passed!"
+report_results SUCCESS publish_type_and_result_dir_but_succeed_if_no_results
