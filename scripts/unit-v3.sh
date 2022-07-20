ddb490d85c71bab9d2edc47c9fa56c94ecb9653f
Shuo Wu <wushuo2010@gmail.com>
2022-07-20 14:39:22 -0400

    unit

--- scripts/unit-v3.sh
+++ scripts/unit-v3.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
-export TEST_SUITE_TYPE="junit"
-export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/unit-v3"
-echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
-echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
+export ARTIFACT_PATH="${OKTA_HOME}/${REPO}"
 
-if ! yarn workspace v3 test; then
-  echo "v3 jest tests failed! Exiting..."
-  exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
-fi
-
-exit $PUBLISH_TYPE_AND_RESULT_DIR;
+use_script_template npm-unit
