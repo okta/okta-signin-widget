09966c53a6e336e6d84eef122e1b73a1a49a95e1
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2022-07-27 19:51:58 -0400

    Updating scripts to generate required config files

--- scripts/unit-v3.sh
+++ scripts/unit-v3.sh
@@ -7,6 +7,12 @@ export TEST_RESULT_FILE_DIR="${REPO}/src/v3/build2/reports/unit"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
+# generated code is required
+if ! yarn codegen; then
+  echo "build codegen failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
+
 if ! yarn workspace v3 test --no-colors; then
   echo "v3 jest tests failed! Exiting..."
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
