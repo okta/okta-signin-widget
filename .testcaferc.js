d65f48c7afdc4db5a46eabdcf7accfe04ec03905
Lester Choi <lester.choi@okta.com>
2022-08-02 17:58:59 -0400

    consolidate testcafe configs

--- .testcaferc.js
+++ .testcaferc.js
@@ -1,13 +1,17 @@
-const defaultConfigs = require('./.testcaferc.json');
-
 module.exports = {
-  ...defaultConfigs,
+  browsers: [
+    "chrome"
+  ],
+  src: "test/testcafe/spec/",
+
   /*
   * NOTE: add a testcafe fixture to the list of specs to run for parity testing
   * by adding fixture metadata {"v3": true}. See example in
   * test/testcafe/spec/Smoke_spec.js
   */
   filter: (testName, fixtureName, fixturePath, testMeta, fixtureMeta) => {
-    return fixtureMeta.v3 === true && testMeta.v3 !== false;
+    return !process.env.OKTA_SIW_V3 || (
+      fixtureMeta.v3 === true && testMeta.v3 !== false
+    );
   },
 };
