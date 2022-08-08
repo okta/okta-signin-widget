6d083195306ccc028d21de7a35ff8988d3cdcd79
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2022-08-08 14:36:22 -0400

    Implement customized key functionality

--- src/v3/src/transformer/identify/transformIdentify.ts
+++ src/v3/src/transformer/identify/transformIdentify.ts
@@ -11,12 +11,15 @@
  */
 
 import {
+  ActionOptions,
   ButtonElement,
   ButtonType,
+  DataSchema,
   FieldElement,
   IdxStepTransformer,
   TitleElement,
   UISchemaElement,
+  WidgetProps,
 } from '../../types';
 import { getUsernameCookie, loc } from '../../util';
 import { getUIElementWithName, removeUIElementWithName } from '../utils';
