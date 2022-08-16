a5d395746225ccad5afd7df9e95937f8bff93df2
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2022-08-16 10:58:24 -0400

    Lint fixes

--- src/v3/src/components/Form/Form.tsx
+++ src/v3/src/components/Form/Form.tsx
@@ -107,7 +107,10 @@ const Form: FunctionComponent<{
       className="o-form" // FIXME update page objects using .o-form selectors
       data-se="form"
     >
-      <Layout uischema={uischema} stepName={currTransaction?.nextStep?.name} />
+      <Layout
+        uischema={uischema}
+        stepName={currTransaction?.nextStep?.name}
+      />
     </form>
   );
 };
