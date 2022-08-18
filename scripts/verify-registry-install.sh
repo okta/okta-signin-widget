a25fdb3fa6ce77cd74172ab1ec1d0ba71769ad92
Jared Perreault <jared.perreault@okta.com>
2022-08-18 14:24:35 -0400

    testing no-save

--- scripts/verify-registry-install.sh
+++ scripts/verify-registry-install.sh
@@ -33,26 +33,26 @@ pushd test/package/angular-sample/custom-login
 
 # sample is setup to use npm
 # broadcast-channel needs to be installed first to guarantee microtime@3.0.0 is used
-if ! npm i broadcast-channel@4.13.0; then
+if ! npm i broadcast-channel@4.13.0 --no-save; then
   echo "install failed broadcast-channel@4.13.0! Exiting..."
   exit ${FAILED_SETUP}
 fi
 
-if ! npm i; then
+if ! npm i --no-save; then
   echo "install failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
 
 # install the version of @okta/okta-signin-widget from artifactory that was published during the `publish` suite
 published_tarball=${REGISTRY}/@okta/okta-signin-widget/-/${artifact_version}.tgz
-if ! npm i ${published_tarball}; then
+if ! npm i ${published_tarball} --no-save; then
   echo "install ${published_tarball} failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
 
 # use the same version of auth-js as the widget, otherwise you'll get type errors
 auth_js_version=$(cat node_modules/@okta/okta-signin-widget/package.json | jq -r '.dependencies."@okta/okta-auth-js"')
-if ! npm i @okta/okta-auth-js@${auth_js_version}; then
+if ! npm i @okta/okta-auth-js@${auth_js_version} --no-save; then
   echo "install auth-js@${auth_js_version} failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
