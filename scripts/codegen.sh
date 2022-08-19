79ba8efa4de774282c2da8f5c815b42c1e1305f1
Lester Choi <lester.choi@okta.com>
2022-08-19 11:27:49 -0400

    prevent error when cache file dne

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -6,36 +6,45 @@ first () {
 	awk '{print $1}'
 }
 
+# sums a dir
+# input: packages/@okta/i18n
+# output: d39d70a51af9ccba5710a089f87a4470 packages/@okta/i18n
 dir-sum () (
 	SUM=$(tar -cf - $@ | md5sum | first)
 	echo "$SUM $@"
 )
 
+# find a sum from cache
+# input: packages/@okta/i18n
+# output: d39d70a51af9ccba5710a089f87a4470 packages/@okta/i18n
 get-sum () {
-	grep "$@" $CACHE_FILE | first
+	if [ -a $CACHE_FILE ]; then
+		grep "$@" $CACHE_FILE
+	else
+		echo 'none'
+	fi
 }
 
+# udpates the cache file
 update-cache () {
 	rm $CACHE_FILE # disable
 	dir-sum packages/@okta/i18n >> $CACHE_FILE
 	dir-sum src/types >> $CACHE_FILE
 }
 
-if [ $(get-sum src/types) != $(dir-sum src/types | first) ]; then
+if [ $(get-sum src/types | first) != $(dir-sum src/types | first) ]; then
 	echo '[codegen]: changes detected in src/types'
 	yarn build:types
-	# update-cache types
 else
 	echo '[codegen]: no changes detected in src/types'
 fi
 
-if [ $(get-sum packages/@okta/i18n) != $(dir-sum packages/@okta/i18n | first) ]; then
+if [ $(get-sum packages/@okta/i18n | first) != $(dir-sum packages/@okta/i18n | first) ]; then
 	echo '[codegen]: changes detected in packages/@okta/i18n'
 	yarn grunt propertiesToJSON && \
 	yarn generate-config
-	# update-cache packages/@okta/i18n
 else
 	echo '[codegen]: no changes detected in packages/@okta/i18n'
 fi
 
-update-cache && cat $CACHE_FILE
+update-cache
