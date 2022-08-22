4c773b3cbd5c939ffcd7cdd89eef14d60256efaa
leemchale-okta <lee.mchale@okta.com>
2022-08-22 11:31:05 -0700

    extracted interactive types to util fn

--- src/v3/src/components/Form/Layout.tsx
+++ src/v3/src/components/Form/Layout.tsx
@@ -21,7 +21,7 @@ import {
   UISchemaLayout,
   UISchemaLayoutType,
 } from '../../types';
-import { isDevelopmentEnvironment, isTestEnvironment } from '../../util';
+import { isDevelopmentEnvironment, isInteractiveType, isTestEnvironment } from '../../util';
 import renderers from './renderers';
 // eslint-disable-next-line import/no-cycle
 import Stepper from './Stepper';
@@ -85,6 +85,11 @@ const Layout: FunctionComponent<LayoutProps> = ({ uischema }) => {
           }
 
           const uischemaElement = (element as UISchemaElement);
+          if (isInteractiveType(uischemaElement.type) && !firstFieldFound) {
+            uischemaElement.focus = true;
+            firstFieldFound = true;
+          }
+
           const Component = renderer.renderer as UISchemaElementComponent;
           return (
             <Box
