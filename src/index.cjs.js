2cdb1f8733e61b04a38cb7ef75c7d754ed54be39
Aaron Granick <aaron.granick@okta.com>
2022-08-29 16:48:35 -0700

    Delete index.cjs.js

--- src/index.cjs.js
+++
@@ -1,6 +0,0 @@
-// This is the primary export for the CommonJS module.
-// DO NOT CONVERT TO TYPESCRIPT - for backward compatibility we need to use `module.exports` not `export default`
-
-const OktaSignIn = require('./exports/default').default;
-
-module.exports = OktaSignIn;
