f407655c9037dc2af0c1fee92431c33b731e4644
leemchale-okta <lee.mchale@okta.com>
2022-08-30 12:25:10 -0700

    requested change

--- src/v3/src/transformer/identify/transformIdentify.ts
+++ src/v3/src/transformer/identify/transformIdentify.ts
@@ -34,17 +34,15 @@ export const transformIdentify: IdxStepTransformer = ({
     'identifier',
     uischema.elements as UISchemaElement[],
   ) as FieldElement;
-  if (identifierElement) {
-    if (!isClientTransaction) {
-      // add username/identifier from config if provided
-      if (username) {
-        data.identifier = username;
-      // TODO: OKTA-508744 - to use rememberMe in features once Default values are added widgetProps.
-      // (i.e. rememberMe is default = true in v2)
-      } else if (features?.rememberMe !== false && features?.rememberMyUsernameOnOIE) {
-        const usernameCookie = getUsernameCookie();
-        data.identifier = usernameCookie;
-      }
+  if (identifierElement && !isClientTransaction) {
+    // add username/identifier from config if provided
+    if (username) {
+      data.identifier = username;
+    // TODO: OKTA-508744 - to use rememberMe in features once Default values are added widgetProps.
+    // (i.e. rememberMe is default = true in v2)
+    } else if (features?.rememberMe !== false && features?.rememberMyUsernameOnOIE) {
+      const usernameCookie = getUsernameCookie();
+      data.identifier = usernameCookie;
     }
   }
 
