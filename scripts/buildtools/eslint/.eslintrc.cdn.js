5ca81eb6937327eeb431972b51375b40dda567de
Shuo Wu <60160041+shuowu-okta@users.noreply.github.com>
2022-09-01 14:01:12 -0400

    chore: setup cdn bundle compat lint (#2758)
    OKTA-505055 chore: setup cdn bundle compat lint

---
+++ scripts/buildtools/eslint/.eslintrc.cdn.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['plugin:compat/recommended'],
+  env: {
+    browser: true
+  },
+  settings: {
+    // TODO: walk through list below to identify if polyfill should be added in build process
+    // JIRA: https://oktainc.atlassian.net/browse/OKTA-529319
+    polyfills: [
+      'navigator.credentials',
+      'AbortController',
+      'BroadcastChannel',
+      'TextEncoder',
+      'URL.username',
+      'URL.host',
+      'URL.hash',
+      'URLSearchParams.get',
+    ],
+  }
+};
