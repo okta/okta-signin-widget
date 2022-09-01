d66c939cca6ffe7a8f18e129bd7f016fbbb9a879
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2022-09-01 09:11:18 -0400

    Removing unused param from function

--- src/v3/src/components/Widget/index.tsx
+++ src/v3/src/components/Widget/index.tsx
@@ -162,7 +162,6 @@ export const Widget: FunctionComponent<WidgetProps> = (widgetProps) => {
       prevTransaction: prevIdxTransactionRef.current,
       step,
       widgetProps,
-      isClientTransaction,
     });
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
