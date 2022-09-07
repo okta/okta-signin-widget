1990a0bdbfd8caac11f64045a16307bb2bf4e649
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2022-09-07 18:19:02 -0400

    Formating

--- src/v3/src/components/Form/Layout.tsx
+++ src/v3/src/components/Form/Layout.tsx
@@ -10,9 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import {
-  Box,
-} from '@mui/material';
+import { Box } from '@mui/material';
 import { FunctionComponent, h } from 'preact';
 
 import {
