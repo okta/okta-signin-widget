aa453eab825a9dc83cc6960f280e3c9926bf2668
Chaitanya Achanta <100623681+chaitanyaachanta-okta@users.noreply.github.com>
2022-09-18 23:35:51 -0500

    initial commit to support SKIP_IDP_FACTOR_VERIFICATION_BUTTON in v2.

--- src/v2/view-builder/views/idp/BaseIdPAuthenticatorBody.js
+++ src/v2/view-builder/views/idp/BaseIdPAuthenticatorBody.js
@@ -5,6 +5,27 @@ export default BaseForm.extend({
   initialize() {
     BaseForm.prototype.initialize.apply(this, arguments);
     this.model.set('useRedirect', true);
-  }
+    this.add('<div class="okta-waiting-spinner"></div>');
+  },
 
+  postRender: function() {
+    BaseForm.prototype.postRender.apply(this, arguments);
+    // get the error messages
+    const messages = this.options.appState.get('messages') || {};
+    if (this.settings.get('features.skipIdpFactorVerificationBtn')) {
+      // In case of failure, don't auto-redirect which will result in infinite redirects.
+      // so catch the error and render to the user.
+      if (Array.isArray(messages.value)) {
+        this.$('.okta-waiting-spinner').hide();
+        this.$('.o-form-button-bar').show();
+      } else {
+        this.$('.o-form-button-bar').hide();
+        this.$('.okta-waiting-spinner').show();
+        this.saveForm(this.model);
+      }
+    } else {
+      this.$('.okta-waiting-spinner').hide();
+      this.$('.o-form-button-bar').show();
+    }
+  }
 });
