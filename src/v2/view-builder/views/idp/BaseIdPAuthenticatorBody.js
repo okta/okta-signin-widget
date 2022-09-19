ea4107db7311ed77f93a7d93a5e421785c29f1c9
Chaitanya Achanta <100623681+chaitanyaachanta-okta@users.noreply.github.com>
2022-09-19 04:18:52 -0500

    minor refactoring.

--- src/v2/view-builder/views/idp/BaseIdPAuthenticatorBody.js
+++ src/v2/view-builder/views/idp/BaseIdPAuthenticatorBody.js
@@ -12,17 +12,12 @@ export default BaseForm.extend({
     BaseForm.prototype.postRender.apply(this, arguments);
     // get the error messages
     const messages = this.options.appState.get('messages') || {};
-    if (this.settings.get('features.skipIdpFactorVerificationBtn')) {
-      // In case of failure, don't auto-redirect which will result in infinite redirects.
-      // so catch the error and render to the user.
-      if (Array.isArray(messages.value)) {
-        this.$('.okta-waiting-spinner').hide();
-        this.$('.o-form-button-bar').show();
-      } else {
-        this.$('.o-form-button-bar').hide();
-        this.$('.okta-waiting-spinner').show();
-        this.saveForm(this.model);
-      }
+    // In case of failure, don't auto-redirect which will result in infinite redirects.
+    // so catch the error and render to the user.
+    if (this.settings.get('features.skipIdpFactorVerificationBtn') && !Array.isArray(messages.value)) {
+      this.$('.o-form-button-bar').hide();
+      this.$('.okta-waiting-spinner').show();
+      this.saveForm(this.model);
     } else {
       this.$('.okta-waiting-spinner').hide();
       this.$('.o-form-button-bar').show();
