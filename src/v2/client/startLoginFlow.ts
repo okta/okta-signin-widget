29311a101981ad62ecd0ba515b0b8b91f8a41884
Jared Perreault <jared.perreault@okta.com>
2022-09-21 10:31:53 -0400

    added test

--- src/v2/client/startLoginFlow.ts
+++ src/v2/client/startLoginFlow.ts
@@ -28,9 +28,11 @@ const handleProxyIdxResponse = async (settings) => {
 // eslint-disable-next-line complexity, max-statements
 export async function startLoginFlow(settings) {
   const authClient = settings.getAuthClient();
+  const { authParams } = settings.toJSON();
   const idxOptions: ProceedOptions = {
     exchangeCodeForTokens: false, // we handle this in interactionCodeFlow.js
     shouldProceedWithEmailAuthenticator: false, // do not auto-select email authenticator
+    ...(!!authParams?.nonce && { nonce: authParams?.nonce })
   };
 
   // Return a preset response
