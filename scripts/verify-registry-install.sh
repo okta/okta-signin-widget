071ca0a01552c1f421756d4a00d4089b75771f54
Aaron Granick <aaron.granick@okta.com>
2022-09-27 16:01:45 -0700

    Update verify-registry-install.sh

--- scripts/verify-registry-install.sh
+++ scripts/verify-registry-install.sh
@@ -7,7 +7,7 @@ export REGISTRY="https://artifacts.aue1d.saasure.com/artifactory/npm-topic"
 
 cd ${OKTA_HOME}/${REPO}
 
-setup_service node v12.22.12
+setup_service node v14.18.0
 # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
 setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
 
