112a2e2a1dbf8d74f88e2efefcaa145ee1b9c99e
dmotter <dmotter@powerplan.com>
2022-09-28 19:35:12 -0400

    Add backToSignInLink to WidgetOptions

--- src/types/options.ts
+++ src/types/options.ts
@@ -63,6 +63,7 @@ export interface WidgetOptions extends Partial<Pick<OktaAuthOptions,
     custom?: Array<Link>;
   };
   signOutLink?: string;
+  backToSignInLink?: string;
   // Buttons
   customButtons?: Array<CustomButton>;
   // Registration
