b2051e0594f034e5d1d89804bd5c9b1c6f9db5bc
Aaron Granick <aaron.granick@okta.com>
2022-09-30 10:30:24 -0700

    fixes router logic with default okta-hosted flow on classic

--- src/router/classic.ts
+++ src/router/classic.ts
@@ -1,7 +1,13 @@
 import V1Router from 'v1/LoginRouter';
 import { RouterConstructor, WidgetOptions } from 'types';
+import Util from 'util/Util';
+import { ConfigError } from 'util/Errors';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function routerClassFactory(options: WidgetOptions): RouterConstructor {
+  if (options.stateToken && !Util.isV1StateToken(options.stateToken)) {
+    throw new ConfigError('This version of the Sign-in Widget does not support Identity Engine');
+  }
+
   return V1Router;
 }
--- src/router/default.ts
+++ src/router/default.ts
@@ -9,10 +9,13 @@ import { RouterConstructor, WidgetOptions } from 'types';
 
 export function routerClassFactory(options: WidgetOptions) {
   let Router: RouterConstructor;
-  if ((options.stateToken && Util.isV1StateToken(options.stateToken))
-    // Self hosted widget can set the `useClassicEngine` option to use V1Router
-    || options.useClassicEngine === true)
-  {
+
+  // V1 ("classic") flow will load under these conditions:
+  const v1DefaultFlow = (!options.stateToken && !options.clientId); // Default entry flow on okta-hosted login page
+  const v1StateTokenFlow = options.stateToken && Util.isV1StateToken(options.stateToken); // Resuming a flow on okta-hosted login page
+  const v1OAuthFlow = (options.clientId && options.useClassicEngine === true); // Self hosted widget can set the `useClassicEngine` option to use V1Router
+
+  if (v1DefaultFlow || v1StateTokenFlow || v1OAuthFlow) {
     Router = V1Router;
   } else {
     Router = V2Router;
