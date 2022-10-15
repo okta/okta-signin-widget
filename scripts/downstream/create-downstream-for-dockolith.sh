1b277bb28515ff4db4af9aa8c3240675fdca1055
Aaron Granick <aaron.granick@okta.com>
2022-10-14 18:27:46 -0700

    update

--- scripts/downstream/create-downstream-for-dockolith.sh
+++ scripts/downstream/create-downstream-for-dockolith.sh
@@ -4,8 +4,7 @@
 local dockolith_branch="${upstream_artifact_branch}"
 local widget_home="$(readlink -f "$(dirname "$BASH_SOURCE")/../..")"
 
-# Update setup script
+# Update script
 pushd ${widget_home}/scripts/monolith > /dev/null
-echo "Update DOCKOLITH_BRANCH version in scripts/install-dockolith.sh to ${dockolith_branch}"
 sed -i "s/\(DOCKOLITH_BRANCH\=\).*/\1\"${dockolith_branch}\"/g" install-dockolith.sh
 popd > /dev/null
--- scripts/monolith/install-dockolith.sh
+++ scripts/monolith/install-dockolith.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -xe
 
-# Set this to a development branch to try new features
-export DOCKOLITH_BRANCH=
 if [[ -n ${DOCKOLITH_BRANCH} ]]; then
-  DOCKOLITH_BRANCH=master
+  export DOCKOLITH_BRANCH=master
 fi
 
 pushd ./scripts
 rm -rf dockolith
-echo "Cloning dockolith from branch ${DOCKOLITH_BRANCH}"
+echo "Cloning dockolith from branch: ${DOCKOLITH_BRANCH}"
 git clone --depth 1 -b $DOCKOLITH_BRANCH https://github.com/okta/dockolith.git
 popd
 
