51e5ad62d3d76ca9c6b712237db1b4fad431910b
Aaron Granick <aaron.granick@okta.com>
2022-10-14 18:18:01 -0700

    fix

--- scripts/downstream/create-downstream-for-dockolith.sh
+++ scripts/downstream/create-downstream-for-dockolith.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+: "${upstream_artifact_branch?:"missing upstream_artifact_branch"}"
 
-echo "${upstream_artifact_branch}"
-
-DOCKOLITH_BRANCH="${upstream_artifact_branch}"
+local dockolith_branch="${upstream_artifact_branch}"
+local widget_home="$(readlink -f "$(dirname "$BASH_SOURCE")/../..")"
 
 # Update setup script
-echo "Update DOCKOLITH_BRANCH version in scripts/install-dockolith.sh to ${DOCKOLITH_BRANCH}"
-sed -i "s/\(DOCKOLITH_BRANCH\=\).*/\1\"${DOCKOLITH_BRANCH}\"/g" install-dockolith.sh
-
+pushd ${widget_home}/scripts > /dev/null
+echo "Update DOCKOLITH_BRANCH version in scripts/install-dockolith.sh to ${dockolith_branch}"
+sed -i "s/\(DOCKOLITH_BRANCH\=\).*/\1\"${dockolith_branch}\"/g" install-dockolith.sh
+popd > /dev/null
