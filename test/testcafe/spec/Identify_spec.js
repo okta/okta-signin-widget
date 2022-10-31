2dfa42c099f432df3f2054554a0b8a9c479cb6a1
leemchale-okta <lee.mchale@okta.com>
2022-10-31 11:55:50 -0700

    fixed test

--- test/testcafe/spec/Identify_spec.js
+++ test/testcafe/spec/Identify_spec.js
@@ -283,6 +283,7 @@ test.meta('v3', false).requestHooks(identifyRequestLogger, identifyMock)('should
 
 test.meta('v3', false).requestHooks(identifyMock)('should render custom Unlock account link', async t => {
   const identityPage = await setup(t);
+  const customUnlockLinkText = 'HELP I\'M LOCKED OUT';
 
   await rerenderWidget({
     helpLinks: {
@@ -290,13 +291,13 @@ test.meta('v3', false).requestHooks(identifyMock)('should render custom Unlock a
     },
     i18n: {
       en: {
-        'unlockaccount': 'HELP I\'M LOCKED OUT'
+        'unlockaccount': customUnlockLinkText
       }
     }
   });
 
-  await t.expect(identityPage.getUnlockAccountLinkText()).eql('HELP I\'M LOCKED OUT');
-  await t.expect(identityPage.getCustomUnlockAccountLinkUrl()).eql('http://unlockaccount');
+  await t.expect(identityPage.unlockAccountLinkExists(customUnlockLinkText)).eql(true);
+  await t.expect(identityPage.getCustomUnlockAccountLinkUrl(customUnlockLinkText)).eql('http://unlockaccount');
 });
 
 test.meta('v3', false).requestHooks(identifyMock)('should not render custom forgot password link', async t => {
