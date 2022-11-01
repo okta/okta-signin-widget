379664449cb9ff441b9d9869807298990bc0cd51
denys.oblohin <denys.oblohin@okta.com>
2022-11-01 21:24:35 +0200

    add prefix @okta/ for typingdna .

--- src/v1/util/TypingUtil.js
+++ src/v1/util/TypingUtil.js
@@ -9,7 +9,7 @@
  *
  * See the License for the specific language governing permissions and limitations under the License.
  */
-import TypingDNA from 'typingdna';
+import TypingDNA from '@okta/typingdna';
 let tdna;
 export default {
   track: function(selectorId) {
