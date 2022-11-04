8fafc6331f89e0e3b5fb8566a52f75149394c8c6
eng-prod-CI-bot-okta <aaron.granick@okta.com>
2022-11-04 19:18:40 +0000

    Downstream Artifact Test for commit f56c3789d791e71c180c783dae763afd067b324f in artifact monolith

--- scripts/e2e-monolith.sh
+++ scripts/e2e-monolith.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Monolith version to test against
-export MONOLITH_BUILDVERSION=2022.10.1-begin-254-gaefef87dfc4e
+export MONOLITH_BUILDVERSION="2022.11.0-begin-351-gf56c3789d791"
 
 export LOCAL_MONOLITH=true
 export CI=true
