eb9a23916122a08ff27ff2e4f3ea271d7155ea05
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2022-11-29 08:40:05 -0500

    Bacon distance fix

--- src/v3/src/transformer/layout/email/emailChallengeTransformer.ts
+++ src/v3/src/transformer/layout/email/emailChallengeTransformer.ts
@@ -64,7 +64,7 @@ export const transformEmailChallenge: IdxStepTransformer = ({ transaction, formB
   }
 
   const redactedEmailAddress = nextStep.relatesTo?.value?.profile?.email;
-  const descriptionBaseText = redactedEmailAddress
+  const instructionBaseText = redactedEmailAddress
     ? loc('oie.email.verify.alternate.magicLinkToEmailAddress', 'login', [redactedEmailAddress])
     : loc('oie.email.verify.alternate.magicLinkToYourEmail', 'login');
   const instrText = useEmailMagicLink
@@ -73,7 +73,7 @@ export const transformEmailChallenge: IdxStepTransformer = ({ transaction, formB
   const informationalText: DescriptionElement = {
     type: 'Description',
     options: {
-      content: `${descriptionBaseText}${instrText}`,
+      content: `${instructionBaseText}${instrText}`,
     },
   };
 
