731ef5c267b1da7581d6867d21b9609e0a1b44da
LazarVuckovic-okta <Lazar.Vuckovic@okta.com>
2022-11-30 13:00:52 -0800

    feat: Adds checkbox to Reset Password page to revoke all sessions (#2896)
    OKTA-532208 feat: Adds checkbox to Reset Password page to revoke all sessions
    
    Adds a checkbox that toggles whether or not we should revoke all of the user's sessions during Password Reset flow in classic mode
    docs: Updates classic docs with showSessionRevocation feature flag

--- test/testcafe/framework/page-objects-v1/ResetPasswordPageObject.js
+++ test/testcafe/framework/page-objects-v1/ResetPasswordPageObject.js
@@ -2,6 +2,12 @@ import { Selector, ClientFunction } from 'testcafe';
 import BasePageObject from '../page-objects/BasePageObject';
 
 const BACK_TO_SIGN_IN_SELECTOR = '[data-se="signout-link"]';
+const REVOKE_SESSIONS_WRAPPER = '.o-form-input-name-revokeSessions';
+const ANSWER_INPUT_NAME = 'answer';
+const NEW_PASSWORD_INPUT_NAME = 'newPassword';
+const CONFIRM_PASSWORD_INPUT_NAME = 'confirmPassword';
+const REVOKE_SESSIONS_CHECKBOX_NAME = 'revokeSessions';
+const RESET_PASSWORD_BUTTON = '.o-form-button-bar .button-primary';
 
 export default class ResetPasswordPageObject extends BasePageObject {
   constructor(t) {
@@ -13,11 +19,35 @@ export default class ResetPasswordPageObject extends BasePageObject {
     return Selector(BACK_TO_SIGN_IN_SELECTOR).exists;
   }
 
+  isRevokeSessionsPresent() {
+    return Selector(REVOKE_SESSIONS_WRAPPER).exists;
+  }
+
   async clickBackToSignInLink() {
     await this.t.click(Selector(BACK_TO_SIGN_IN_SELECTOR));
   }
 
   async getPageUrl() {
     return await ClientFunction(() => window.location.href)();
+  }
+
+  answerChallenge(answer) {
+    return this.form.setTextBoxValue(ANSWER_INPUT_NAME, answer);
+  }
+
+  setNewPassword(newPassword) {
+    return this.form.setTextBoxValue(NEW_PASSWORD_INPUT_NAME, newPassword);
+  }
+
+  setConfirmPassword(confirmPassword) {
+    return this.form.setTextBoxValue(CONFIRM_PASSWORD_INPUT_NAME, confirmPassword);
+  }
+
+  async clickResetPasswordButton() {
+    await this.t.click(Selector(RESET_PASSWORD_BUTTON));
+  }
+
+  async setRevokeSessionsCheckbox(value) {
+    return this.form.setCheckbox(REVOKE_SESSIONS_CHECKBOX_NAME, value);
   }
 }
--- test/testcafe/spec/v1/ResetPassword_spec.js
+++ test/testcafe/spec/v1/ResetPassword_spec.js
@@ -2,6 +2,7 @@ import { RequestMock, RequestLogger, ClientFunction } from 'testcafe';
 import { checkConsoleMessages } from '../../framework/shared';
 import ResetPasswordPageObject from '../../framework/page-objects-v1/ResetPasswordPageObject';
 import recoveryPasswordResponse from '../../../../playground/mocks/data/api/v1/authn/recovery-password';
+import recoveryAnswerResponse from '../../../../playground/mocks/data/api/v1/authn/recovery-answer';
 import cancelResponse from '../../../../playground/mocks/data/api/v1/authn/cancel';
 
 const renderWidget = ClientFunction((settings) => {
@@ -9,36 +10,47 @@ const renderWidget = ClientFunction((settings) => {
   window.renderPlaygroundWidget(settings);
 });
 
-const resetPasswordMock = RequestMock()
+const cancelResetPasswordMock = RequestMock()
   .onRequestTo('http://localhost:3000/api/v1/authn/recovery/token')
   .respond(recoveryPasswordResponse)
   .onRequestTo('http://localhost:3000/api/v1/authn/cancel')
   .respond(cancelResponse);
 
+const resetPasswordMock = RequestMock()
+  .onRequestTo('http://localhost:3000/api/v1/authn/recovery/token')
+  .respond(recoveryPasswordResponse)
+  .onRequestTo('http://localhost:3000/api/v1/authn/recovery/answer')
+  .respond(recoveryAnswerResponse)
+  .onRequestTo('http://localhost:3000/api/v1/authn/credentials/reset_password')
+  .respond(null);
+
 fixture('Reset Password');
 
-const logger = RequestLogger(/token|cancel/, {
+const logger = RequestLogger(/token|cancel|reset_password/, {
   logRequestBody: true,
   stringifyRequestBody: true,
   logResponseBody: true
 });
 
-async function setup(t) {
+const defaultConfig = {
+  stateToken: null, // setting stateToken to null to trigger the V1 flow
+  features: {
+    router: true,
+    showSessionRevocation: false
+  },
+  useClassicEngine: true
+};
+
+async function setup(t, config = defaultConfig) {
   const resetPasswordPage = new ResetPasswordPageObject(t);
   await resetPasswordPage.navigateToPage({ render: false });
   
   await resetPasswordPage.mockCrypto();
-  await renderWidget({
-    stateToken: null, // setting stateToken to null to trigger the V1 flow
-    features: {
-      router: true,
-    },
-    useClassicEngine: true
-  });
+  await renderWidget(config);
   return resetPasswordPage;
 }
 
-test.requestHooks(logger, resetPasswordMock)(
+test.requestHooks(logger, cancelResetPasswordMock)(
   'should clear the reset password transaction after going back to sign in and not navigate back to the security question after clicking the browser back button',
   async (t) => {
     let resetPasswordPage = await setup(t);
@@ -65,5 +77,88 @@ test.requestHooks(logger, resetPasswordMock)(
     const req2 = logger.requests[1].request;
     await t.expect(req2.url).eql('http://localhost:3000/api/v1/authn/cancel');
     await t.expect(req2.method).eql('post');
+  }
+);
+
+test.requestHooks(logger, resetPasswordMock)(
+  'should show the session revocation checkbox and properly handle its state by sending the value in the body of the request',
+  async (t) => {
+    let resetPasswordPage = await setup(t, {
+      ...defaultConfig,
+      features: {
+        ...defaultConfig.features,
+        showSessionRevocation: true
+      }
+    });
+
+    await checkConsoleMessages({ controller: 'recovery-loading' });
+    let pageUrl = await resetPasswordPage.getPageUrl();
+    await t.expect(pageUrl).eql('http://localhost:3000/signin/recovery-question');
+
+    await resetPasswordPage.answerChallenge('okta');
+    await resetPasswordPage.clickResetPasswordButton();
+
+    pageUrl = await resetPasswordPage.getPageUrl();
+    await t.expect(pageUrl).eql('http://localhost:3000/signin/password-reset');
+
+    await t.expect(resetPasswordPage.isRevokeSessionsPresent()).eql(true);
+
+    const newPassword = 'Abcd1234';
+    await resetPasswordPage.setNewPassword(newPassword);
+    await resetPasswordPage.setConfirmPassword(newPassword);
+    await resetPasswordPage.setRevokeSessionsCheckbox(true);
+
+    await resetPasswordPage.clickResetPasswordButton();
+
+    await t.expect(logger.requests.length).eql(2);
+
+    await resetPasswordPage.setRevokeSessionsCheckbox(false);
+    
+    const req1 = logger.requests[0].request;
+    await t.expect(req1.url).eql('http://localhost:3000/api/v1/authn/recovery/token');
+    await t.expect(req1.method).eql('post');
+
+    const req2 = logger.requests[1].request;
+    await t.expect(req2.url).eql('http://localhost:3000/api/v1/authn/credentials/reset_password');
+    await t.expect(req2.method).eql('post');
+    const requestBody = JSON.parse(req2.body);
+    await t.expect(requestBody.revokeSessions).eql(true);
+  }
+);
+
+test.requestHooks(logger, resetPasswordMock)(
+  'should not show the session revocation checkbox and should not send it in the body of the request',
+  async (t) => {
+    let resetPasswordPage = await setup(t);
+
+    await checkConsoleMessages({ controller: 'recovery-loading' });
+    let pageUrl = await resetPasswordPage.getPageUrl();
+    await t.expect(pageUrl).eql('http://localhost:3000/signin/recovery-question');
+
+    await resetPasswordPage.answerChallenge('okta');
+    await resetPasswordPage.clickResetPasswordButton();
+
+    pageUrl = await resetPasswordPage.getPageUrl();
+    await t.expect(pageUrl).eql('http://localhost:3000/signin/password-reset');
+
+    await t.expect(resetPasswordPage.isRevokeSessionsPresent()).eql(false);
+
+    const newPassword = 'Abcd1234';
+    await resetPasswordPage.setNewPassword(newPassword);
+    await resetPasswordPage.setConfirmPassword(newPassword);
+
+    await resetPasswordPage.clickResetPasswordButton();
+
+    await t.expect(logger.requests.length).eql(2);
+    
+    const req1 = logger.requests[0].request;
+    await t.expect(req1.url).eql('http://localhost:3000/api/v1/authn/recovery/token');
+    await t.expect(req1.method).eql('post');
+
+    const req2 = logger.requests[1].request;
+    await t.expect(req2.url).eql('http://localhost:3000/api/v1/authn/credentials/reset_password');
+    await t.expect(req2.method).eql('post');
+    const requestBody = JSON.parse(req2.body);
+    await t.expect(requestBody.revokeSessions).eql(undefined);
   }
 );
