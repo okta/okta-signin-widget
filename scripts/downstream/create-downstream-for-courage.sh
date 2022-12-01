ab8bcb2b0ccdc90f361e06e5ada2f87bbfcc37a4
Aaron Granick <aaron.granick@okta.com>
2022-12-01 11:27:17 -0800

    Adds downstream for courage

---
+++ scripts/downstream/create-downstream-for-courage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+# download okta-ui artifact version if empty and assign to upstream_artifact_version
+if [[ -z "${upstream_artifact_version}" ]]; then
+  pushd ${OKTA_HOME}/okta-ui > /dev/null
+    download_job_data global publish_receipt upstream_artifact_version okta-ui ${upstream_artifact_sha}
+  popd > /dev/null
+  echo "okta-ui version that will be tested: ${upstream_artifact_version}"
+fi
+
+# Get the Courage version
+local -r courage_version="$(echo ${upstream_artifact_version} | cut -d'@' -f3)"
+
+# Update and build courage-for-signin-widget
+pushd ${OKTA_HOME}/okta-signin-widget/packages/@okta/courage-for-signin-widget > /dev/null
+
+yarn add -D @okta/courage@${courage_version} --force
+yarn build
+
+popd > /dev/null
