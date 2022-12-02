14400e5ac0f64f0f704ccf42dee633ee8291b27f
Aaron Granick <aaron.granick@okta.com>
2022-12-01 16:36:41 -0800

    Update create-downstream-for-courage.sh

--- scripts/downstream/create-downstream-for-courage.sh
+++ scripts/downstream/create-downstream-for-courage.sh
@@ -4,22 +4,29 @@ setup_service node v14.18.0
 
 # download okta-ui artifact version if empty and assign to upstream_artifact_version
 if [[ -z "${upstream_artifact_version}" ]]; then
+  local upstream_publish_receipt;
   pushd ${OKTA_HOME}/okta-ui > /dev/null
-    download_job_data global publish_receipt upstream_artifact_version okta-ui ${upstream_artifact_sha}
+    download_job_data global publish_receipt upstream_publish_receipt okta-ui ${upstream_artifact_sha}
   popd > /dev/null
-  echo "okta-ui version that will be tested: ${upstream_artifact_version}"
+  echo "okta-ui publish receipt: ${upstream_publish_receipt}"
+
+  # Get the Courage version
+  local -r courage_version="$(echo ${upstream_publish_receipt} | cut -d'@' -f3)"
+
+  echo "courage version: ${courage_version}"
 fi
 
-# Get the Courage version
-local -r courage_version="$(echo ${upstream_artifact_version} | cut -d'@' -f3)"
 
 # Install top-level dependencies
-yarn install
+echo "installing top-level dependencies"
+pushd ${OKTA_HOME}/okta-signin-widget > /dev/null
+  yarn install
+popd > /dev/null
 
 # Update and build courage-for-signin-widget
 pushd ${OKTA_HOME}/okta-signin-widget/packages/@okta/courage-for-signin-widget > /dev/null
 
-yarn add -D @okta/courage@${courage_version} --force
-yarn build
+  yarn add -D @okta/courage@${courage_version} --force
+  yarn build
 
 popd > /dev/null
