fdf36dffc2b780f4d92375c24e139b3918dfba56
Aaron Granick <aaron.granick@okta.com>
2022-12-01 16:19:49 -0800

    install top-level

--- scripts/downstream/create-downstream-for-courage.sh
+++ scripts/downstream/create-downstream-for-courage.sh
@@ -13,6 +13,9 @@ fi
 # Get the Courage version
 local -r courage_version="$(echo ${upstream_artifact_version} | cut -d'@' -f3)"
 
+# Install top-level dependencies
+yarn install
+
 # Update and build courage-for-signin-widget
 pushd ${OKTA_HOME}/okta-signin-widget/packages/@okta/courage-for-signin-widget > /dev/null
 
