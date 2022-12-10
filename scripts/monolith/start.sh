d28b9a5603c901a9b5fc378e3a0c841b82e4e0fd
Aaron Granick <aaron.granick@okta.com>
2022-12-09 16:31:18 -0800

    Update start.sh

--- scripts/monolith/start.sh
+++ scripts/monolith/start.sh
@@ -1,49 +1,33 @@
 #!/bin/bash -xe
 
 export WIDGET_HOME=${WIDGET_HOME:-`(readlink -f "$(dirname "$0")/../..")`}
+source ${WIDGET_HOME}/scripts/monolith/lib/common-widget-setup.sh
 
-create_log_group "Setup"
+if [ -z "${DOCKOLITH_CI}" ]; then # Local
+  # remove all running containers and networks before running
+  source ${DOCKOLITH_HOME}/scripts/smoke-docker.sh
 
-  source ${WIDGET_HOME}/scripts/monolith/lib/common-widget-setup.sh
+  # clear all tmp files
+  rm -rf ${DOCKOLITH_TMP}
+fi
 
-  if [ -z "${DOCKOLITH_CI}" ]; then # Local
-    # remove all running containers and networks before running
-    source ${DOCKOLITH_HOME}/scripts/smoke-docker.sh
+dockolith::setup;
 
-    # clear all tmp files
-    rm -rf ${DOCKOLITH_TMP}
-  fi
+echo "start DOCKER_HOST_CONTAINER_IP before: ${DOCKER_HOST_CONTAINER_IP}"
+common::widget::start_webapp;
+echo "start DOCKER_HOST_CONTAINER_IP after: ${DOCKER_HOST_CONTAINER_IP}"
 
-  dockolith::setup;
-finish_log_group $?
+update_hosts_entry $DOCKER_HOST_CONTAINER_IP cdn.okta1.com
+update_hosts_entry $DOCKER_HOST_CONTAINER_IP rain.okta1.com
+update_hosts_entry $DOCKER_HOST_CONTAINER_IP backdoorentry.okta1.com
 
-create_log_group "Start Tomcat"
-  common::widget::start_webapp;
-finish_log_group $?
+cat http://backdoorentry.okta1.com:1802
 
-create_log_group "Verify Webapp"
-
-  update_hosts_entry $DOCKER_HOST_CONTAINER_IP cdn.okta1.com
-  update_hosts_entry $DOCKER_HOST_CONTAINER_IP rain.okta1.com
-  update_hosts_entry $DOCKER_HOST_CONTAINER_IP backdoorentry.okta1.com
-
-  cat http://backdoorentry.okta1.com:1802
-
-finish_log_group $?
-
-create_log_group "Bootstrap"
-
-  export METRIC_TO_LOG=bootstrap_db
-  if ! log_metric_wrapper dockolith::bootstrap;
-  then
-      echo "bootstrap failed!"
-      move_logs_tmp_api
-      log_extra_dir_as_zip ${TMP_LOGS_LOCATION} run_logs.zip
-      exit ${BUILD_FAILURE}
-  fi
-
-finish_log_group $?
-
-move_logs_tmp_api
-log_extra_dir_as_zip ${TMP_LOGS_LOCATION} run_logs.zip
-#exit $SUCCESS
+export METRIC_TO_LOG=bootstrap_db
+if ! log_metric_wrapper dockolith::bootstrap;
+then
+    echo "bootstrap failed!"
+    move_logs_tmp_api
+    log_extra_dir_as_zip ${TMP_LOGS_LOCATION} run_logs.zip
+    exit ${BUILD_FAILURE}
+fi
