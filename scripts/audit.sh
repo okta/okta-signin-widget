666d5e1d15b87866e7056b4adca7b205e105270e
Lester Choi <lester.choi@okta.com>
2022-12-12 14:46:21 -0500

    chore: avoid yarn install, just use yarn.lock

--- scripts/audit.sh
+++ scripts/audit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
+setup_service node v14.18.0
+setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
+
+pushd ${OKTA_HOME}/${REPO}
 
 # print md5sum of yarn.lock
 md5sum yarn.lock
