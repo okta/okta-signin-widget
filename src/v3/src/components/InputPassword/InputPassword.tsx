be7a590b058ea842a404651043a5727c36bdbb64
leemchale-okta <lee.mchale@okta.com>
2022-12-12 23:12:06 -0800

    lint

--- src/v3/src/components/InputPassword/InputPassword.tsx
+++ src/v3/src/components/InputPassword/InputPassword.tsx
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-//import { Visibility, VisibilityOff } from '@mui/icons-material';
+// import { Visibility, VisibilityOff } from '@mui/icons-material';
 import {
   Box,
   IconButton,
