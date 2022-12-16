495a9852a380c3cc3281bcc59a0481e084e57d8f
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2022-12-16 10:33:01 -0800

    Enroll Email Authenticator v3 parity spec

--- test/testcafe/framework/page-objects/EnrollEmailPageObject.js
+++ test/testcafe/framework/page-objects/EnrollEmailPageObject.js
@@ -1,15 +1,16 @@
 import BasePageObject from './BasePageObject';
-import ResendEmailObject from './components/ResendEmailObject';
-import { Selector } from 'testcafe';
+import { Selector, userVariables } from 'testcafe';
 
 const CODE_FIELD_NAME = 'credentials.passcode';
-const ENTER_CODE_FROM_EMAIL = '.enter-auth-code-instead-link';
+const ENTER_CODE_FROM_EMAIL_CLASS = '.enter-auth-code-instead-link';
+const ENTER_CODE_FROM_EMAIL_TEXT = 'Enter a code from the email instead';
+const RESEND_EMAIL = '.resend-email-view';
+const RESEND_LINK = '.resend-link';
 
 export default class EnrollAuthenticatorPhonePageObject extends BasePageObject {
 
   constructor(t) {
     super(t);
-    this.resendEmail = new ResendEmailObject(t, this.form.el);
   }
 
   enterCode(value) {
@@ -21,12 +22,57 @@ export default class EnrollAuthenticatorPhonePageObject extends BasePageObject {
   }
 
   async enterCodeFromEmailLinkExists() {
-    const elCount = await Selector(ENTER_CODE_FROM_EMAIL).count;
-    return elCount === 1;
+    if (userVariables.v3) {
+      const elCount = await Selector('[type="button"]').count;
+      return elCount === 1;
+    } else {
+      const elCount = await Selector(ENTER_CODE_FROM_EMAIL_CLASS).count;
+      return elCount === 1;
+    }
   }
 
   async clickElement(selector) {
     await this.form.clickElement(selector);
   }
 
+  async clickEnterCodeInstead() {
+    if (userVariables.v3) {
+      await this.form.clickButton(ENTER_CODE_FROM_EMAIL_TEXT);
+    } else {
+      await this.form.clickElement(ENTER_CODE_FROM_EMAIL_CLASS);
+    }
+  }
+
+  async resendEmailExists() {
+    if (userVariables.v3) {
+      return Selector('[role="alert"]').exists;
+    }
+
+    const isHidden = await Selector(RESEND_EMAIL).hasClass('hide')
+    return !isHidden;
+  }
+
+  resendEmailText() {
+    if (userVariables.v3) {
+      return this.form.el.find('[role="alert"]').textContent;
+    }
+    else {
+      return this.form.el.find(RESEND_EMAIL).textContent;
+    }  
+  }
+
+  async clickResendEmail() {
+    if (userVariables.v3) {
+      const resendEmail = this.form.getLink('Send again');
+      await this.t.click(resendEmail);
+    }
+    else {
+      const resendEmailLink = this.form.el.find(RESEND_LINK);
+      await this.t.click(resendEmailLink);
+    }
+  }
+
+  clickVerifyButton() {
+    return this.form.clickSaveButton('Verify');
+  }
 }
