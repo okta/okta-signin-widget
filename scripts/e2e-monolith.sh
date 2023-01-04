3b50c42a36de5751fbfa519950f37a3350a379e3
Aaron Granick <aaron.granick@okta.com>
2023-01-04 09:36:23 -0800

    monolith version: 2022.12.3-begin-75-ge12fe65f6ab9

--- scripts/e2e-monolith.sh
+++ scripts/e2e-monolith.sh
@@ -2,7 +2,7 @@
 
 # Monolith version to test against
 # TODO: auto-select a recent stable version OKTA-561403
-export MONOLITH_BUILDVERSION=2022.12.2-begin-263-gf27ee20e1cf5
+export MONOLITH_BUILDVERSION=2022.12.3-begin-75-ge12fe65f6ab9
 
 export WIDGET_HOME="$(readlink -f "$(dirname "$0")/..")"
 export LOCAL_MONOLITH=true
