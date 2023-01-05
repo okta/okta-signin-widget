55944e2bf1fbb3057eb66610d7d5c6156c501dc6
Shuo Wu <wushuo2010@gmail.com>
2023-01-05 11:20:21 -0500

    update mono build version

--- scripts/e2e-monolith.sh
+++ scripts/e2e-monolith.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Monolith version to test against
-export MONOLITH_BUILDVERSION=2022.10.1-begin-254-gaefef87dfc4e
+export MONOLITH_BUILDVERSION=2023.01.0-begin-28-g616122a68e33
 
 export LOCAL_MONOLITH=true
 export CI=true
