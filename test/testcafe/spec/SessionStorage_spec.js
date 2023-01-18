4590ff755e866070afc0767b0bd06ab8219b8cee
Lester Choi <lester.choi@okta.com>
2023-01-18 15:27:39 -0500

    chore: remove unused var

--- test/testcafe/spec/SessionStorage_spec.js
+++ test/testcafe/spec/SessionStorage_spec.js
@@ -1,5 +1,4 @@
 import { ClientFunction, RequestLogger, RequestMock } from 'testcafe';
-import { checkA11y } from '../framework/a11y';
 import xhrEmailVerification from '../../../playground/mocks/data/idp/idx/authenticator-verification-email';
 import xhrSessionExpried from '../../../playground/mocks/data/idp/idx/error-401-session-expired';
 import xhrIdentify from '../../../playground/mocks/data/idp/idx/identify';
