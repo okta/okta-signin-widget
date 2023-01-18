1a45893bc1a3274b9fbb5fbff66f568c7825168b
Lester Choi <lester.choi@okta.com>
2023-01-18 15:20:00 -0500

    chore: reduce pr noise

--- test/testcafe/spec/GranularConsentView_spec.js
+++ test/testcafe/spec/GranularConsentView_spec.js
@@ -20,10 +20,12 @@ const consentGranularFailedMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/consent')
   .respond(terminalResetPasswordNotAllowed);
 
-const requestLogger = RequestLogger(/consent/, {
-  logRequestBody: true,
-  stringifyRequestBody: true,
-});
+const requestLogger = RequestLogger(/consent/,
+  {
+    logRequestBody: true,
+    stringifyRequestBody: true,
+  }
+);
 
 async function setup(t) {
   const consentPage = new ConsentPageObject(t);
