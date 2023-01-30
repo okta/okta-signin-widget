6dcb41ef2a6726bc93217f3928b288e37c4b221c
Denys Oblohin <72614880+denysoblohin-okta@users.noreply.github.com>
2023-01-30 23:24:02 +0200

    Fix CSP for DeviceFingerprinting (#3003)
    OKTA-555211 Fix CSP for DeviceFingerprinting
    add fix for v1
    lint fix
    remove task link

--- src/v1/util/DeviceFingerprint.js
+++ src/v1/util/DeviceFingerprint.js
@@ -84,10 +84,11 @@ export default {
 
     // Attach listener
     window.addEventListener('message', onMessageReceivedFromOkta, false);
-    // https://oktainc.atlassian.net/browse/OKTA-555211
     // Create and Load devicefingerprint page inside the iframe
     $iframe = $('<iframe>', {
-      style: 'display: none;',
+      css: {
+        display: 'none'
+      },
       src: oktaDomainUrl + '/auth/services/devicefingerprint',
     });
     element.append($iframe);
--- src/v2/view-builder/utils/DeviceFingerprinting.js
+++ src/v2/view-builder/utils/DeviceFingerprinting.js
@@ -89,10 +89,11 @@ export default {
 
       // Attach listener
       window.addEventListener('message', onMessageReceivedFromOkta, false);
-      // https://oktainc.atlassian.net/browse/OKTA-553082
       // Create and Load devicefingerprint page inside the iframe
       $iframe = $('<iframe>', {
-        style: 'display: none;',
+        css: {
+          display: 'none'
+        },
         src: fingerprintData.oktaDomainUrl + '/auth/services/devicefingerprint',
       });
       fingerprintData.element.append($iframe);
