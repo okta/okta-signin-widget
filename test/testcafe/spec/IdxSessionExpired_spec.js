3d655790b312851b8351751e4fb95ccfd36517c0
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2023-02-07 12:01:27 -0800

    V3 parity spec for IDX Session Expired flow

--- test/testcafe/spec/IdxSessionExpired_spec.js
+++ test/testcafe/spec/IdxSessionExpired_spec.js
@@ -1,4 +1,4 @@
-import { RequestMock } from 'testcafe';
+import { RequestMock, userVariables } from 'testcafe';
 import { checkConsoleMessages, renderWidget as rerenderWidget } from '../framework/shared';
 import IdentityPageObject from '../framework/page-objects/IdentityPageObject';
 import xhrWellKnownResponse from '../../../playground/mocks/data/oauth2/well-known-openid-configuration.json';
@@ -32,11 +32,13 @@ const interactionCodeFlowBaseMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/challenge/answer')
   .respond(xhrSessionExpired, 401);
 
-fixture('IDX Session Expired');
+fixture('IDX Session Expired')
+  .meta('v3', true);
 
 async function setup(t) {
   const identityPage = new IdentityPageObject(t);
   await identityPage.navigateToPage();
+  await t.expect(identityPage.formExists()).eql(true);
   await checkConsoleMessages({
     controller: 'primary-auth',
     formName: 'identify',
@@ -70,11 +72,14 @@ test.requestHooks(sessionExpiresDuringPasswordChallenge)('reloads into fresh sta
   await identityPage.clickNextButton();
 
   await identityPage.fillPasswordField('credentials.passcode', 'test');
-  await identityPage.clickNextButton();
+  await identityPage.clickVerifyButton();
 
   await t.expect(identityPage.getGlobalErrors()).eql('You have been logged out due to inactivity. Refresh or return to the sign in screen.');
   await t.expect(identityPage.getSignoutLinkText()).eql('Back to sign in'); // confirm they can get out of terminal state
-  await t.expect(identityPage.getIdentifier()).eql('testUser@okta.com');
+  // OKTA-551654 - Doesn't work in v3. userId isn't present on session expired screen
+  if (!userVariables.v3) {
+    await t.expect(identityPage.getIdentifier()).eql('testUser@okta.com');
+  }
 
   await identityPage.refresh();
 
@@ -90,11 +95,14 @@ test.requestHooks(sessionExpiresBackToSignIn)('back to sign loads identify after
   await identityPage.clickNextButton();
 
   await identityPage.fillPasswordField('credentials.passcode', 'test');
-  await identityPage.clickNextButton();
+  await identityPage.clickVerifyButton();
 
   await t.expect(identityPage.getGlobalErrors()).eql('You have been logged out due to inactivity. Refresh or return to the sign in screen.');
   await t.expect(identityPage.getSignoutLinkText()).eql('Back to sign in'); // confirm they can get out of terminal state
-  await t.expect(identityPage.getIdentifier()).eql('testUser@okta.com');
+  // OKTA-551654 - Doesn't work in v3. userId isn't present on session expired screen
+  if (!userVariables.v3) {
+    await t.expect(identityPage.getIdentifier()).eql('testUser@okta.com');
+  }
 
   await identityPage.clickSignOutLink();
 
@@ -110,11 +118,14 @@ test.requestHooks(interactionCodeFlowBaseMock)('Int. Code Flow: reloads into fre
   await identityPage.clickNextButton();
 
   await identityPage.fillPasswordField('credentials.passcode', 'test');
-  await identityPage.clickNextButton();
+  await identityPage.clickVerifyButton();
 
   await t.expect(identityPage.getGlobalErrors()).eql('You have been logged out due to inactivity. Refresh or return to the sign in screen.');
   await t.expect(identityPage.getSignoutLinkText()).eql('Back to sign in'); // confirm they can get out of terminal state
-  await t.expect(identityPage.getIdentifier()).eql('testUser@okta.com');
+  // OKTA-551654 - Doesn't work in v3. userId isn't present on session expired screen
+  if (!userVariables.v3) {
+    await t.expect(identityPage.getIdentifier()).eql('testUser@okta.com');
+  }
 
   await identityPage.refresh();
   identityPage = await setupInteractionCodeFlow(t);
@@ -131,11 +142,14 @@ test.requestHooks(interactionCodeFlowBaseMock)('Int. Code Flow: back to sign loa
   await identityPage.clickNextButton();
 
   await identityPage.fillPasswordField('credentials.passcode', 'test');
-  await identityPage.clickNextButton();
+  await identityPage.clickVerifyButton();
 
   await t.expect(identityPage.getGlobalErrors()).eql('You have been logged out due to inactivity. Refresh or return to the sign in screen.');
   await t.expect(identityPage.getSignoutLinkText()).eql('Back to sign in'); // confirm they can get out of terminal state
-  await t.expect(identityPage.getIdentifier()).eql('testUser@okta.com');
+  // OKTA-551654 - Doesn't work in v3. userId isn't present on session expired screen
+  if (!userVariables.v3) {
+    await t.expect(identityPage.getIdentifier()).eql('testUser@okta.com');
+  }
 
   await identityPage.clickSignOutLink();
 
