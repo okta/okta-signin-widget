754fb90a2b740227693ac91ef646528a84339a78
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-03-06 15:11:04 -0500

    Updating to use Odyssey design token for color

--- src/v3/src/components/PasswordRequirements/PasswordRequirementListItem.tsx
+++ src/v3/src/components/PasswordRequirements/PasswordRequirementListItem.tsx
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import { green } from '@mui/material/colors';
+import * as Tokens from '@okta/odyssey-design-tokens';
 import { Box, Typography } from '@okta/odyssey-react-mui';
 import { FunctionComponent, h } from 'preact';
 
@@ -26,7 +26,7 @@ const PasswordRequirementListItem: FunctionComponent<PasswordRequirementProps> =
     <Box
       component="li"
       sx={{ marginBlockEnd: (theme) => theme.spacing(2) }}
-      color={status === 'complete' ? green[600] : undefined}
+      color={status === 'complete' ? Tokens.ColorPaletteGreen600 : undefined}
     >
       <Box
         display="flex"
