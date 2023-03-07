5f5d6b537645de3dca7bcb6f6b655e65ba11996a
Shuo Wu <wushuo2010@gmail.com>
2023-03-07 16:10:15 -0500

    debug

--- scripts/testcafe-saucelabs.sh
+++ scripts/testcafe-saucelabs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-source $OKTA_HOME/$REPO/scripts/setup.sh
-
-setup_service node v16.16.0
+source $OKTA_HOME/$REPO/scripts/setup.sh "v16.16.0"
 
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/junit"
