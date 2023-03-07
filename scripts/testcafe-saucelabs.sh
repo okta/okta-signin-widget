e968821a95a5194e08fdb6e635d85c507b9a0227
Shuo Wu <wushuo2010@gmail.com>
2023-03-07 16:47:21 -0500

    set creds

--- scripts/testcafe-saucelabs.sh
+++ scripts/testcafe-saucelabs.sh
@@ -2,10 +2,15 @@
 
 source $OKTA_HOME/$REPO/scripts/setup.sh "v16.16.0"
 
+export SAUCE_USERNAME=OktaSignInWidget
+get_vault_secret_key devex/sauce-labs accessKey SAUCE_ACCESS_KEY
+
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/junit"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
+
+yarn saucectl configure -u $SAUCE_USERNAME -a $SAUCE_ACCESS_KEY
 
 if ! yarn test:testcafe-saucelabs-ci; then
   echo "testcafe tests failed! Exiting..."
