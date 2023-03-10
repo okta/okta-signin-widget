f4a996c1dd77fe0bf200a54310f41c18b90c388e
Shuo Wu <wushuo2010@gmail.com>
2023-03-10 16:01:05 -0500

    update config

--- scripts/saucelabs/start-sauce-connect-tunnel.sh
+++ scripts/saucelabs/start-sauce-connect-tunnel.sh
@@ -1,3 +1,6 @@
+: "${SAUCE_USERNAME?:"missing SAUCE_USERNAME"}"
+: "${SAUCE_ACCESS_KEY?:"missing SAUCE_ACCESS_KEY"}"
+
 curl -LO https://saucelabs.com/downloads/sc-4.8.2-linux.tar.gz
 tar xvf ./sc-4.8.2-linux.tar.gz
-./sc-4.8.2-linux/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY --region us-west --tunnel-name SIW_testcafe_bacon --readyfile .ready.json
+./sc-4.8.2-linux/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY --region us-west --tunnel-name SIW_testcafe_saucelabs_tunnel --readyfile .ready.json
