796848638579012de86151dc124db2633a0dc8bf
Lazar Vuckovic <lazar.vuckovic@okta.com>
2023-03-16 04:59:09 +0000

    fix: Fixes English leak for Session Revocation checkbox
    Fixes the English leak with the Sign me out of all other devices checkbox on resetting the password authenticator
    
    OKTA-535435
    <<<Jenkins Check-In of Tested SHA: f624f8c896a72990853c41caae8db13fae88e865 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-signin-widget
    Files changed count: 12
    PR Link:

--- scripts/downstream/create-downstream-for-monolith.sh
+++ scripts/downstream/create-downstream-for-monolith.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-setup_service node v14.18.0
+setup_service node v14.18.2
 # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
 setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
 
