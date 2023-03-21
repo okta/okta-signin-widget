c80436fc068ab25c9839035e623988df41b2e76b
Yang Chen <83233935+YangChen-okta@users.noreply.github.com>
2023-03-21 10:53:31 -0400

    cleanup

--- src/v2/controllers/FormController.ts
+++ src/v2/controllers/FormController.ts
@@ -174,6 +174,8 @@ export default Controller.extend({
 
     // Build options to invoke or throw error for invalid action
     if (FORMS.LAUNCH_AUTHENTICATOR === actionPath && actionParams) {
+      //https://oktainc.atlassian.net/browse/OKTA-562885  a temp solution to send rememberMe when click the launch OV buttion.
+      //will redesign to handle FastPass silent probing case where no username and rememberMe opiton at all.
       invokeOptions = {
         ...invokeOptions,
         actions: [{
