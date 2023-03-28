e4a7f50a4bc4bfb8c4d5c737961776628bd10cfc
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2023-03-28 11:54:38 -0700

    V3 parity spec for enduser email consent view

--- test/testcafe/framework/page-objects/EnduserConsentPageObject.js
+++ test/testcafe/framework/page-objects/EnduserConsentPageObject.js
@@ -1,4 +1,5 @@
 import ConsentPageObject from './ConsentPageObject';
+import { userVariables } from 'testcafe';
 
 export default class EnduserConsentPageObject extends ConsentPageObject {
   constructor(t) {
@@ -13,15 +14,31 @@ export default class EnduserConsentPageObject extends ConsentPageObject {
     return this.form.clickCancelButton();
   }
 
-  getInfoItemTexts() {
+  getInfoItemTextBrowser() {
+    if(userVariables.v3) {
+      return this.form.getElement('[data-se="text-browser"]').innerText;
+    }
+    return this.form.getInnerTexts('.enduser-email-consent--info');
+  }
+
+  getInfoItemTextAppName() {
+    if(userVariables.v3) {
+      return this.form.getElement('[data-se="text-appName"]').innerText;
+    }
     return this.form.getInnerTexts('.enduser-email-consent--info');
   }
 
   getSaveButtonLabel() {
+    if (userVariables.v3) {
+      return this.form.getElement('[data-type="save"]').innerText; 
+    }
     return this.form.getElement('input[type="submit"]').value;
   }
 
   getCancelButtonLabel() {
+    if (userVariables.v3) {
+      return this.form.getElement('[data-type="cancel"]').innerText; 
+    }
     return this.form.getElement('input[data-type="cancel"]').value;
   }
 }
--- test/testcafe/spec/EnduserEmailConsentView_spec.js
+++ test/testcafe/spec/EnduserEmailConsentView_spec.js
@@ -1,5 +1,4 @@
 import { RequestMock, RequestLogger } from 'testcafe';
-import { checkA11y } from '../framework/a11y';
 import EnduserConsentPageObject from '../framework/page-objects/EnduserConsentPageObject';
 import enduserEmailConsentChallengeResponse from '../../../playground/mocks/data/idp/idx/email-challenge-consent';
 import enduserEmailConsentChallengeSuccess from '../../../playground/mocks/data/idp/idx/terminal-return-email-consent';
@@ -23,7 +22,8 @@ const enduserEmailConsentFailure = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/consent')
   .respond(enduserEmailConsentChallengeDenied);
 
-fixture('Enduser Email Consent');
+fixture('Enduser Email Consent')
+  .meta('v3', true);
 
 async function setup(t) {
   const consentPageObject = new EnduserConsentPageObject(t);
@@ -34,13 +34,13 @@ async function setup(t) {
 test
   .requestHooks(enduserEmailConsentSuccess)('has the right title, info and button texts', async t => {
     const consentPage = await setup(t);
-    await checkA11y(t);
-    const infoTexts = await consentPage.getInfoItemTexts();
+    const infoTextBrowser = await consentPage.getInfoItemTextBrowser();
+    const infoTextAppName = await consentPage.getInfoItemTextAppName();
     const title = await consentPage.getFormTitle();
     const saveButtonText = await consentPage.getSaveButtonLabel();
     const cancelButtonText = await consentPage.getCancelButtonLabel();
-    await t.expect(infoTexts).contains('FIREFOX');
-    await t.expect(infoTexts).contains('Info DyneX');
+    await t.expect(infoTextBrowser).contains('FIREFOX');
+    await t.expect(infoTextAppName).contains('Info DyneX');
     await t.expect(title).eql('Did you just try to sign in?');
     await t.expect(saveButtonText).eql('Yes, it\'s me');
     await t.expect(cancelButtonText).eql('No, it\'s not me');
@@ -50,7 +50,6 @@ test
 test
   .requestHooks(requestLogger, enduserEmailConsentSuccess)('consent granted flow', async t => {
     const consentPage  = await setup(t);
-    await checkA11y(t);
     await consentPage.clickAllowButton();
 
     const {
@@ -69,14 +68,13 @@ test
     const terminalPage = new TerminalPageObject(t);
     await t.expect(terminalPage.getBeaconClass()).contains('mfa-okta-email');
     await t.expect(terminalPage.getFormTitle()).contains('Success! Return to the original tab or window');
-    await t.expect(terminalPage.getMessages()).contains('To continue, please return to the original browser tab or window you used to verify.');
-    await t.expect(terminalPage.getMessages()).contains('Close this window anytime.');
+    await t.expect(terminalPage.getMessages(0)).contains('To continue, please return to the original browser tab or window you used to verify.');
+    await t.expect(terminalPage.getMessages(1)).contains('Close this window anytime.');
   });
 
 test
   .requestHooks(requestLogger, enduserEmailConsentFailure)('consent denied flow', async t => {
     const consentPage  = await setup(t);
-    await checkA11y(t);
     await consentPage.clickDontAllowButton();
 
     const {
