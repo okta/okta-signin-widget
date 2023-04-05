bfbfef5e2cd0e151d20274dde1d527b363573149
leemchale-okta <lee.mchale@okta.com>
2023-04-05 12:07:20 -0700

    fixed spec test

--- test/testcafe/framework/page-objects/TerminalOtpOnlyPageObject.js
+++ test/testcafe/framework/page-objects/TerminalOtpOnlyPageObject.js
@@ -48,7 +48,7 @@ export default class TerminalOtpOnlyPageObject extends TerminalPageObject {
     if(userVariables.v3) {
       const iconSelector = getOtpOnlyIconSelector('browser');
       return within(this.form.getElement(iconSelector))
-        .findByTitle('Mobile browser').exists;
+        .findByTitle('Browser').exists;
     }
     return this.form.elementExist(BROWSER_OS_SMARTPHONE_ICON_SELECTOR);
   }
