542a902873c4a6138e33914e2b018c8331800d90
Shuo Wu <wushuo2010@gmail.com>
2023-04-20 09:31:24 -0400

    update install-dockolith script

--- scripts/monolith/install-dockolith.sh
+++ scripts/monolith/install-dockolith.sh
@@ -1,27 +1,12 @@
 #!/bin/bash -xe
 
-# if [[ -z ${DOCKOLITH_BRANCH} ]]; then
-#   export DOCKOLITH_BRANCH=master
-# fi
+# Yarn "add" always modifies package.json https://github.com/yarnpkg/yarn/issues/1743
+# Make a backup of package.json and restore it after install
+# NOTE: export YARN_REGISTRY as env var when running locally
+# YARN_REGISTRY={internalRegistry} yarn add @okta/dockolith@1.6.1 -WD --no-lockfile
+cp package.json package.json.bak
+yarn add @okta/dockolith@1.6.1 -WD --no-lockfile
+mv package.json.bak package.json
 
-# pushd ./scripts
-#   rm -rf dockolith
-#   echo "Cloning dockolith from branch: ${DOCKOLITH_BRANCH}"
-#   git clone --depth 1 -b $DOCKOLITH_BRANCH https://github.com/okta/dockolith.git
-  
-#   # build dockolith target
-#   pushd ./dockolith
-#   yarn
-#   yarn build
-#   popd
-# popd
-
-# # Yarn "add" always modifies package.json https://github.com/yarnpkg/yarn/issues/1743
-# # Make a backup of package.json and restore it after install
-# cp package.json package.json.bak
-# yarn add -DW --no-lockfile file:./scripts/dockolith
-# mv package.json.bak package.json
-
-yarn add @okta/dockolith@1.6.1 -WD
 
 
