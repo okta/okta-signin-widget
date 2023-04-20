e0f2d6f9036f51a2f5c2aa4390f0a46437af7037
Shuo Wu <wushuo2010@gmail.com>
2023-04-20 10:33:47 -0400

    update dockolith downstream script

--- scripts/monolith/install-dockolith.sh
+++ scripts/monolith/install-dockolith.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -xe
 
+if [[ -z ${DOCKOLITH_VERSION} ]]; then
+  export DOCKOLITH_VERSION=1.6.1
+fi
+
 # Yarn "add" always modifies package.json https://github.com/yarnpkg/yarn/issues/1743
 # Make a backup of package.json and restore it after install
 # NOTE: export YARN_REGISTRY as env var when running locally
 # YARN_REGISTRY={internalRegistry} yarn add @okta/dockolith@1.6.1 -WD --no-lockfile
 cp package.json package.json.bak
-yarn add -DW --no-lockfile @okta/dockolith@1.6.1
+yarn add -DW --no-lockfile @okta/dockolith@$DOCKOLITH_VERSION
 mv package.json.bak package.json
 
 
