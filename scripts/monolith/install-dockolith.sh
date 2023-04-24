6a94ad7bae478a3f162a059dfcfd3c3c200b8a6c
Jared Perreault <jared.perreault@okta.com>
2023-04-24 14:42:30 -0400

    use setup_service to install dockolith

--- scripts/monolith/install-dockolith.sh
+++ scripts/monolith/install-dockolith.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 if [[ -z ${DOCKOLITH_VERSION} ]]; then
   export DOCKOLITH_VERSION=1.6.1
@@ -7,11 +7,14 @@ fi
 
 # only install if not triggered by upstream build
 if [[ -z ${DOCKOLITH_DOWNSTREAM} ]]; then
-  # Yarn "add" always modifies package.json https://github.com/yarnpkg/yarn/issues/1743
-  # Make a backup of package.json and restore it after install
-  # NOTE: export YARN_REGISTRY as env var when running locally
-  # YARN_REGISTRY={internalRegistry} yarn add @okta/dockolith@1.6.1 -WD --no-lockfile
-  cp package.json package.json.bak
-  yarn add -DW --no-lockfile @okta/dockolith@$DOCKOLITH_VERSION
-  mv package.json.bak package.json
+  setup_service dockolith $DOCKOLITH_VERSION
+  export DOCKOLITH_HOME="$(yarn global dir)/node_modules/@okta/dockolith"
 fi
+
+pushd $(yarn global dir)/node_modules/@okta/dockolith > /dev/null
+  yarn link
+popd > /dev/null
+
+pushd ${OKTA_HOME}/${REPO} > /dev/null
+  yarn link @okta/dockolith
+popd > /dev/null
