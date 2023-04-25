cc453625614f93cf130b09fa0814b377aba080c0
Shuo Wu <wushuo2010@gmail.com>
2023-04-25 14:30:46 -0400

    add global variable for karma

--- test/unit/main.js
+++ test/unit/main.js
@@ -1,5 +1,7 @@
 import './karma/karma-enforce-precompile';
 
+window.global = window;
+
 const karma = window.__karma__;
 const testsContext = require.context('./spec/v1', true, /_spec\.js$/);
 const legacyTests = require('./legacy-tests');
