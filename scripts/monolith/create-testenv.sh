08d6aa652328ae72bf771f3a66b2c2a7403a6aaf
Jared Perreault <jared.perreault@okta.com>
2023-04-25 11:45:40 -0400

    test

--- scripts/monolith/create-testenv.sh
+++ scripts/monolith/create-testenv.sh
@@ -5,12 +5,15 @@
 # Write environment variables to testenv file
 
 # Get API token, if necessary
-if [[ -z "${OKTA_CLIENT_TOKEN}" ]]; then
-  source ./scripts/monolith/get-token.sh
-fi
+# if [[ -z "${OKTA_CLIENT_TOKEN}" ]]; then
+#   source ./scripts/monolith/get-token.sh
+# fi
+dockolith get-token
+echo $OKTA_CLIENT_TOKEN
 
 # Creates a test org and outputs environment variables to a file named "testenv.local" in the project root
-yarn -s --cwd test/e2e ts-node ./support/monolith/create-testenv.ts
+# yarn -s --cwd test/e2e ts-node ./support/monolith/create-testenv.ts
+dockolith exec test/e2e/support/monolith/create-testenv.ts
 cat testenv.local >> testenv
 echo "LOCAL_MONOLITH=1" >> testenv
 echo "updated testenv"
--- scripts/monolith/install-dockolith.sh
+++ scripts/monolith/install-dockolith.sh
@@ -2,24 +2,22 @@
 
 if [[ -z ${DOCKOLITH_VERSION} ]]; then
   # export DOCKOLITH_VERSION=1.6.1
-  export DOCKOLITH_VERSION=1.9.0-gd8bf663
+  export DOCKOLITH_VERSION=1.9.0-g4fd1148
 fi
 
 
 # only install if not triggered by upstream build
 if [[ -z ${DOCKOLITH_DOWNSTREAM} ]]; then
   setup_service dockolith $DOCKOLITH_VERSION
-  # export DOCKOLITH_HOME="$(yarn global dir)/node_modules/@okta/dockolith"
 fi
 
-# pushd $(yarn global dir)/node_modules/@okta/dockolith > /dev/null
-#   yarn link
-# popd > /dev/null
-
-# pushd ${OKTA_HOME}/${REPO} > /dev/null
-#   yarn link @okta/dockolith
-# popd > /dev/null
-
+echo '#####################'
 dockolith --version
-dockolith bootstrap ${OKTA_HOME}/${REPO}
+echo '#####################'
+
+if [ -n "${DOCKOLITH_CI}" ]; then # CI only
+  echo "Boostrapping dockolith..."
+  dockolith bootstrap ${OKTA_HOME}/${REPO}
+fi
+
 export DOCKOLITH_HOME=$(dockolith home)
