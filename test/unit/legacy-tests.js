6430d838439979bc3a4d1c55ef75c0b4944d35c4
Shuo Wu <wushuo2010@gmail.com>
2023-04-27 12:45:27 -0400

    debug

--- test/unit/legacy-tests.js
+++ test/unit/legacy-tests.js
@@ -4,10 +4,11 @@
 // - ???
 module.exports = [
   'Animations_spec.js', // need rework in jest
-  'EnrollCall_spec.js', // followup needed, need revisit ajax mock
+  'EnrollCall_spec.js', // followup needed, spyon
   'EnrollSms_spec.js', // followup needed, spyon
   'IDPDiscovery_spec.js', // followup needed, 7 failed, jQuery :visible selector
   'LoginRouter_spec.js', // followup needed, 34 failed, login bundle + pkce + clock
   'MfaVerify_spec.js', // followup needed, 18 failed, isVisible + dropdown style + complex
   'PrimaryAuth_spec.js', // followup needed, 17 failed, css + clock + legacy function + isVisible
+  'EnrollTotpController_spec.js'
 ];
