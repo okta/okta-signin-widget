6d7be4e5aa225687feea98f28bebbd999c785390
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-05-10 12:42:43 -0400

    Adding timeout to selector for okta dashboard

--- test/testcafe/framework/page-objects/SuccessPageObject.js
+++ test/testcafe/framework/page-objects/SuccessPageObject.js
@@ -6,7 +6,7 @@ export default class SuccessPageObject extends BasePageObject {
     super(t);
   }
   async getPageUrl() {
-    await this.t.expect(Selector('#mock-user-dashboard-title').innerText).eql('Mock User Dashboard');
+    await this.t.expect(Selector('#mock-user-dashboard-title', { timeout: 5000 }).innerText).eql('Mock User Dashboard');
 
     const pageUrl = await ClientFunction(() => window.location.href)();
     return pageUrl;
