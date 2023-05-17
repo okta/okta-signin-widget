3db0b424874a8449d28036d00fcb512927616edb
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-05-17 17:35:13 -0400

    Annotating the new file with comments and fixing the spec test

--- src/v2/ion/payloadTransformer.js
+++ src/v2/ion/payloadTransformer.js
@@ -29,6 +29,19 @@ const flattenObj = (obj) => {
   return result;
 };
 
+/**
+ * This function is for the Granular Consent remediation, scopes within the optedScopes
+ * property can include a singular value or n values delimited by a "."
+ * When they are delimited, BackBone's toJSON function will create a nested object, however,
+ * these should not be nested and the delimited key names should stay as is. So this function will
+ * flatten those nested proeprties to format it how the backend expects it. 
+ * i.e. { optedScopes: { some: { scope: true }}} = { optedScopes: { 'some.scope': true }}
+ * 
+ * @param {JSON} modelJSON JSON Equivalent of the Backbone Model's attributes/fields
+ * @returns If the JSON contains the optedScopes Property, we will flatten the fields from
+ * a nested object into K/V pair with dot notation for nested key names. Otherwise, we will return
+ * the JSON as is.
+ */
 const transformOptedScopes = (modelJSON) => {
   if (modelJSON.optedScopes && typeof modelJSON.optedScopes !== 'string') {
     const data = {
@@ -44,6 +57,15 @@ const FormNameToTransformerHandler = {
   [RemediationForms.CONSENT_GRANULAR]: transformOptedScopes,
 };
 
+/**
+ * The purpose of this function is the transform the
+ * Backbone Model's attributes/fields into a JSON equivalent before sending to IDX,
+ * since the Model fields are all flattend when the UI Schema is transformed on consumption.
+ * 
+ * @param {string} formName Form name of the current remediation
+ * @param {Model} model Backbone Model Class
+ * @returns JSON equivalent of the Model
+ */
 const transformPayload = (formName, model) => {
   const modelJSON = model.toJSON();
   const transformHandler = FormNameToTransformerHandler[formName];
