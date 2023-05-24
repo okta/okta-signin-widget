11ebb1e30834bf7004574da5a7cf6fd46b282741
Vicente Villegas <90656212+vicentevillegas-okta@users.noreply.github.com>
2023-05-24 17:32:50 -0500

    adding comments

--- test/unit/legacy-tests.js
+++ test/unit/legacy-tests.js
@@ -3,13 +3,13 @@
 // - events are missing properties such as "origin" that are bound to the source window
 // - ???
 module.exports = [
-  'Animations_spec.js', // need rework in jest
-  // 'EnrollCall_spec.js', // spyon
-  // 'EnrollSms_spec.js', // spyon
-  // 'IDPDiscovery_spec.js', // 7 failed, jQuery :visible selector
+  'Animations_spec.js', // good candidate for migration to TestCafe
+  // 'EnrollCall_spec.js', // migration is complete
+  // 'EnrollSms_spec.js', // migration is complete
+  // 'IDPDiscovery_spec.js', // 4 tests should be migrated to TestCafe
   'LoginRouter_spec.js', // 34 failed, login bundle + pkce + clock
   'MfaVerify_spec.js', // 18 failed, isVisible + dropdown style + complex
-  // 'PrimaryAuth_spec.js', // 17 failed, css + clock + legacy function + isVisible
-  // 'EnrollTotpController_spec.js', // flaky in bacon - Async callback was not invoked issue
-  // 'PollController_spec.js' // flaky in bacon - Async callback was not invoked issue in bacon
+  // 'PrimaryAuth_spec.js', // 7 tests should be migrated to TestCafe
+  // 'EnrollTotpController_spec.js', // migration is complete
+  // 'PollController_spec.js' // migration is complete
 ];
