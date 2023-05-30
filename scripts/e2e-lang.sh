a5eef95f5f669ddabbf135bdb30b9f80230107a9
Shaw Yu <shaw.yu@okta.com>
2023-05-30 14:05:59 -0700

    remove old commented code and unused secrets

--- scripts/e2e-lang.sh
+++ scripts/e2e-lang.sh
@@ -13,7 +13,6 @@ export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/junit"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
-get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_TEST_SERVER WIDGET_TEST_SERVER
 get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_BASIC_USER WIDGET_BASIC_USER
 get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_BASIC_PASSWORD WIDGET_BASIC_PASSWORD
 get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_BASIC_USER_2 WIDGET_BASIC_USER_2
@@ -24,12 +23,6 @@ get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_BASIC_USER_4
 get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_BASIC_PASSWORD_4 WIDGET_BASIC_PASSWORD_4
 get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_BASIC_USER_5 WIDGET_BASIC_USER_5
 get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_BASIC_PASSWORD_5 WIDGET_BASIC_PASSWORD_5
-get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_FB_USER WIDGET_FB_USER
-get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_FB_PASSWORD WIDGET_FB_PASSWORD
-get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_FB_USER_2 WIDGET_FB_USER_2
-get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_FB_PASSWORD_2 WIDGET_FB_PASSWORD_2
-get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_FB_USER_3 WIDGET_FB_USER_3
-get_vault_secret_key repo_gh-okta-okta-signin-widget/default WIDGET_FB_PASSWORD_3 WIDGET_FB_PASSWORD_3
 
 # We use the below OIE enabled org and clients for OIE tests
 export WIDGET_TEST_SERVER=https://oie-signin-widget.okta.com
