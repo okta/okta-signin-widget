a08986883addac2f177ecb48258656ecb7651a86
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-05-31 17:34:23 -0400

    Updating lint disable comment

--- src/util/CryptoUtil.js
+++ src/util/CryptoUtil.js
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-// eslint-disable-next-line @typescript-eslint/no-redeclare
+// eslint-disable-next-line
 /* global Uint8Array, atob, btoa */
 
 const fn = {};
