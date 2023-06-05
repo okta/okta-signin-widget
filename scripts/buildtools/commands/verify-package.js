a4cc592c76c80b582ee1136230445e65b2e57476
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-06-05 17:19:05 -0400

    Updating expected package size with gen3 bundled

--- scripts/buildtools/commands/verify-package.js
+++ scripts/buildtools/commands/verify-package.js
@@ -3,8 +3,8 @@ const { readFileSync } = require('fs');
 
 const KB = 1024;
 const MB = 1024 * 1024;
-const EXPECTED_PACKAGE_SIZE = 43.35 * MB;
-const EXPECTED_PACKAGE_FILES = 13000;
+const EXPECTED_PACKAGE_SIZE = 45 * MB;
+const EXPECTED_PACKAGE_FILES = 15000;
 
 const EXPECTED_BUNDLE_SIZES = {
   'okta-plugin-a11y.js': 3.6 * KB,
