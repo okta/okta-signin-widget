69c271c94517cf9bd56786dc3626bd4b346b7804
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-06-13 18:20:59 -0400

    Updating package file count

--- scripts/buildtools/commands/verify-package.js
+++ scripts/buildtools/commands/verify-package.js
@@ -4,7 +4,7 @@ const { readFileSync } = require('fs');
 const KB = 1024;
 const MB = 1024 * 1024;
 const EXPECTED_PACKAGE_SIZE = 49 * MB;
-const EXPECTED_PACKAGE_FILES = 18000;
+const EXPECTED_PACKAGE_FILES = 20000;
 
 const EXPECTED_BUNDLE_SIZES = {
   'okta-plugin-a11y.js': 3.6 * KB,
