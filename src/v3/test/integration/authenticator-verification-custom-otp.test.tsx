c14f7f70762285b2e96dd3eae34657f452a02b7e
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-06-20 11:45:33 -0400

    Updating mock file import

--- src/v3/test/integration/authenticator-verification-custom-otp.test.tsx
+++ src/v3/test/integration/authenticator-verification-custom-otp.test.tsx
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import mockResponse from '@okta/mocks/data/idp/idx/authenticator-verification-custom-otp.json';
+import mockResponse from '../../../../playground/mocks/data/idp/idx/authenticator-verification-custom-otp.json';
 
 import { waitFor } from '@testing-library/preact';
 import { createAuthJsPayloadArgs, setup } from './util';
