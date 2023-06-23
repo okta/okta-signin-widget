b961d77b02a8a7a62747d0e4e4c92365507da326
leemchale-okta <lee.mchale@okta.com>
2023-06-22 18:15:10 -0700

    lint

--- src/v3/src/components/InfoBox/InfoBox.tsx
+++ src/v3/src/components/InfoBox/InfoBox.tsx
@@ -75,7 +75,11 @@ const InfoBox: UISchemaElementComponent<{
               sx={{ paddingLeft: 0 }}
               key={link.label}
             >
-              <Link href={link.url} target = "_blank" variant='monochrome'>
+              <Link
+                href={link.url}
+                target="_blank"
+                variant="monochrome"
+              >
                 {link.label}
               </Link>
             </ListItem>
