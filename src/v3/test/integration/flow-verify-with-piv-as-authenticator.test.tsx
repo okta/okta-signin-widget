9657e5495994d171b228b9198e0e68a47e8333a0
Glen <glen.fannin@okta.com>
2023-07-27 17:35:01 -0400

    OKTA-632459 : SIW G3 : fix : PIV as authenticator view fix

---
+++ src/v3/test/integration/flow-verify-with-piv-as-authenticator.test.tsx
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2022-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import selectAuthenticatorListWithPIV from '../../src/mocks/response/idp/idx/authenticator-select-piv.json';
+import pivVerifyResponse from '../../src/mocks/response/idp/idx/authenticator-piv-verify.json';
+import { createAuthJsPayloadArgs, setup } from './util';
+
+describe('flow-verify-with-piv-as-authenticator', () => {
+  it('should clear global errors when transitioning between pages', async () => {
+    const {
+      authClient,
+      container,
+      user,
+      findByRole,
+      findByText,
+      findByTestId,
+    } = await setup({
+      mockResponses: {
+        '/introspect': {
+          data: selectAuthenticatorListWithPIV,
+          status: 200,
+        },
+        '/idp/idx/challenge': {
+          data: pivVerifyResponse,
+          status: 200,
+        },
+      },
+    });
+
+    const selectAuthenticatorHeading = await findByRole('heading', { level: 2 });
+
+    expect(selectAuthenticatorHeading.textContent).toBe('Verify it\'s you with a security method');
+
+    const pivAuthenticatorButton = await findByTestId('smart_card_idp');
+    await user.click(pivAuthenticatorButton);
+    expect(authClient.options.httpRequestClient).toHaveBeenCalledWith(
+      ...createAuthJsPayloadArgs('POST', 'idp/idx/challenge', {
+        authenticator: { id: 'aut7rcjloeQUmFInC1d7' },
+      }),
+    );
+
+    await findByText('PIV / CAC card');
+    const pivViewHeading = await findByRole('heading', { level: 2 });
+    expect(pivViewHeading.textContent).toBe('PIV / CAC card');
+    expect(container).toMatchSnapshot();
+  });
+});
