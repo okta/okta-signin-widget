11ac33e34252eae878db192dfcc6fc0eaa08ad0e
Shaw Yu <shaw.yu@okta.com>
2023-08-03 10:05:51 -0700

    update gruntfile to use new build:release script in v3

--- Gruntfile.js
+++ Gruntfile.js
@@ -236,7 +236,7 @@ module.exports = function(grunt) {
       'build-esm': 'yarn build:esm',
       'build-dev-watch':
       'yarn build:webpack-dev --watch --env skipAnalyzer=true' + (mockDuo ? ' --env mockDuo=true' : ''),
-      'build-release': 'yarn build:webpack-release && yarn workspace v3 build',
+      'build-release': 'yarn build:webpack-release && yarn workspace v3 build:release',
       'build-e2e-app': 'yarn build:webpack-e2e-app',
       'generate-config': 'yarn generate-config',
       'run-protractor': 'yarn protractor',
