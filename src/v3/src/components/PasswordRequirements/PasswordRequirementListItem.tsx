43cfcd775711f3de08e5bae78240f7b83c02d885
Glen <glen.fannin@okta.com>
2023-08-21 11:20:14 -0400

    Snapshot updates and adding comment

--- src/v3/src/components/PasswordRequirements/PasswordRequirementListItem.tsx
+++ src/v3/src/components/PasswordRequirements/PasswordRequirementListItem.tsx
@@ -32,6 +32,11 @@ const PasswordRequirementListItem: FunctionComponent<PasswordRequirementProps> =
         display="flex"
         alignItems="center"
       >
+        {/*
+          If any changes are made here, please test with addtl languages i.e. ok-pl / ok-sk
+          This is due to an issue that was found where the label text below modified the size of
+          of the icon. See: OKTA-586924
+        */}
         <Icon status={status} />
         <Box><Typography variant="body1">{label}</Typography></Box>
       </Box>
