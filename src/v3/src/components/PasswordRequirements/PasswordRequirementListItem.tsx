b333927a6f0b455cf407522717a124f209fdbfe1
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2023-08-23 17:21:31 -0400

    OKTA-586924 : G3 : fixing requirements list icon in additional langs (#3338)
    OKTA-586924 OKTA-586924 : G3 : fixing requirements list icon in additional langs
    Snapshot updates and adding comment
    Making icon and text top align instead of center align per UX suggestion
    Updating comment
    Lint fix

--- src/v3/src/components/PasswordRequirements/PasswordRequirementListItem.tsx
+++ src/v3/src/components/PasswordRequirements/PasswordRequirementListItem.tsx
@@ -30,11 +30,14 @@ const PasswordRequirementListItem: FunctionComponent<PasswordRequirementProps> =
     >
       <Box
         display="flex"
-        alignItems="center"
+        alignItems="start"
       >
-        <Box sx={{ minInlineSize: (theme) => theme.spacing(2) }}>
-          <Icon status={status} />
-        </Box>
+        {/**
+         * If any changes are made here, please test with addtl languages i.e. ok-pl / ok-sk
+         * This is due to an issue that was found where the label text below modified the size of
+         * of the icon. See: OKTA-586924
+         * */}
+        <Icon status={status} />
         <Box><Typography variant="body1">{label}</Typography></Box>
       </Box>
     </Box>
