5398e7e7a1eec6a6055d351c06d9784d59d68ef7
leemchale-okta <lee.mchale@okta.com>
2023-08-25 13:00:10 -0700

    added comments

--- src/v3/src/transformer/phone/phoneVerificationTransformer.ts
+++ src/v3/src/transformer/phone/phoneVerificationTransformer.ts
@@ -59,6 +59,8 @@ export const transformPhoneVerification: IdxStepTransformer = ({ transaction, fo
   uischema.elements.unshift(carrierInfoText);
 
   const redactedPhoneNumber = nextStep.relatesTo?.value?.profile?.phoneNumber as string;
+  // using the &lrm; unicode mark to keep the phone number in ltr format
+  // https://www.w3.org/TR/WCAG20-TECHS/H34.html
   const phoneNumberSpan = redactedPhoneNumber ? `<span class="strong no-translate">&lrm;${redactedPhoneNumber}</span>` : null;
   const phoneInfoText = phoneNumberSpan || loc('oie.phone.alternate.title', 'login');
   const smsInfoTextElement: DescriptionElement = {
