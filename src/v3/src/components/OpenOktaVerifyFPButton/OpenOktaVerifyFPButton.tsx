6a0a2c4645c8d939de0669046b8f0ce8aaed0f2f
Shaw Yu <shaw.yu@okta.com>
2023-08-30 13:59:19 -0400

    move styles from css module to emotion for openoktaverifyfpbutton

--- src/v3/src/components/OpenOktaVerifyFPButton/OpenOktaVerifyFPButton.tsx
+++ src/v3/src/components/OpenOktaVerifyFPButton/OpenOktaVerifyFPButton.tsx
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import classNames from 'classnames';
 import { FunctionComponent, h } from 'preact';
 import React from 'preact/compat';
 import { useState } from 'preact/hooks';
@@ -28,18 +27,18 @@ import {
   getBaseUrl, getTranslation, isAndroid, setUrlQueryParams,
 } from '../../util';
 import Button from '../Button';
-import style from './style.module.css';
 
 type IFrameProps = {
   src: string;
 };
 const IFrame: FunctionComponent<IFrameProps> = ({ src }) => {
-  const classes = classNames(style.hidden);
   return (
     // eslint-disable-next-line jsx-a11y/iframe-has-title
     <iframe
       src={src}
-      className={classes}
+      style={{
+        display: 'none',
+      }}
     />
   );
 };
