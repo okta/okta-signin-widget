9aa755135bf50a3a7cc163d570c3d764fa36b649
denys.oblohin <denys.oblohin@okta.com>
2023-10-04 18:06:23 +0300

    use babel-plugin-import to optimize lodash, mui imports

--- src/v3/webpack.common.config.ts
+++ src/v3/webpack.common.config.ts
@@ -41,6 +41,32 @@ const babelOptions = {
   ],
   plugins: [
     [
+      'babel-plugin-import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+    ],
+    [
+      'babel-plugin-import',
+      {
+        libraryName: '@mui/material',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'core',
+    ],
+    [
+      'babel-plugin-import',
+      {
+        libraryName: '@mui/icons-material',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'icons',
+    ],
+    [
       '@babel/plugin-transform-react-jsx',
       {
         runtime: 'automatic',
