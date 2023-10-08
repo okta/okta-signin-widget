446929fc027b7f11c1d1c6a595a03b88aebd2fb0
denys.oblohin <denys.oblohin@okta.com>
2023-10-08 12:31:42 +0300

    don't need babel-plugin-import for mui

--- src/v3/webpack.common.config.ts
+++ src/v3/webpack.common.config.ts
@@ -48,24 +48,24 @@ const babelOptions = {
         camel2DashComponentName: false,
       },
     ],
-    [
-      'babel-plugin-import',
-      {
-        libraryName: '@mui/material',
-        libraryDirectory: '',
-        camel2DashComponentName: false,
-      },
-      'core',
-    ],
-    [
-      'babel-plugin-import',
-      {
-        libraryName: '@mui/icons-material',
-        libraryDirectory: '',
-        camel2DashComponentName: false,
-      },
-      'icons',
-    ],
+    // [
+    //   'babel-plugin-import',
+    //   {
+    //     libraryName: '@mui/material',
+    //     libraryDirectory: '',
+    //     camel2DashComponentName: false,
+    //   },
+    //   'core',
+    // ],
+    // [
+    //   'babel-plugin-import',
+    //   {
+    //     libraryName: '@mui/icons-material',
+    //     libraryDirectory: '',
+    //     camel2DashComponentName: false,
+    //   },
+    //   'icons',
+    // ],
     [
       '@babel/plugin-transform-react-jsx',
       {
