ed044db26b299ca3a7ad36b537551f5ec69fc229
Lazar Vuckovic <lazar.vuckovic@okta.com>
2023-10-23 14:30:18 -0700

    fix: Fixes L10N error when no app label is present
    When the IDX response does not contain the app label, it no longer displays the app line with the L10N error

--- test/testcafe/spec/EmailMagicLinkOTPTerminalView_spec.js
+++ test/testcafe/spec/EmailMagicLinkOTPTerminalView_spec.js
@@ -41,6 +41,13 @@ const terminalReturnOtpUnexpectedResponseyMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/introspect')
   .respond(terminalReturnOtpUnexpectedResponse);
 
+const terminalReturnOtpOnlyNoAppLabel = JSON.parse(JSON.stringify(terminalReturnOtpOnlyFullLocation));
+terminalReturnOtpOnlyNoAppLabel.app.value.label = null;
+
+const terminalReturnOtpOnlyNoAppLabelMock = RequestMock()
+  .onRequestTo('http://localhost:3000/idp/idx/introspect')
+  .respond(terminalReturnOtpOnlyNoAppLabel);
+
 fixture('Email Magic Link OTP Terminal view');
 
 async function setupOtpOnly(t) {
@@ -132,4 +139,15 @@ test.requestHooks(terminalReturnOtpUnexpectedResponseyMock)('should gracefully h
   const terminalOtpOnlyPage = await setupOtpOnly(t);
   await t.expect(await terminalOtpOnlyPage.doesEnterCodeOnPageExist()).ok();
   await t.expect(terminalOtpOnlyPage.getEnterCodeOnPageElement().innerText).notContains('L10N_ERROR');
+});
+
+test.requestHooks(terminalReturnOtpOnlyFullLocationMock)('should display the application label if it is present in the app state', async t => {
+  const terminalOtpOnlyPage = await setupOtpOnly(t);
+  await t.expect(terminalOtpOnlyPage.doesAppNameElementExist()).eql(true);
+  await t.expect(terminalOtpOnlyPage.getAppNameElement().textContent).eql('my 3rd magic link spa');
+});
+
+test.requestHooks(terminalReturnOtpOnlyNoAppLabelMock)('should not display the application label if it is not present in the app state', async t => {
+  const terminalOtpOnlyPage = await setupOtpOnly(t);
+  await t.expect(terminalOtpOnlyPage.doesAppNameElementExist()).eql(false);
 });
