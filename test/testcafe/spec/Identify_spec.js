34ba81482dd179d5761f0be2e36cbaa00a9574df
leemchale-okta <lee.mchale@okta.com>
2023-10-24 22:30:32 -0700

    bacon test

--- test/testcafe/spec/Identify_spec.js
+++ test/testcafe/spec/Identify_spec.js
@@ -157,11 +157,10 @@ test.meta('gen3', false).requestHooks(identifyRequestLogger, identifyMock)('shou
 test.only.requestHooks(identifyMock)('should show errors if required fields are empty', async t => {
   const identityPage = await setup(t);
   await checkA11y(t);
-  await t.customActions.vrt('one');
-
+  await t.customActions.vrt();
   await identityPage.clickNextButton();
   await identityPage.waitForErrorBox();
-  await t.customActions.vrt('two');
+  await t.customActions.vrt('identify error');
 
   await t.expect(identityPage.getIdentifierErrorMessage()).eql('This field cannot be left blank');
 });
---
+++ vrtUtil/ImageDiff.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const { PNG } = require('pngjs');
+const pixelmatch = require('pixelmatch');
+
+function readPngImage(image) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(image).pipe(new PNG())
+      .on('parsed', function onParse() {
+        resolve(this);
+      })
+      .on('error', reject);
+  });
+}
+
+function fixPngImage(image, width, height) {
+  if (image.width !== width || image.height !== height) {
+    const fixedImage = new PNG({
+      width,
+      height,
+      bitDepth: image.bitDepth,
+      inputHasAlpha: true,
+    });
+
+    PNG.bitblt(image, fixedImage, 0, 0, image.width, image.height, 0, 0);
+    return fixedImage;
+  }
+  return image;
+}
+
+class ImageDiff {
+  constructor(opts) {
+    this.options = { ...opts };
+    this.width = 0;
+    this.height = 0;
+    this.differences = 0;
+  }
+
+  async run(callback) {
+    const aImage = await readPngImage(this.options.imageAPath);
+    const bImage = await readPngImage(this.options.imageBPath);
+
+    const dstImage = new PNG({
+      width: Math.max(aImage.width, bImage.width),
+      height: Math.max(aImage.height, bImage.height),
+    });
+
+    const aCanvas = await fixPngImage(aImage, dstImage.width, dstImage.height);
+    const bCanvas = await fixPngImage(bImage, dstImage.width, dstImage.height);
+
+    const options = { threshold: 0.1, diffMask: true };
+    const result = pixelmatch(aCanvas.data, bCanvas.data, dstImage.data, dstImage.width, dstImage.height, options);
+
+    dstImage.pack().pipe(fs.createWriteStream(this.getImageOutput()));
+
+    callback(Object.assign(this, {
+      width: dstImage.width,
+      height: dstImage.height,
+      differences: result,
+    }));
+  }
+
+  getImageOutput() {
+    return this.options.imageOutputPath;
+  }
+
+  getDifference() {
+    // eslint-disable-next-line no-mixed-operators
+    return Math.round(100 * 100 * this.differences / (this.width * this.height)) / 100;
+  }
+
+  hasPassed() {
+    const percentage = this.getDifference();
+    return percentage <= this.options.threshold;
+  }
+}
+
+module.exports = ImageDiff;
