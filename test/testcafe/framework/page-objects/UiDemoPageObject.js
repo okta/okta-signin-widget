9474adb678c996579d5835f06eef310c4330b88f
leemchale-okta <lee.mchale@okta.com>
2023-10-26 13:53:08 -0700

    added ui demo spec test

--- test/testcafe/framework/page-objects/UiDemoPageObject.js
+++ test/testcafe/framework/page-objects/UiDemoPageObject.js
@@ -1,7 +1,33 @@
 import BasePageObject from './BasePageObject';
+import { ClientFunction } from 'testcafe';
+
 
 export default class UiDemoPageObject extends BasePageObject {
   constructor(t) {
     super(t);
+  }
+
+  async scrollToNextPage() {
+    const getClientHeight = ClientFunction(() => window.document.documentElement.clientHeight);
+    const windowheight = await getClientHeight();
+    await this.t.scrollBy(0, windowheight);
+  }
+
+  async hideProgressBar() {
+    await ClientFunction(() => {
+      // the testcafe progress bar element is in the shadow dom so we cannot query it directly
+      const style = document.createElement('style');
+      style.textContent = '.progress-bar-hammerhead-shadow-ui { visibility: hidden !important; }';
+      document.head.appendChild(style);
+    })();
+  }
+
+  async stopSpinnerAnimation() {
+    await ClientFunction(() => {
+      const spinnerRoot = document.querySelector('.MuiCircularProgress-root');
+      const spinnerCircle = document.querySelector('.MuiCircularProgress-circle');
+      spinnerRoot.setAttribute('style', 'animation: none;');
+      spinnerCircle.setAttribute('style', 'animation: none;');
+    })();
   }
 }
