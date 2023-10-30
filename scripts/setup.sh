926dfd3f4ca7b105a2cf1bfbf26d00a910f7fe12
denys.oblohin <denys.oblohin@okta.com>
2023-10-30 19:45:34 +0200

    use 7.0 for gen2
    .
    
    .
    
    .
    
    .

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,12 +40,14 @@ fi
 
 npm config set @okta:registry ${INTERNAL_REGISTRY}
 
-if ! yarn_sync; then
-  echo "yarn.lock file is not in sync, see diff below. Please make sure this file is up-to-date by running 'yarn install' at the repo root and checking in yarn.lock changes"
-  echo "############## yarn.lock diff starts here ##############"
-  git diff *yarn.lock
-  echo "############## yarn.lock diff ends here ##############"
-  exit ${FAILED_SETUP}
+if [ -z "$AUTHJS_VERSION" ]; then
+  if ! yarn_sync; then
+    echo "yarn.lock file is not in sync, see diff below. Please make sure this file is up-to-date by running 'yarn install' at the repo root and checking in yarn.lock changes"
+    echo "############## yarn.lock diff starts here ##############"
+    git diff *yarn.lock
+    echo "############## yarn.lock diff ends here ##############"
+    exit ${FAILED_SETUP}
+  fi
 fi
 
 # Install upstream artifacts
@@ -54,20 +56,32 @@ if [ ! -z "$AUTHJS_VERSION" ]; then
 
   yarn global add @okta/siw-platform-scripts@0.7.0
 
-  rm -rf ./node_modules/@okta/okta-auth-js
+  mv -f ./node_modules/@okta/okta-auth-js ./okta-auth-js-orig
   rm -rf ./src/v3/node_modules/@okta/okta-auth-js
 
+  authjs_semver_g2=$(cat ./package.json | jq '.dependencies["@okta/okta-auth-js"]')
   json=$(cat ./package.json | jq 'del(.dependencies["@okta/okta-auth-js"])')
   printf '%s\n' "${json}" > ./package.json
+  json=$(cat ./src/v3/package.json | jq 'del(.dependencies["@okta/okta-auth-js"])')
+  printf '%s\n' "${json}" > ./src/v3/package.json
 
   if ! siw-platform install-artifact -n @okta/okta-auth-js -v ${AUTHJS_VERSION} ; then
     echo "AUTHJS_VERSION could not be installed: ${AUTHJS_VERSION}"
     exit ${FAILED_SETUP}
   fi
 
-  json_v3=$(cat ./src/v3/package.json | jq --arg ver $AUTHJS_VERSION '.dependencies["@okta/okta-auth-js"] = $ver')
-  printf '%s\n' "${json_v3}" > ./src/v3/package.json
-  cp -r ./node_modules/@okta/okta-auth-js ./src/v3/node_modules/@okta
+  json=$(cat ./src/v3/package.json | jq --arg ver $AUTHJS_VERSION '.dependencies["@okta/okta-auth-js"] = $ver')
+  printf '%s\n' "${json}" > ./src/v3/package.json
+  json=$(cat ./package.json | jq --arg ver $authjs_semver_g2 '.dependencies["@okta/okta-auth-js"] = $ver')
+  printf '%s\n' "${json}" > ./package.json
+
+  mv -f ./node_modules/@okta/okta-auth-js ./src/v3/node_modules/@okta/okta-auth-js
+  mv -f ./okta-auth-js-orig ./node_modules/@okta/okta-auth-js
+
+  authjs_ver_g2=$(cat ./node_modules/@okta/okta-auth-js/package.json | jq '.version')
+  authjs_ver_g3=$(cat ./src/v3/node_modules/@okta/okta-auth-js/package.json | jq '.version')
+  echo "okta-auth-js versions used: ${authjs_ver_g2} for gen 2, ${authjs_ver_g3} for gen 3"
+
 
   # Remove any changes to package.json
   git checkout .
