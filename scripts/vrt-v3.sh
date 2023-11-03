eb434500dd6ad7b6fb01be942828a0f8dc4c5b9b
leemchale-okta <lee.mchale@okta.com>
2023-11-02 23:04:19 -0700

    added test result directory back in

--- scripts/vrt-v3.sh
+++ scripts/vrt-v3.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export TEST_SUITE_TYPE="jsunit"
+export TEST_RESULT_FILE_DIR=${REPO}/src/v3/build2/reports/e2e
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
+echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
 source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 
