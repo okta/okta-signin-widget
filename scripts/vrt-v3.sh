3690943628e2e1d95c110e9a3627846c2a66de76
leemchale-okta <lee.mchale@okta.com>
2023-11-09 11:50:15 -0800

    moved VRT_CI to bacon script

--- scripts/vrt-v3.sh
+++ scripts/vrt-v3.sh
@@ -7,10 +7,11 @@ export TEST_RESULT_FILE_DIR="${REPO}/src/v3/build2/reports/vrt"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
+export VRT_CI=true
 setup_service google-chrome-stable 119.0.6045.105-1
 
 echo 'Starting vrt test suite'
-if ! yarn workspace v3 test:vrt-ci --no-color; then
+if ! yarn workspace v3 test:vrt --no-color; then
 	echo "vrt tests failed! Exiting..."
 	exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
