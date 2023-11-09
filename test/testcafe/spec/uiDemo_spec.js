a38f77100d60a37868966539d8100f02b274f655
leemchale-okta <lee.mchale@okta.com>
2023-11-08 23:14:51 -0800

    test add language

--- test/testcafe/spec/uiDemo_spec.js
+++ test/testcafe/spec/uiDemo_spec.js
@@ -8,6 +8,23 @@ import compareScreenshot from '../../../vrtUtil/vrtUtil';
 const uiDemoMock = RequestMock()
   .onRequestTo('http://localhost:3000/idp/idx/introspect')
   .respond(uiDemoResponse);
+  // .onRequestTo('http://localhost:3000/mocks/labels/json/login_ar.json')
+  // .respond({ 'oie.phone.enroll.title': 'Set up foo authentication' })
+  // .onRequestTo('http://localhost:3000/mocks/labels/json/country_ar.json')
+  // .respond({ 'US': 'Foonited States' })
+  // .onRequestTo('http://localhost:3000/labels/json/login_ar.json')
+  // .respond(null, 404)
+  // .onRequestTo('http://localhost:3000/labels/json/country_ar.json')
+  // .respond(null, 404)
+
+  // // Hostname is not set/available in node env, so requests to "/" are not
+  // // made relative to the location.href. This issue exists in tests only, i.e.,
+  // // it has no equivalent in prod. NOTE: Not providing these mocks cause the
+  // // test to hang indefinitely and time out when assertionTimeout is exceeded.
+  // .onRequestTo('http://labels/json/login_ar.json')
+  // .respond(null, 404)
+  // .onRequestTo('http://labels/json/country_ar.json')
+  // .respond(null, 404);
 
 async function setup(t) {
   const pageObject = new UiDemoPageObject(t);
@@ -30,7 +47,7 @@ test.requestHooks(uiDemoMock)('UI demo VRT', async t => {
 test.requestHooks(uiDemoMock)('UI demo RTL VRT', async t => {
   const pageObject = await setup(t);
   await renderWidget({
-    language: 'en',
+    language: 'ar',
   });
   await t.expect(pageObject.formExists()).eql(true);
 
