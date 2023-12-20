3785d6c10a362cd3e922fff35a31bb692ed4dc11
Glen <glen.fannin@okta.com>
2023-12-19 19:18:44 -0500

    removing role=img since img is hidden

--- src/v3/src/components/Icon/CheckCircle.tsx
+++ src/v3/src/components/Icon/CheckCircle.tsx
@@ -19,7 +19,6 @@ export const CheckCircle: FunctionComponent = () => (
     viewBox="0 0 16 16"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    role="img"
   >
     <path
       fillRule="evenodd"
