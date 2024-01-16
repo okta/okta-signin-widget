f4fbf01e8fd2b53265a035dd361573e8db0e7a9e
Shuo Wu <shuo.wu@okta.com>
2024-01-16 12:53:43 -0500

    add page title test

---
+++ src/v3/test/e2e/tests/pagetitle.spec.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022-present, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import { fixture, ClientFunction } from 'testcafe';
+
+const getTitle = ClientFunction(() => document.title);
+
+fixture('PageTitle')
+  .page('http://localhost:3000/?siw-use-mocks=true&siw-mock-response=/idp/idx/identify/default');
+
+test('Page title applies correctly', async (t) => {
+  // title should not inlcude escaped characters
+  await t.expect(getTitle()).eql(`Verify it's you with a security method`);
+});
--- src/v3/test/integration/admin-consent.test.tsx
+++ src/v3/test/integration/admin-consent.test.tsx
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import { waitFor } from '@testing-library/preact';
 import { createAuthJsPayloadArgs, setup } from './util';
 
 import adminConsentResponse from '../../../../playground/mocks/data/idp/idx/consent-admin.json';
@@ -30,24 +29,6 @@ describe('admin-consent', () => {
 
     expect(container).toMatchSnapshot();
     expect(document.title).toBe('Native client');
-  });
-
-  // disable for now as document is a global state, this test cannot be run in parallel
-  xit('should render correct page title with special character', async () => {
-    const mockResponse = {
-      ...adminConsentResponse,
-      app: {
-        ...adminConsentResponse.app,
-        value: {
-          ...adminConsentResponse.app.value,
-          label: 'Mock&label'
-        }
-      }
-    }
-    const { findByRole } = await setup({ mockResponse });
-    const appNameHeading = await findByRole('heading', { level: 2 });
-    expect(appNameHeading.textContent).toBe('Mock&amp;label');
-    await waitFor(() => expect(document.title).toBe('Mock&label'));
   });
 
   it('should send correct payload when consent is given', async () => {
