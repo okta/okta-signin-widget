7cbfa557475ffc242ea4bfcdf87d84bcf37313c0
Trevor Ing <trevor.ing@okta.com>
2024-01-17 11:00:46 -0500

    Update snapshots after Preact patch

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -18,7 +18,8 @@ const idx = {
   ],
 
   '/idp/idx/introspect': [
-    'identify',
+    'authenticator-enroll-data-phone',
+    // 'identify',
     // 'error-identify-multiple-errors',
     // 'authenticator-enroll-ov-qr-enable-biometrics',
     // 'authenticator-verification-okta-verify-push',
