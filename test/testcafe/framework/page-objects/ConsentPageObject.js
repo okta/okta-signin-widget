874e058825af468ea47e5d81227c70da3b6c3b14
Trevor Ing <trevor.ing@okta.com>
2024-01-31 12:59:26 -0500

    test fixes

--- test/testcafe/framework/page-objects/ConsentPageObject.js
+++ test/testcafe/framework/page-objects/ConsentPageObject.js
@@ -35,7 +35,7 @@ export default class ConsentPageObject extends BasePageObject {
 
   async getHeaderTitleText() {
     if (userVariables.gen3) {
-      return Selector('span.title-text > p').innerText;
+      return Selector('[data-se=title-text] > p').innerText;
     }
     const parent = Selector('.consent-title .title-text');
     // Don't want the <b> nor its content (appName)
