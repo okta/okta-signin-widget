1ceb909458e93ecb5cb5d32945f85c3bec6d4021
Trevor Ing <trevor.ing@okta.com>
2024-01-31 12:56:23 -0500

    Snapshot updates

--- src/v3/src/util/htmlContentParserUtils.tsx
+++ src/v3/src/util/htmlContentParserUtils.tsx
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import { Link, Typography } from '@okta/odyssey-react-mui';
+import { Link, Typography } from '@mui/material';
 import {
   attributesToProps,
   DOMNode,
