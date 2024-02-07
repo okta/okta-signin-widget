9641fb5c03a0857c0b78bbe5c8f73cda7acae8e1
Jared Perreault <jared.perreault@okta.com>
2024-02-07 12:57:32 -0500

    test

--- src/widget/getAuthClient.ts
+++ src/widget/getAuthClient.ts
@@ -35,6 +35,9 @@ export default function getAuthClientInstance(
       codeChallengeMethod,
       transformErrorXHR: Util.transformErrorXHR,
       recoveryToken,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      __INTERNAL_legacyTerminalSaveBehavior__: true,
       ...authParams
     };
 
