c279c87b1a5ef4c1900aa0ff2882aca16c8ff3c9
Trevor Ing <trevor.ing@okta.com>
2024-02-08 18:09:44 -0500

    Snapshot and VRT updates

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -18,7 +18,7 @@ const idx = {
   ],
 
   '/idp/idx/introspect': [
-    'identify',
+    '_ui-demo',
     // 'error-identify-multiple-errors',
     // 'authenticator-enroll-ov-qr-enable-biometrics',
     // 'authenticator-verification-okta-verify-push',
