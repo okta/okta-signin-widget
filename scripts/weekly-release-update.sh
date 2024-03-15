e7be90bd4cfdafe82a955ca4501173c592584870
Glen <glen.fannin@okta.com>
2024-03-15 12:42:54 -0400

    Updating script to push directly into specified release branch

--- scripts/weekly-release-update.sh
+++ scripts/weekly-release-update.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-VERSION=$RELEASE_VERSION
-
-# temp branch for commit
-FIX_BRANCH="weekly-patch-$(git rev-parse --verify $RELEASE_BRANCH | cut -c-8)"
-
 # get latest
 git fetch origin && \
 
 # weekly release branch
 git checkout $RELEASE_BRANCH && \
 
-# create temporary update branch
-git checkout -b $FIX_BRANCH && \
-
 # update files
-yarn run update-readme --ver=$VERSION
-yarn run update-patch-version $VERSION
+yarn run update-readme --ver=$RELEASE_VERSION
+yarn run update-patch-version $RELEASE_VERSION
 
 # add files
 git add $OKTA_HOME/$REPO/package.json
@@ -24,10 +16,10 @@ git add $OKTA_HOME/$REPO/README.md
 git add $OKTA_HOME/$REPO/polyfill/README.md
 
 # commit files
-git commit -m "chore: version bump $VERSION"
+git commit -m "chore: version bump $RELEASE_VERSION"
 
 # push
-if git push --set-upstream origin $FIX_BRANCH ; then
+if git push --set-upstream origin $RELEASE_BRANCH ; then
 	info
 else
 	printf "${RED}Push failed.\n"
