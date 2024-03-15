a7230444ad446b643bb8ff59a5459fcf6edc1897
Glen <glen.fannin@okta.com>
2024-03-15 12:16:33 -0400

    OKTA-674227 : Adding scripts to automate weekly release version bump

---
+++ scripts/update-patch-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Version was not provided. Exiting..."
+    exit ${BUILD_FAILURE}
+fi
+
+NEW_VERSION=$1
+REPO=okta-signin-widget
+package_json_contents="$(jq '.version = "'$NEW_VERSION'"' $OKTA_HOME/$REPO/package.json)" && \
+echo -E "${package_json_contents}" > $OKTA_HOME/$REPO/package.json
