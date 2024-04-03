6da7315dbaa5ab1a6982e3235f599ec1db88655d
Glen <glen.fannin@okta.com>
2024-04-03 14:51:13 -0400

    Testing script

--- scripts/weekly-release-update.sh
+++ scripts/weekly-release-update.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+VERSION=$RELEASE_VERSION
+
+# temp branch for commit
+FIX_BRANCH="weekly-patch-$VERSION"
+
 # get latest
 git fetch origin && \
 
 # weekly release branch
 git checkout $RELEASE_BRANCH && \
 
+# create temporary update branch
+git checkout -b $FIX_BRANCH && \
+
 # update files
-yarn run update-readme --ver=$RELEASE_VERSION
-yarn run update-patch-version $RELEASE_VERSION
+yarn run update-readme --ver=$VERSION
+yarn run update-patch-version $VERSION
 
 # add files
 git add $OKTA_HOME/$REPO/package.json
@@ -16,10 +24,10 @@ git add $OKTA_HOME/$REPO/README.md
 git add $OKTA_HOME/$REPO/polyfill/README.md
 
 # commit files
-git commit -m "chore: version bump $RELEASE_VERSION"
+git commit -m "chore: version bump $VERSION"
 
 # push
-if git push --set-upstream origin $RELEASE_BRANCH ; then
+if git push --set-upstream origin $FIX_BRANCH ; then
 	info
 else
 	printf "${RED}Push failed.\n"
