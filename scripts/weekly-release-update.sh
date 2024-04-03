aceb2309fb3ce5cfe06223474227af2efc104b21
Glen <glen.fannin@okta.com>
2024-04-03 17:08:59 -0400

    Updating script

--- scripts/weekly-release-update.sh
+++ scripts/weekly-release-update.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# temp branch for commit
+FIX_BRANCH="weekly-patch-$RELEASE_VERSION"
+
 # get latest
 git fetch origin && \
 
+# weekly release branch
+git checkout $RELEASE_BRANCH && \
+
+# create temporary update branch
+git checkout -b $FIX_BRANCH
+
+# update files
+yarn run update-readme --ver=$RELEASE_VERSION
 # update package.json
 package_json_contents="$(jq '.version = "'$RELEASE_VERSION'"' $OKTA_HOME/$REPO/package.json)" && \
 echo -E "${package_json_contents}" > $OKTA_HOME/$REPO/package.json
-
-# update README files
-yarn run update-readme --ver=$RELEASE_VERSION
 
 # add files
 git add $OKTA_HOME/$REPO/package.json
@@ -18,10 +26,22 @@ git add $OKTA_HOME/$REPO/polyfill/README.md
 # commit files
 git commit -m "chore: version bump $RELEASE_VERSION"
 
+printf "Pushing to Temp branch...\n"
 # push
-if git push --set-upstream origin $RELEASE_BRANCH ; then
-  info
+if git push --set-upstream origin $FIX_BRANCH ; then
+	printf "${GREEN}Push to $FIX_BRANCH was successful.\n"
+	info
 else
-  printf "${RED}Push failed.\n"
-  info
+	printf "${RED}Push to $FIX_BRANCH failed.\n"
+	info
+fi
+
+printf "Pushing to release branch...\n"
+# push
+if git push origin $FIX_BRANCH:$RELEASE_BRANCH ; then
+	printf "${GREEN}Push to $RELEASE_BRANCH was successful.\n"
+	info
+else
+	printf "${RED}Push to $RELEASE_BRANCH failed.\n"
+	info
 fi
