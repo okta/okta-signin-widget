fe34af10209ed7376b3dd0b97bf9328bcb40855f
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2024-04-04 13:48:50 -0700

    OKTA-674227 : Adding scripts to automate weekly release version bump (#3573)
    OKTA-674224 OKTA-674227 : Adding scripts to automate weekly release version bump
    Updating script to push directly into specified release branch
    Testing script
    chore: version bump 7.17.4
    Additional script testing
    Testing
    Additional Tests
    Removing readme commits
    Removing additional readme commits
    Updating script
    Removing readme update step
    Adding status checks during script
    Checking git config on bacon
    Adding tests
    More tests
    additional test changes
    Modifying script
    Modifying script
    removing config list command
    Adding readme update script
    removing comments
    Commenting readme update script
    Updating to use script in siw platform scripts
    Adding weekly release scripts and removing dups
    Updating node version in script
    Updating registry for internal packages
    Adding tests for platform scripts
    Re-introducing script update logic
    Removing replace-in-file dependency due to moving to platform scripts

---
+++ scripts/weekly-release-update.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+INTERNAL_REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta-release"
+REPO_PATH=$OKTA_HOME/$REPO
+
+if ! setup_service node v16.19.1 &> /dev/null; then
+  echo "Failed to install node"
+  exit ${FAILED_SETUP}
+fi
+
+if ! setup_service yarn 1.22.19 &> /dev/null; then
+  echo "Failed to install yarn"
+  exit ${FAILED_SETUP}
+fi
+
+# temp branch for commit
+FIX_BRANCH="weekly-patch-$RELEASE_VERSION"
+
+# get latest
+git fetch origin && \
+
+# weekly release branch
+git checkout $RELEASE_BRANCH && \
+
+# create temporary update branch
+git checkout -b $FIX_BRANCH
+
+# update files
+npm config set @okta:registry ${INTERNAL_REGISTRY}
+yarn global add @okta/siw-platform-scripts@0.11.0
+
+if ! siw-platform weekly-release-update --ver=$RELEASE_VERSION --repoPath=$REPO_PATH ; then
+	echo "weekly-release-update script failed : repo path: $REPO_PATH : release version: $RELEASE_VERSION"
+	exit ${FAILED_SETUP}
+fi
+
+# add files
+git add --all
+git status
+
+# commit files
+git commit -m "chore: version bump $RELEASE_VERSION"
+git status
+
+printf "Pushing to Temp branch...\n"
+# push
+if git push --set-upstream origin $FIX_BRANCH ; then
+	printf "${GREEN}Push to $FIX_BRANCH was successful.\n"
+	info
+else
+	printf "${RED}Push to $FIX_BRANCH failed.\n"
+	info
+fi
+
+printf "Pushing to release branch...\n"
+# push
+if git push origin $FIX_BRANCH:$RELEASE_BRANCH ; then
+	printf "${GREEN}Push to $RELEASE_BRANCH was successful.\n"
+	info
+else
+	printf "${RED}Push to $RELEASE_BRANCH failed.\n"
+	info
+fi
