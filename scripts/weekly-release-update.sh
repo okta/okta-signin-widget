d7c2d71590e92b342600cdf9590de33dc3e65e41
Glen <glen.fannin@okta.com>
2024-04-04 13:05:55 -0400

    Adding weekly release scripts and removing dups

--- scripts/weekly-release-update.sh
+++ scripts/weekly-release-update.sh
@@ -1,54 +1,51 @@
 #!/bin/bash
 
 REPO_PATH=$OKTA_HOME/$REPO
-yarn global add @okta/siw-platform-scripts@0.11.0-g1127f75
 
-if ! siw-platform weekly-release-update --ver=$RELEASE_VERSION --repo_path=$REPO_PATH ; then
+# temp branch for commit
+FIX_BRANCH="weekly-patch-$RELEASE_VERSION"
+
+# get latest
+git fetch origin && \
+
+# weekly release branch
+git checkout $RELEASE_BRANCH && \
+
+# create temporary update branch
+git checkout -b $FIX_BRANCH
+
+# update files
+yarn global add @okta/siw-platform-scripts@0.11.0
+
+if ! siw-platform weekly-release-update --ver=$RELEASE_VERSION --repoPath=$REPO_PATH ; then
 	echo "weekly-release-update script failed : repo path: $REPO_PATH : release version: $RELEASE_VERSION"
 	exit ${FAILED_SETUP}
 fi
 
-# # temp branch for commit
-# FIX_BRANCH="weekly-patch-$RELEASE_VERSION"
+# add files
+git add --all
+git status
 
-# # get latest
-# git fetch origin && \
+# commit files
+git commit -m "chore: version bump $RELEASE_VERSION"
+git status
 
-# # weekly release branch
-# git checkout $RELEASE_BRANCH && \
+printf "Pushing to Temp branch...\n"
+# push
+if git push --set-upstream origin $FIX_BRANCH ; then
+	printf "${GREEN}Push to $FIX_BRANCH was successful.\n"
+	info
+else
+	printf "${RED}Push to $FIX_BRANCH failed.\n"
+	info
+fi
 
-# # create temporary update branch
-# git checkout -b $FIX_BRANCH
-
-# # update files
-# # yarn run update-readme --ver=$RELEASE_VERSION
-# package_json_contents="$(jq '.version = "'$RELEASE_VERSION'"' $OKTA_HOME/$REPO/package.json)" && \
-# echo -E "${package_json_contents}" > $OKTA_HOME/$REPO/package.json
-
-# # add files
-# git add --all
-# git status
-
-# # commit files
-# git commit -m "chore: version bump $RELEASE_VERSION"
-# git status
-
-# printf "Pushing to Temp branch...\n"
-# # push
-# if git push --set-upstream origin $FIX_BRANCH ; then
-# 	printf "${GREEN}Push to $FIX_BRANCH was successful.\n"
-# 	info
-# else
-# 	printf "${RED}Push to $FIX_BRANCH failed.\n"
-# 	info
-# fi
-
-# printf "Pushing to release branch...\n"
-# # push
-# if git push origin $FIX_BRANCH:$RELEASE_BRANCH ; then
-# 	printf "${GREEN}Push to $RELEASE_BRANCH was successful.\n"
-# 	info
-# else
-# 	printf "${RED}Push to $RELEASE_BRANCH failed.\n"
-# 	info
-# fi
+printf "Pushing to release branch...\n"
+# push
+if git push origin $FIX_BRANCH:$RELEASE_BRANCH ; then
+	printf "${GREEN}Push to $RELEASE_BRANCH was successful.\n"
+	info
+else
+	printf "${RED}Push to $RELEASE_BRANCH failed.\n"
+	info
+fi
