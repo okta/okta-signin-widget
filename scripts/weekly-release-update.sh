b05ca9e320e586c296e52fc183e0f232c08c89e7
Glen <97472729+glenfannin-okta@users.noreply.github.com>
2024-04-26 15:45:21 -0400

    OKTA-722740 : update weekly release script to utilize internal commit command. (#3611)
    OKTA-722740 Updating weekly update script to use new robowarrior command
    updating weekly release script
    Removing temp branch for weekly update
    OKTA-722740 : task : update weekly release script to use internal command

--- scripts/weekly-release-update.sh
+++ scripts/weekly-release-update.sh
@@ -13,17 +13,11 @@ if ! setup_service yarn 1.22.19 &> /dev/null; then
   exit ${FAILED_SETUP}
 fi
 
-# temp branch for commit
-FIX_BRANCH="weekly-patch-$RELEASE_VERSION"
-
 # get latest
 git fetch origin && \
 
 # weekly release branch
-git checkout $RELEASE_BRANCH && \
-
-# create temporary update branch
-git checkout -b $FIX_BRANCH
+git checkout $RELEASE_BRANCH
 
 # update files
 npm config set @okta:registry ${INTERNAL_REGISTRY}
@@ -38,26 +32,6 @@ fi
 git add --all
 git status
 
-# commit files
-git commit -m "chore: version bump $RELEASE_VERSION"
-git status
-
-printf "Pushing to Temp branch...\n"
 # push
-if git push --set-upstream origin $FIX_BRANCH ; then
-	printf "${GREEN}Push to $FIX_BRANCH was successful.\n"
-	info
-else
-	printf "${RED}Push to $FIX_BRANCH failed.\n"
-	info
-fi
-
 printf "Pushing to release branch...\n"
-# push
-if git push origin $FIX_BRANCH:$RELEASE_BRANCH ; then
-	printf "${GREEN}Push to $RELEASE_BRANCH was successful.\n"
-	info
-else
-	printf "${RED}Push to $RELEASE_BRANCH failed.\n"
-	info
-fi
+commit_sign_push "chore: version bump $RELEASE_VERSION"
