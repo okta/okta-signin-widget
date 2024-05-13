07ef16f1a0c45eafc7c93ea7840ff9fefe915fe7
denys.oblohin <denys.oblohin@okta.com>
2024-05-13 18:35:49 +0300

    Remove unused webdriver-manager, scripts/update-se-drivers, wdio-iedriver-service. Replace dyson with express

--- playground/mocks/server.js
+++ playground/mocks/server.js
@@ -1,21 +1,80 @@
-const dyson = require('dyson');
 const path = require('path');
+const fs = require('fs');
+const express = require('express');
 
 const {
   MOCK_SERVER_PORT,
+  MOCK_SPEC_DIR_NAME,
 } = process.env;
+const port = MOCK_SERVER_PORT || 3030;
+const specDir = MOCK_SPEC_DIR_NAME || 'spec-okta-api';
+const configDir = path.resolve(__dirname, specDir);
 
-const options = {
-  multiRequest: false,
-  proxy: false,
-  quiet: false,
-  port: MOCK_SERVER_PORT,
-  configDir: path.resolve(__dirname, 'spec-okta-api'),
-};
+function sleep(timeMs) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeMs);
+  });
+}
 
-const configs = dyson.getConfigurations(options);
-const app = dyson.createServer(options);
-dyson.registerServices(app, options, configs);
+function getRandomDelay([min, max]) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
-// eslint-disable-next-line no-console
-console.log('\x1b[32m%s\x1b[0m', 'Dyson mock server started at port:', options.port);
+function configureMockRoutes(app, mocksPath = []) {
+  /* eslint max-depth: [2, 4] */
+  const entries = fs.readdirSync(path.resolve(configDir, ...mocksPath), {
+    withFileTypes: true,
+  });
+  for (const entry of entries) {
+    if (entry.isDirectory()) {
+      configureMockRoutes(app, [...mocksPath, entry.name]);
+    } else if (entry.isFile()) {
+      const ext = entry.name.split('.').pop().toLowerCase();
+      if (['js', 'ts'].includes(ext)) {
+        let mockRoutes = require(path.resolve(configDir, ...mocksPath, entry.name));
+        if (!Array.isArray(mockRoutes)) {
+          mockRoutes = [mockRoutes];
+        }
+        for (const mock of mockRoutes) {
+          configureMockRoute(app, mock);
+        }
+      }
+    }
+  }
+}
+
+function configureMockRoute(app, mock) {
+  console.log(`Registering ${mock.method} service at ${mock.path}`);
+  const method = mock.method.toLowerCase();
+  app[method](mock.path, async (req, res) => {
+    // Delay
+    if (mock.delay) {
+      await sleep(Array.isArray(mock.delay) ? getRandomDelay(mock.delay) : mock.delay);
+    }
+    // Render
+    if (mock.template) {
+      const resp = typeof mock.template === 'function' ? mock.template(req.params) : mock.template;
+      res.send(resp);
+    } else if (typeof mock.render === 'function') {
+      mock.render(req, res);
+    }
+    // Status
+    if (mock.status) {
+      if (typeof mock.template === 'function') {
+        mock.status(req, res, () => {});
+      } else {
+        res.status(mock.status);
+      }
+    }
+    res.end();
+  });
+}
+
+
+const app = express();
+configureMockRoutes(app);
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('\x1b[32m%s\x1b[0m', 'Mock server started at port:', port);
+});
