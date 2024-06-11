0c80ff4fc7f7ec058f3901fcde53953defb0ca12
denys.oblohin <denys.oblohin@okta.com>
2024-06-11 12:31:01 +0300

    Revert "test - what if remove saving bad tx for all cases?"
    This reverts commit b3941c6b3e0f701baf203e2f9c04e7be66fb2529.

--- src/v2/controllers/FormController.ts
+++ src/v2/controllers/FormController.ts
@@ -361,6 +361,7 @@ export default Controller.extend({
    */
   async showFormErrors(model, error, form) {
     /* eslint max-statements: [2, 24] */
+    const formName = model.get('formName');
     let errorObj;
     let idxStateError;
     let showErrorBanner = true;
@@ -392,6 +393,16 @@ export default Controller.extend({
     // show error before updating app state.
     model.trigger('error', model, errorObj, showErrorBanner);
     idxStateError = Object.assign({}, idxStateError, {hasFormError: true});
+
+    // TODO OKTA-408410: Widget should update the state on every new response. It should NOT do selective update.
+    // For eg 429 rate-limit errors, we have to skip updating idx state, because error response is not an idx response.
+    // OKTA-725716: Reusing stateHandle of failed transaction for form 'identify-recovery' would result in a broken flow.
+    const shouldSaveFailedTransaction = Array.isArray(idxStateError?.neededToProceed)
+      && idxStateError.neededToProceed.length
+      && !['identify', 'identify-recovery'].includes(formName);
+    if (shouldSaveFailedTransaction) {
+      await this.handleIdxResponse(idxStateError);
+    }
   },
 
   async handleIdxResponse(idxResp) {
