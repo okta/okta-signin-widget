cf561faaf6feac940d713037a5909c1dcf23d967
Lazar Vuckovic <lazar.vuckovic@okta.com>
2024-06-13 11:14:55 -0700

    chore: Removes a comment that is deprecated

--- scripts/parity-v3.sh
+++ scripts/parity-v3.sh
@@ -9,9 +9,6 @@ export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/junit"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
-# NOTE: add a testcafe fixture to the list of specs to run for parity testing by
-# adding fixture metadata {"v3": true}. See example in
-# test/testcafe/spec/Smoke_spec.js
 echo 'starting testcafe v2->v3 parity tests'
 if ! yarn test:parity-ci --no-color; then
 	echo "testcafe v2->v3 parity tests failed! Exiting..."
