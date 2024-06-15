c1766f83329e1b1fe01471b92fa91a2d04c3b98f
Trevor Ing <trevor.ing@okta.com>
2024-06-14 21:12:51 -0400

    Bump script version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -52,7 +52,7 @@ fi
 if [ ! -z "$AUTHJS_VERSION" ]; then
   echo "Installing AUTHJS_VERSION: ${AUTHJS_VERSION}"
 
-  yarn global add @okta/siw-platform-scripts@0.13.0-g1c53b5b
+  yarn global add @okta/siw-platform-scripts@0.13.0-gc19cc07
 
   if ! siw-platform install-downstream @okta/okta-auth-js ${AUTHJS_VERSION} ; then
     echo "AUTHJS_VERSION could not be installed: ${AUTHJS_VERSION}"
