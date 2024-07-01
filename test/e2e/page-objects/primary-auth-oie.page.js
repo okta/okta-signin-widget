64650f933d7764a6d16fc91fd91e49716ceea607
denys.oblohin <denys.oblohin@okta.com>
2024-07-02 00:42:47 +0300

    fix page sels for gen3

--- test/e2e/page-objects/primary-auth-oie.page.js
+++ test/e2e/page-objects/primary-auth-oie.page.js
@@ -1,26 +1,36 @@
+/* eslint-disable max-len */
 import { waitForLoad } from '../util/waitUtil';
+
+const { BUNDLE } = process.env;
 
 class PrimaryAuthOIEPage {
   identifierFieldSelector = 'input[name="identifier"]';
 
   get enrollProfileButton() { return $('[data-se=enroll]'); }
 
-  get forgotPassword() { return $('.siw-main-view.identify-recovery.forgot-password'); }
+  get forgotPassword() { return $(BUNDLE === 'next' ? 'form[data-se="o-form"]' : '.siw-main-view.identify-recovery.forgot-password'); }
   get forgotPasswordButton() { return $('[data-se="forgot-password"]'); }
   get unlockButton() {return $('[data-se="unlock"]');}
   get nextButton() { return $('[value="Next"]'); }
-  get signupForm() { return $('.siw-main-view.enroll-profile.registration'); }
+  get signupForm() { return $(BUNDLE === 'next' ? 'form[data-se="o-form"]' : '.siw-main-view.enroll-profile.registration'); }
   get signupLink() { return $('a[data-se="enroll"]'); }
-  get unlockAccountForm() { return $('.siw-main-view.select-authenticator-unlock-account'); }
-  get primaryAuthForm() { return $('.siw-main-view.primary-auth'); }
+  get unlockAccountForm() { return $(BUNDLE === 'next' ? 'form[data-se="o-form"]' : '.siw-main-view.select-authenticator-unlock-account'); }
+  get primaryAuthForm() { return $(BUNDLE === 'next' ? 'form[data-se="o-form"]' : '.siw-main-view.primary-auth'); }
   get identifierField() { return $(this.identifierFieldSelector); }
   get passwordField() { return $('input[name="credentials.passcode"]'); }
-  get submitButton() { return $('input[data-type="save"]'); }
+  get submitButton() { return $(BUNDLE === 'next' ? 'button[data-se="save"]' : 'input[data-type="save"]'); }
   get formTitle() { return $('[data-se="o-form-head"]'); }
   get oktaOidcIdPButton() { return $('[data-se="social-auth-general-idp-button"]'); }
 
   async waitForForgotPassword() {
-    await waitForLoad(this.forgotPassword);
+    if (BUNDLE === 'next') {
+      await browser.waitUntil(async () => {
+        const formTitle = await this.formTitle;
+        return (await formTitle.isDisplayed()) && (await formTitle.getText()) === 'Reset your password';
+      }, 10_000, 'wait for page title');
+    } else {
+      await waitForLoad(this.forgotPassword);
+    }
   }
 
   async waitForSubmitButton() {
@@ -28,7 +38,13 @@ class PrimaryAuthOIEPage {
   }
 
   async waitForPrimaryAuthForm() {
-    await waitForLoad(this.primaryAuthForm);
+    if (BUNDLE === 'next') {
+      await waitForLoad(this.identifierField);
+      await waitForLoad(this.passwordField);
+      await waitForLoad(this.submitButton);
+    } else {
+      await waitForLoad(this.primaryAuthForm);
+    }
   }
 
   async clickEnrollProfileButton() {
@@ -66,11 +82,25 @@ class PrimaryAuthOIEPage {
   }
 
   async waitForSignupForm() {
-    await waitForLoad(this.signupForm);
+    if (BUNDLE === 'next') {
+      await browser.waitUntil(async () => {
+        const formTitle = await this.formTitle;
+        return (await formTitle.isDisplayed()) && (await formTitle.getText()) === 'Sign up';
+      }, 10_000, 'wait for page title');
+    } else {
+      await waitForLoad(this.signupForm);
+    }
   }
 
   async waitForUnlockAccountForm() {
-    await waitForLoad(this.unlockAccountForm);
+    if (BUNDLE === 'next') {
+      await browser.waitUntil(async () => {
+        const formTitle = await this.formTitle;
+        return (await formTitle.isDisplayed()) && (await formTitle.getText()) === 'Unlock account?';
+      }, 10_000, 'wait for page title');
+    } else {
+      await waitForLoad(this.unlockAccountForm);
+    }
   }
 }
 
