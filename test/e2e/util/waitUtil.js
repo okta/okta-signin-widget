ea0858bc05f200395239a4fa441baf536fbb3d50
denys.oblohin <denys.oblohin@okta.com>
2024-07-04 13:55:21 +0300

    nit

--- test/e2e/util/waitUtil.js
+++ test/e2e/util/waitUtil.js
@@ -1,11 +1,11 @@
 export const waitForLoad = async (element) => {
   const $element = await element;
-  await $element.waitForDisplayed({ timeout: 10_000 });
+  await $element.waitForDisplayed({ timeout: 20_000 });
 };
 
 export const waitForText = async (element, expectedText) => {
   await browser.waitUntil(async () => {
     const $element = await element;
     return (await $element.isDisplayed()) && (await $element.getText()) === expectedText;
-  }, 10_000, `wait for element to load and have text: ${expectedText}`);
+  }, 20_000, `wait for element to load and have text: ${expectedText}`);
 };
