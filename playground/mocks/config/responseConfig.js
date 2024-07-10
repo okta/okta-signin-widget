e894f998bd144b9db43a8693f43c9198c6f382a6
Shuo Wu <shuo.wu@okta.com>
2024-07-10 14:28:32 -0400

    reset respnseConfig

--- playground/mocks/config/responseConfig.js
+++ playground/mocks/config/responseConfig.js
@@ -114,8 +114,6 @@ const idx = {
     // 'authenticator-verification-custom-app-push',
     // 'authenticator-enroll-custom-app-push',
     // 'request-activation-email'    
-    // 'authenticator-enroll-google-authenticator',
-    // 'authenticator-enroll-ov-via-sms',
   ],
   '/idp/idx/enroll': [
     'enroll-profile-new',
@@ -124,11 +122,11 @@ const idx = {
     // 'enroll-profile-with-idps'
   ],
   '/idp/idx/credential/enroll': [
-    'authenticator-enroll-ov-via-sms',
+    // 'authenticator-enroll-ov-via-sms',
     // 'authenticator-enroll-security-question',
     // 'authenticator-enroll-google-authenticator',
     // 'authenticator-enroll-email-first-emailmagiclink-true',
-    // 'error-authenticator-enroll-phone-invalid-number',
+    'error-authenticator-enroll-phone-invalid-number',
   ],
   '/idp/idx/identify': [
     // 'authenticator-verification-data-ov-only-without-device-known',
@@ -174,7 +172,7 @@ const idx = {
     // 'authenticator-verification-phone-voice'
   ],
   '/idp/idx/challenge/poll': [
-    // 'authenticator-verification-email',
+    'authenticator-verification-email',
     // 'success',
     // 'authenticator-verification-email-polling-long',
     // 'error-429-too-many-request',
@@ -184,7 +182,7 @@ const idx = {
     // 'authenticator-enroll-email-emailmagiclink-true',
     // 'authenticator-enroll-email-emailmagiclink-false',
     // 'authenticator-verification-okta-verify-push',
-    'authenticator-verification-custom-app-push',
+    // 'authenticator-verification-custom-app-push',
     // 'authenticator-verification-custom-app-push-reject',
     // 'authenticator-enroll-ov-sms-enable-biometrics',
     // 'okta-verify-version-upgrade',
@@ -221,9 +219,6 @@ const idx = {
   '/idp/idx/device/activate': [
     'identify-with-password',
     // 'error-invalid-device-code',
-  ],
-  '/idp/idx/authenticators/poll/cancel': [
-    'identify',
   ]
 };
 
