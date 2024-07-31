1cbe18c5e1e828e3d09a4474f9935dcfff1e258c
denys.oblohin <denys.oblohin@okta.com>
2024-07-31 21:27:31 +0300

    fix specs for gen2

--- src/v3/src/util/deviceFingerprintingUtils.ts
+++ src/v3/src/util/deviceFingerprintingUtils.ts
@@ -16,7 +16,9 @@ export const generateDeviceFingerprint = (
   authClient: OktaAuthIdxInterface,
   timeoutDuration?: number,
 ): Promise<string> => {
+  const formElement = document.querySelector('form[data-se="o-form"]');
   return authClient.fingerprint({
     timeout: timeoutDuration || 2000,
+    element: formElement,
   });
 };
