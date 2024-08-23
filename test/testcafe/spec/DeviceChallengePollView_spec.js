697af9806024f3d0125367b646b9f077d7ae17b6
Lester Choi <lester.choi@okta.com>
2024-08-22 22:02:55 -0400

    test: restore original timers

--- test/testcafe/spec/DeviceChallengePollView_spec.js
+++ test/testcafe/spec/DeviceChallengePollView_spec.js
@@ -586,7 +586,7 @@ test
       record => record.response.statusCode === 200 &&
                 record.request.url.match(/introspect/)
     )).eql(1);
-    await t.wait(15_000); // wait a moment for all probes to fail
+    await t.wait(6000); // wait a moment for all probes to fail
     await t.expect(loopbackChallengeWrongProfileLogger.count(
       record => record.response.statusCode === 500 &&
                 record.request.url.match(/(2000|6512)\/probe/)
@@ -623,7 +623,7 @@ test
       record => record.response.statusCode === 200 &&
         record.request.url.match(/introspect/)
     )).eql(1);
-    await t.wait(15_000);
+    await t.wait(2000);
     await t.expect(loopbackSuccessButNotAssignedLogger.count(
       record => record.response.statusCode === 200 &&
         record.request.method === 'get' &&
@@ -651,7 +651,7 @@ test
       record => record.response.statusCode === 200 &&
         record.request.url.match(/introspect/)
     )).eql(1);
-    await t.wait(15_000);
+    await t.wait(1000);
     await t.expect(loopbackFallbackLogger.count(
       record => record.response.statusCode === 500 &&
         record.request.url.match(/2000|6511|6512|6513/)
@@ -683,7 +683,7 @@ test
       record => record.response.statusCode === 200 &&
         record.request.url.match(/introspect/)
     )).eql(1);
-    await t.wait(15_000);
+    await t.wait(1000);
     await t.expect(appLinkWithoutLaunchLogger.count(
       record => record.response.statusCode === 500 &&
         record.request.url.match(/2000|6511|6512|6513/)
@@ -701,7 +701,7 @@ test
     await t.expect(await deviceChallengePollPageObject.hasSpinner()).eql(true);
     await t.expect(deviceChallengePollPageObject.getFooterCancelPollingLink().exists).eql(true);
 
-    await t.wait(15_000); // wait for FASTPASS_FALLBACK_SPINNER_TIMEOUT
+    await t.wait(5000); // wait for FASTPASS_FALLBACK_SPINNER_TIMEOUT
 
     await t.expect(deviceChallengePollPageObject.waitForPrimaryButtonAfterSpinner().innerText).eql('Open Okta Verify');
 
