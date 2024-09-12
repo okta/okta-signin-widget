68083b4d484928469cae1ef43cf62a2a36598088
mauriciocastillosilva-okta <mauricio.castillosilva@okta.com>
2024-09-12 12:14:58 -0700

    chore: supports Haitian Creole (ht) (#3716)
    OKTA-803221 chore: supports Haitian Creole (ht)
    
    - Removes temporary logic to filter out ht
    - Adds country_ht.properties (we need both login and country bundles for a language to load)

--- scripts/buildtools/commands/generate-language-config.js
+++ scripts/buildtools/commands/generate-language-config.js
@@ -36,12 +36,7 @@ exports.handler = () => {
   });
   // Default language is special - it is just login.json, and is ignored in our glob.
   supportedLanguages.unshift(DEFAULT_LANGUAGE);
-
-  // FIXME OKTA-735346 exclude 'ht' until i18n framework support is added
-  console.warn('NOTE: disable "ht" translation until i18n framework support is added');
-  config.supportedLanguages = supportedLanguages.filter(lang => lang !== 'ht');
-  console.log('config.supportedLanguages:');
-  console.log(config.supportedLanguages.join(', '));
+  config.supportedLanguages = supportedLanguages;
 
   writeFileSync(OUTPUT_FILE, JSON.stringify(config, null, 2));
   console.log('Wrote config to ' + OUTPUT_FILE);
