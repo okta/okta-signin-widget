c106f1d404b7fbb1d62c7da141c692859f54153f
Praveen <praveen.durai@okta.com>
2024-09-24 14:07:47 -0500

    lint fix

--- src/v3/src/transformer/layout/idp/transformIdvIdpAuthenticator.ts
+++ src/v3/src/transformer/layout/idp/transformIdvIdpAuthenticator.ts
@@ -52,8 +52,18 @@ export const transformIdvIdpAuthenticator: IdxStepTransformer = ({
     options: {
       variant: 'subtitle1',
       content: loc('oie.idv.idp.description.termsOfUse', 'login', undefined, {
-        $1: { element: 'a', attributes: { class:'terms-of-use', href: 'https://withpersona.com/legal/terms-of-use', target: '_blank', rel: 'noopener noreferrer' } },
-        $2: { element: 'a', attributes: { class:'privacy-policy', href: 'https://withpersona.com/legal/privacy-policy', target: '_blank', rel: 'noopener noreferrer' } },
+        $1: {
+          element: 'a',
+          attributes: {
+            class: 'terms-of-use', href: 'https://withpersona.com/legal/terms-of-use', target: '_blank', rel: 'noopener noreferrer',
+          },
+        },
+        $2: {
+          element: 'a',
+          attributes: {
+            class: 'privacy-policy', href: 'https://withpersona.com/legal/privacy-policy', target: '_blank', rel: 'noopener noreferrer',
+          },
+        },
       }),
     },
   };
