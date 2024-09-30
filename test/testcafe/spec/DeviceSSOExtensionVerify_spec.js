9c3d52917c155182bc186dfd4b817371011ac915
Denys Oblohin <denys.oblohin@okta.com>
2024-09-30 15:37:39 +0300

    add test from Shuo's PR

---
+++ test/testcafe/spec/DeviceSSOExtensionVerify_spec.js
@@ -0,0 +1,41 @@
+import { RequestLogger, RequestMock } from 'testcafe';
+import BasePageObject from '../framework/page-objects/BasePageObject';
+import IdentityPageObject from '../framework/page-objects/IdentityPageObject';
+import identifyWithAppleRedirectSSOExtension from '../../../playground/mocks/data/idp/idx/identify-with-apple-redirect-sso-extension-2.json';
+import appleSSOEVerifyProbe from '../../../playground/mocks/data/idp/idx/error-401-apple-sso-extension-verify.json';
+import appleSSOEVerify from '../../../playground/mocks/data/idp/idx/apple-sso-extension-verify-cancel.json';
+import identify from '../../../playground/mocks/data/idp/idx/identify';
+
+const logger = RequestLogger(/introspect/);
+
+const credentialSSOExtensionMock = RequestMock()
+  .onRequestTo(/idp\/idx\/introspect/)
+  .respond(identifyWithAppleRedirectSSOExtension)
+  .onRequestTo('http://localhost:3000/idp/idx/authenticators/sso_extension/transactions/456/verify')
+  .respond((req, res) => {
+    res.statusCode = 401;
+    res.headers['content-type'] = 'application/json';
+    res.headers['WWW-Authenticate'] = 'Oktadevicejwt realm="Okta Device"';
+    const hasProbe = new URL(req.url).searchParams.get('probe');
+    res.setBody(hasProbe ? appleSSOEVerifyProbe : appleSSOEVerify);
+  })
+  .onRequestTo('http://localhost:3000/idp/idx/authenticators/sso_extension/transactions/456/verify/cancel')
+  .respond(identify);
+
+fixture('Apple SSO Extension Verify');
+
+test
+  .requestHooks(logger, credentialSSOExtensionMock)('with credential SSO Extension approach, opens the verify URL', async t => {
+    const ssoExtensionPage = new BasePageObject(t);
+    await ssoExtensionPage.navigateToPage();
+    await t.expect(ssoExtensionPage.formExists()).ok();
+    await t.expect(logger.count(
+      record => record.response.statusCode === 200 &&
+        record.request.url.match(/introspect/)
+    )).eql(1);
+
+    // verify the end result
+    const identityPage = new IdentityPageObject(t);
+    await identityPage.fillIdentifierField('Test Identifier');
+    await t.expect(identityPage.getIdentifierValue()).eql('Test Identifier');
+  });
