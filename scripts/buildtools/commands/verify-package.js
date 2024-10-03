6197da5bc6c7c57dd0a984046a313a6ee3b30369
Lazar Vuckovic <lazar.vuckovic@okta.com>
2024-10-02 20:04:45 -0700

    trying to fix build failure

--- scripts/buildtools/commands/verify-package.js
+++ scripts/buildtools/commands/verify-package.js
@@ -17,8 +17,8 @@ const EXPECTED_BUNDLE_SIZES = {
   'okta-sign-in.oie.min.js': 1.3 * MB,
   'okta-sign-in.polyfill.js': 504 * KB,
   'okta-sign-in.polyfill.min.js': 108 * KB,
-  'okta-sign-in.next.js': 1.7 * MB,
-  'okta-sign-in.next.no-polyfill.js': 1.9 * MB,
+  'okta-sign-in.next.js': 1.8 * MB,
+  'okta-sign-in.next.no-polyfill.js': 1.8 * MB,
   'okta-sign-in.debugger.min.js': 170 * KB,
 };
 
