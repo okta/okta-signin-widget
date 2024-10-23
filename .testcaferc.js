52a1013123679111684565faa43739aaa2e9750c
Denys Oblohin <denys.oblohin@okta.com>
2024-10-23 19:03:31 +0300

    concurr 4

--- .testcaferc.js
+++ .testcaferc.js
@@ -118,10 +118,10 @@ const config = {
   // OKTA_SIW_ONLY_FLAKY: 1 ← flaky
   // !CHROME_HEADLESS: 1 ← for debugging tests
   // OKTA_SIW_EN_LEAKS: 2 ← somewhat flaky
-  // !OKTA_SIW_EN_LEAKS: 6 ← most tests
+  // !OKTA_SIW_EN_LEAKS: 4 ← most tests
   concurrency: (OKTA_SIW_ONLY_FLAKY || !env.CHROME_HEADLESS) 
     ? 1 
-    : (env.OKTA_SIW_EN_LEAKS ? 2 : 6),
+    : (env.OKTA_SIW_EN_LEAKS ? 2 : 4),
 
   // retry failed tests
   quarantineMode: env.OKTA_SIW_EN_LEAKS ? false : {
