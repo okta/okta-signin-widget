213c2c8131a4f2f0bb6cb2ceccd34abbe5c66fa4
Praveen <praveen.durai@okta.com>
2024-12-08 23:12:22 -0600

    added v2 support

---
+++ src/v2/view-builder/utils/IdpUtil.js
@@ -0,0 +1,31 @@
+import { FORMS } from 'v2/ion/RemediationConstants';
+
+export function getIdvName(remediations) {
+  let redirectIDVerifyRemediation = remediations.find(remediation => {
+    return remediation.name === FORMS.REDIRECT_IDVERIFY;
+  });
+  return redirectIDVerifyRemediation.idp.name;
+}
+
+export function getHelpLinks(remediations) {
+  const idpName = getIdvName(remediations);
+  let termsOfUse, privacyPolicy;
+  switch (idpName) {
+  case 'Persona':
+    termsOfUse = 'https://withpersona.com/legal/terms-of-use';
+    privacyPolicy = 'https://withpersona.com/legal/privacy-policy';
+    break;
+  case 'Clear':
+    termsOfUse = 'https://www.clearme.com/member-terms';
+    privacyPolicy = 'https://www.clearme.com/privacy-policy';
+    break;
+  case 'Incode':
+    termsOfUse = 'https://incode.id/terms';
+    privacyPolicy = 'https://incode.id/privacy';
+    break;
+  }
+  return {
+    termsOfUse,
+    privacyPolicy
+  };
+}
--- src/v2/view-builder/views/idp/RedirectIdvView.js
+++ src/v2/view-builder/views/idp/RedirectIdvView.js
@@ -1,32 +1,36 @@
 import { loc, View } from '@okta/courage';
 import { BaseIdPAuthenticatorBody, BaseIdpAuthenticatorView } from './BaseIdpAuthenticator';
-import { FORMS } from 'v2/ion/RemediationConstants';
 import hbs from '@okta/handlebars-inline-precompile';
 import { BaseFooter } from 'v2/view-builder/internals';
+import { getHelpLinks, getIdvName } from '../../utils/IdpUtil';
 
 const PrivacyTermsFooterView = View.extend({
   tagName: 'p',
   className: 'privacy-footer',
   template: hbs`
-      <p>{{i18n code="oie.idv.idp.description.termsOfUse" bundle="login" 
-        $1="<a href='https://withpersona.com/legal/terms-of-use' class='inline-link' data-se='terms-of-use'
+      <p>{{i18n code="oie.idv.idp.desc.termsOfUse" bundle="login" arguments="idpName"
+        $1="<a class='inline-link' data-se='terms-of-use'
           target='_blank' rel='noopener noreferrer'>$1</a>"
-        $2="<a href='https://withpersona.com/legal/privacy-policy' class='inline-link' data-se='privacy-policy'
+        $2="<a class='inline-link' data-se='privacy-policy'
            target='_blank' rel='noopener noreferrer'>$2</a>"}}
       </p>
-      <p>{{i18n code="oie.idv.idp.description.agreement" bundle="login"}}</p>
-    `
+      <p>{{i18n code="oie.idv.idp.desc.agreement" bundle="login" arguments="idpName"}}</p>
+    `,
+  getTemplateData() {
+    const idpName = getIdvName(this.options.appState.get('remediations'));
+    return {
+      idpName,
+    };
+  },
+
 });
 
 const Body = BaseIdPAuthenticatorBody.extend({
   title() {
-    const redirectIDVerifyRemediation = this.options.appState.get('remediations').find((remediation) => {
-      return remediation.name === FORMS.REDIRECT_IDVERIFY;
-    });
-    return loc('oie.idv.idp.title', 'login', [redirectIDVerifyRemediation.idp.name]);
+    return loc('oie.idv.idp.title', 'login', [getIdvName(this.options.appState.get('remediations'))]);
   },
   subtitle() {
-    return loc('oie.idv.idp.description', 'login');
+    return loc('oie.idv.idp.desc', 'login', [getIdvName(this.options.appState.get('remediations'))]);
   },
   save() {
     return loc('oie.optional.authenticator.button.title', 'login');
@@ -38,6 +42,9 @@ export default BaseIdpAuthenticatorView.extend({
   Footer: BaseFooter.extend({
     postRender() {
       this.add(PrivacyTermsFooterView);
+      const { termsOfUse, privacyPolicy} = getHelpLinks(this.options.appState.get('remediations'));
+      this.$el.find('[data-se="terms-of-use"]').attr('href', termsOfUse);
+      this.$el.find('[data-se="privacy-policy"]').attr('href', privacyPolicy);
     }
   }),
 });
