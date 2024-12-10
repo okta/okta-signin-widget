4504d3778c66175f375b8fc0e86bbac46329d68f
Praveen <praveen.durai@okta.com>
2024-12-10 12:18:26 -0600

    Convert to Uppercase before validate

--- src/v2/view-builder/utils/IdpUtil.js
+++ src/v2/view-builder/utils/IdpUtil.js
@@ -10,16 +10,16 @@ export function getIdvName(remediations) {
 export function getHelpLinks(remediations) {
   const idpName = getIdvName(remediations);
   let termsOfUse, privacyPolicy;
-  switch (idpName) {
-  case 'Persona':
+  switch (idpName?.toUpperCase()) {
+  case 'PERSONA':
     termsOfUse = 'https://withpersona.com/legal/terms-of-use';
     privacyPolicy = 'https://withpersona.com/legal/privacy-policy';
     break;
-  case 'Clear':
+  case 'CLEAR':
     termsOfUse = 'https://www.clearme.com/member-terms';
     privacyPolicy = 'https://www.clearme.com/privacy-policy';
     break;
-  case 'Incode':
+  case 'INCODE':
     termsOfUse = 'https://incode.id/terms';
     privacyPolicy = 'https://incode.id/privacy';
     break;
