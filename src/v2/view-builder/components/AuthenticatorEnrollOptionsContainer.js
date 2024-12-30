d6f2446c91cdbd7808fc90de324b97372960a86c
Tiffany Ng <tiffany.ng@okta.com>
2024-12-30 10:47:05 -0800

    format iso instead of epoch, clean up testcafe tests

--- src/v2/view-builder/components/AuthenticatorEnrollOptionsContainer.js
+++ src/v2/view-builder/components/AuthenticatorEnrollOptionsContainer.js
@@ -25,7 +25,7 @@ export default View.extend({
 
     this.options.optionItems.forEach((authenticator) => {
       if (authenticator.relatesTo?.gracePeriod?.expiry
-        && parseInt(authenticator.relatesTo?.gracePeriod?.expiry) > (new Date(Date.now()).getTime() / 1000)) {
+        && new Date(authenticator.relatesTo?.gracePeriod?.expiry).getTime() > (new Date(Date.now()).getTime())) {
         authenticatorsWithGracePeriod.push(authenticator);
       } else {
         authenticators.push(authenticator);
