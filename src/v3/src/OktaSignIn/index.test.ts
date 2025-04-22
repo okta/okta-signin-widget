03b8741c486f5d417ab3365d25962242ec8b123b
Shaw Yu <shaw.yu@okta.com>
2025-04-22 15:57:53 -0700

    dont need h

--- src/v3/src/OktaSignIn/index.test.ts
+++ src/v3/src/OktaSignIn/index.test.ts
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-import { h, render } from 'preact';
+import { render } from 'preact';
 
 import type { RenderResult } from '../../../types';
 import type { WidgetProps } from '../types';
