df46e37b0c40e6bf8976dad2922a71413d148d24
Shaw Yu <shaw.yu@okta.com>
2025-06-05 10:56:49 -0700

    Revert "run the test file only and also run it 100 times"
    This reverts commit 2533fc6edb9b8515ee12950cd85ca54e1cfa1279.

--- scripts/unit-v3.sh
+++ scripts/unit-v3.sh
@@ -7,25 +7,9 @@ export TEST_RESULT_FILE_DIR="${REPO}/src/v3/build2/reports/unit"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
-fail_count=0
-
-yarn workspace v3 run codegen
-
-for i in {1..50}; do
-  echo "$i | Running test, attempt $i"
-  if ! yarn workspace v3 run jest flow-okta-verify-enrollment.test.tsx --no-colors; then
-    ((fail_count++))
-  fi
-  echo "$i | Done with attempt $i"
-done
-
-if [ "$fail_count" -gt 0 ]; then
-  echo "v3 jest tests failed [[[[ $fail_count ]]]] times! Exiting..."
+if ! yarn workspace v3 test --no-colors; then
+  echo "v3 jest tests failed! Exiting..."
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
-# if ! yarn workspace v3 test flow-okta-verify-enrollment.test.tsx --no-colors; then
-#   echo "v3 jest tests failed! Exiting..."
-#   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
-# fi
 
 exit $PUBLISH_TYPE_AND_RESULT_DIR;
