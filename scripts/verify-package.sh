42a1bbef982e8bd4d2f89a8b63ba32ba54596ae5
Jared Perreault <jared.perreault@okta.com>
2025-08-13 11:40:14 -0400

    authjs bump

--- scripts/verify-package.sh
+++ scripts/verify-package.sh
@@ -125,11 +125,6 @@ if ! setup_service node v14.18.2 &> /dev/null; then
   exit ${FAILED_SETUP}
 fi
 
-echo '############'
-echo $INTERNAL_REGISTRY
-echo $PUBLIC_REGISTRY
-echo '############'
-
 # ensure registry is configured after node verison is changed
 npm config set @okta:registry ${PUBLIC_REGISTRY}
 npm config set registry ${PUBLIC_REGISTRY}
@@ -180,7 +175,6 @@ fi
 pushd test/package/tsc
 if ! (yarn clean && yarn install && yarn test); then
   echo "TSC package verification failed! Exiting..."
-  yarn why oblivious-set
   exit ${TEST_FAILURE}
 fi
 popd
