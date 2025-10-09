a34c9b8a7c5adb9b2ac09d61189c0ef153d5c760
Lee McHale <lee.mchale@okta.com>
2025-10-09 11:33:14 -0700

    add comment

--- test/testcafe/framework/shared/mockGetPostMessage.js
+++ test/testcafe/framework/shared/mockGetPostMessage.js
@@ -1,3 +1,7 @@
+// the method __get$PostMessage is used by testcafe hammerhead script to communicate between the recaptcha iframe and the testcafe server
+// it causes a MessagePort cloning error when SIW loads the recaptcha script.  
+// Since we don't need testcafe to communicate with the recaptcha iframe for the test to function,
+// we work around this by overriding window.__get$PostMessage with a dummy function at the beginning of a recaptcha test.
 Object.defineProperty(window, '__get$PostMessage', {
   value: function() {
     return function() {};
