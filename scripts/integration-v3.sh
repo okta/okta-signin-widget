e57c38af90e15b5b2dc043926b26484966efe768
Shuo Wu <shuo.wu@okta.com>
2026-01-07 12:52:53 -0500

    split unit and integration for gen3

--- scripts/integration-v3.sh
+++ scripts/integration-v3.sh
@@ -3,12 +3,12 @@
 source $OKTA_HOME/$REPO/scripts/setup.sh
 
 export TEST_SUITE_TYPE="junit"
-export TEST_RESULT_FILE_DIR="${REPO}/src/v3/build2/reports/unit"
+export TEST_RESULT_FILE_DIR="${REPO}/src/v3/build2/reports/integration"
 echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
 echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 
-if ! yarn workspace v3 test:unit --coverage --no-colors; then
-  echo "v3 unit tests failed! Exiting..."
+if ! yarn workspace v3 test:integration --no-colors; then
+  echo "v3 integration tests failed! Exiting..."
   exit ${PUBLISH_TYPE_AND_RESULT_DIR_BUT_ALWAYS_FAIL}
 fi
 
