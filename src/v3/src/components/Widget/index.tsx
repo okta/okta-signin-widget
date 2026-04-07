8a836b1bda53799abf2464bd6c6cf63390bd9ef9
Lester Choi <lester.choi@okta.com>
2026-04-07 16:26:42 -0400

    fix: remove usage of withNetworkRetry hook

--- src/v3/src/components/Widget/index.tsx
+++ src/v3/src/components/Widget/index.tsx
@@ -35,7 +35,6 @@ import { mergeThemes } from 'src/util/mergeThemes';
 
 import Bundles from '../../../../util/Bundles';
 import Logger from '../../../../util/Logger';
-import { withNetworkRetry } from '../../util/retryRequest';
 import {
   ABORT_REASON_WEBAUTHN_AUTOFILLUI_STEP_NOT_FOUND,
   IDX_STEP,
@@ -120,6 +119,7 @@ export const Widget: FunctionComponent<WidgetProps> = (widgetProps) => {
     elements: [],
   });
   const [message, setMessage] = useState<IdxMessage | undefined>();
+  useOnlineStatus(setMessage);
   const [idxTransaction, setIdxTransaction] = useState<IdxTransaction | undefined>();
   const [isClientTransaction, setIsClientTransaction] = useState<boolean>(false);
   const [stepToRender, setStepToRender] = useState<string | undefined>(undefined);
@@ -220,11 +220,11 @@ export const Widget: FunctionComponent<WidgetProps> = (widgetProps) => {
         setIdxTransaction(await triggerEmailVerifyCallback(widgetProps));
         return;
       }
-      let transaction: IdxTransaction = await withNetworkRetry(() => authClient.idx.start({
+      let transaction: IdxTransaction = await authClient.idx.start({
         stateHandle,
         // Required to prevent auth-js from clearing sessionStorage and breaking interaction code flow
         exchangeCodeForTokens: false,
-      }));
+      });
       const hasError = !transaction.requestDidSucceed || transaction.messages?.some(
         (msg) => msg.class === MessageType.ERROR.toString(),
       );
@@ -236,10 +236,10 @@ export const Widget: FunctionComponent<WidgetProps> = (widgetProps) => {
       // OKTA-651781
       // bootstrap into enroll flow when flow param is set to signup
       if (shouldRedirectToEnrollFlow(transaction)) {
-        transaction = await withNetworkRetry(() => authClient.idx.proceed({
+        transaction = await authClient.idx.proceed({
           stateHandle: transaction?.context.stateHandle,
           step: IDX_STEP.SELECT_ENROLL_PROFILE,
-        }));
+        });
       }
 
       await widgetHooks.callHooks('before', transaction);
@@ -380,17 +380,17 @@ export const Widget: FunctionComponent<WidgetProps> = (widgetProps) => {
         setIdxTransaction(await triggerEmailVerifyCallback(widgetProps));
         return;
       }
-      let transaction = await withNetworkRetry(() => authClient.idx.proceed({
+      let transaction = await authClient.idx.proceed({
         stateHandle: idxTransaction?.context.stateHandle,
-      }));
+      });
 
       // TODO
       // OKTA-651781
       if (shouldRedirectToEnrollFlow(transaction)) {
-        transaction = await withNetworkRetry(() => authClient.idx.proceed({
+        transaction = await authClient.idx.proceed({
           stateHandle: transaction?.context.stateHandle,
           step: IDX_STEP.SELECT_ENROLL_PROFILE,
-        }));
+        });
       }
 
       await widgetHooks.callHooks('before', transaction);
