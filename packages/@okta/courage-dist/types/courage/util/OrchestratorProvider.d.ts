5b4d95f413472ad2f22c93497077e77f7dacd7e9
Shuo Wu <shuo.wu@okta.com>
2026-04-10 14:24:48 -0400

    ci fix

--- packages/@okta/courage-dist/types/courage/util/OrchestratorProvider.d.ts
+++ packages/@okta/courage-dist/types/courage/util/OrchestratorProvider.d.ts
@@ -1,7 +1,8 @@
-import { HttpMethod, AuthorizationHeaderTypes } from '@okta/ui-libraries-oidc-auth-headers';
-export { AuthorizationHeaderTypes };
+export declare type HttpMethod = string;
+export declare type HeaderRecord<T extends string> = Record<T, string>;
+export declare enum AuthorizationHeaderTypes {}
 export declare type AuthParams = {
     acrValues?: string;
     maxAge?: number;
 };
-export declare const getOidcRequestHeaders: (scopes: string[], url: string, method: HttpMethod, authParams?: AuthParams) => Promise<Partial<import("@okta/ui-libraries-oidc-auth-headers").HeaderRecord<"dpop" | "authorization">>>;
+export declare const getOidcRequestHeaders: (scopes: string[], url: string, method: HttpMethod, authParams?: AuthParams) => Promise<Partial<HeaderRecord<"dpop" | "authorization">>>;
