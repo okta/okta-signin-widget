f7cba4fc44d349ed81520ae70056bc1ef9eef922
Lazar Vuckovic <lazar.vuckovic@okta.com>
2026-04-28 14:47:19 -0700

    chore: fix lint issues

--- src/v3/src/util/webauthnUtils.ts
+++ src/v3/src/util/webauthnUtils.ts
@@ -57,9 +57,10 @@ export const isPasskeyAutofillAvailable = async () => {
 
 export const isGetPasskeyAvailable = () => isCredentialsGetApiAvailable() && typeof PublicKeyCredential !== 'undefined';
 
-export const isRelyingPartyIdMismatchError = (error: unknown): boolean =>
-  (error as DOMException)?.name === 'SecurityError' &&
-  (error as DOMException)?.code === 18;
+export const isRelyingPartyIdMismatchError = (error: unknown): boolean => (
+  (error as DOMException)?.name === 'SecurityError'
+  && (error as DOMException)?.code === 18
+);
 
 /**
  * Uses the Web Authentication API to generate credentials for enrolling
